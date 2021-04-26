46d889a8737c444aa1fa46563c6e745fd3b5f8ec
Santiago8888 <santiago.marti67@gmail.com>
2021-04-26 13:14:06 -0500

    Exported Video.

--- cicero/src/components/Elements/Video.tsx
+++ cicero/src/components/Elements/Video.tsx
@@ -1,5 +1,5 @@
 interface iVideo { title:string, src:string, description:string }
-const Video = ({ title, src, description }: iVideo) => <div className="content">
+export const Video = ({ title, src, description }: iVideo) => <div className="content">
     <h1> { title } </h1>
     <iframe src={ src } width="600" height="338" frameBorder="0" allowFullScreen/>
     <p> { description } </p>
