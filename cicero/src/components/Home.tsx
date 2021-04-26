ddf49e40dc1b4f48ebdb47afa44184f23352c572
Santiago8888 <santiago.marti67@gmail.com>
2021-04-26 16:48:13 -0500

    Logic for Views on Home.

--- cicero/src/components/Home.tsx
+++ cicero/src/components/Home.tsx
@@ -1,1 +1,19 @@
-export const Home = () => <></>
+import { Document, Quiz, Video } from './Views'
+import { Recordings } from './Forum/Recordings'
+
+import { Landing } from './Auth/Landing'
+import { iLesson } from './LayOut/Menu'
+import { Forum } from './Forum/Forum'
+
+
+interface iHome { isAuth:boolean, lesson: iLesson, isForum:boolean, isRecordings:boolean }
+export const Home = ({ isAuth, lesson, isForum, isRecordings }: iHome) => {
+    return isAuth
+        ?   isForum ?   <Forum title={''} questions={[]} description={''}/>
+        :   isRecordings ? <Recordings recordings={[]} title={''} description={''}/>
+        :   lesson.type === 'Video' ? <Video {...lesson} next={() => {}}/>
+        :   lesson.type === 'Lectura' ? <Document {...lesson} next={() => {}}/>
+        :   lesson.type === 'Quiz' ? <Quiz  {...lesson} next={() => {}}/> : <Landing/>
+        :   <Landing/>
+
+}
