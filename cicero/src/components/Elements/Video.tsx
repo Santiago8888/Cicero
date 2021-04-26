60f67ca5407107848ee3cff4ce4caf34c55580aa
Santiago8888 <santiago.marti67@gmail.com>
2021-04-26 14:37:22 -0500

    Next button on video.

--- cicero/src/components/Elements/Video.tsx
+++ cicero/src/components/Elements/Video.tsx
@@ -1,6 +1,8 @@
-interface iVideo { title:string, src:string, description:string }
-export const Video = ({ title, src, description }: iVideo) => <div className="content">
+interface iVideo { title:string, src:string, description:string, next():void }
+export const Video = ({ title, src, description, next }: iVideo) => <div className="content">
     <h1> { title } </h1>
-    <iframe src={ src } width="600" height="338" frameBorder="0" allowFullScreen/>
     <p> { description } </p>
+
+    <iframe src={ src } width="600" height="338" frameBorder="0" allowFullScreen/>
+    <a className='button'> onClick={next} Siguiente </a>
 </div>
