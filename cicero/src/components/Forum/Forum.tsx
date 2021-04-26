92f0adf6a6d71d2a2b260334551531ae423d20ec
Santiago8888 <santiago.marti67@gmail.com>
2021-04-26 15:21:41 -0500

    Modal on Forum with Submit Event.

--- cicero/src/components/Forum/Forum.tsx
+++ cicero/src/components/Forum/Forum.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react"
 
 const cardStyle = {
     backgroundColor: 'rgb(48, 48, 48)',
@@ -9,10 +10,10 @@ const cardStyle = {
 
 const headerStyle = { backgroundColor: 'rgb(72, 72, 72)', borderTopLeftRadius: 12, borderTopRightRadius: 12 }
 
-interface iQuestion { title:string, details:string }
-const Question = ({ title, details }: iQuestion) => <div className='card' style={cardStyle}>
+interface iQuestion { question:string, details:string }
+const Question = ({ question, details }: iQuestion) => <div className='card' style={cardStyle}>
     <header className='card-header' style={headerStyle}>
-        <p className='card-header-title' style={{color:'white', fontSize:'1.25rem'}}> { title } </p>
+        <p className='card-header-title' style={{color:'white', fontSize:'1.25rem'}}> { question } </p>
     </header>
 
     <div className='content' style={{color:'whitesmoke', marginTop:'1rem'}}> 
@@ -20,27 +21,77 @@ const Question = ({ title, details }: iQuestion) => <div className='card' style=
     </div>
 </div>
 
-interface iForum { title:string, description:string, questions:iQuestion[] }
-export const Forum = ({ title, description, questions }: iForum) => <div className="content">
-    <h1> { title } </h1>
-    <div className='columns' >
-        <div className='column'>
-            <p> { description } </p>
+
+interface iModal { isActive:boolean, deactivate():void, submit(question:iQuestion):void }
+const Modal = ({ isActive, deactivate, submit }:iModal) => {
+    const [question, setQuestion] = useState('')
+    const [details, setDetails] = useState('')
+
+    return <div className={`modal ${isActive ? 'is-active' : ''}`}>
+        <div className="modal-background" />
+        <div className="modal-content">
+            <button className="delete" aria-label="close" style={{float:'right'}} onClick={deactivate}/>
+            <div className="field">
+                <label className="label"> Pregunta </label>
+
+                <div className="control">
+                    <input 
+                        className="input" 
+                        type="text" 
+                        value={question} 
+                        onChange={({target:{value}})=> setQuestion(value)}
+                    />
+                </div>
+            </div>
+
+            <div className="field">
+                <label className="label"> Detalles Adicionales (opcional): </label>    
+                <div className="control">
+                    <textarea 
+                        className="textarea" 
+                        placeholder="e.g. Hello world" 
+                        value={details} 
+                        onChange={({target:{value}})=> setDetails(value)}
+                    />
+                </div>
+            </div>
         </div>
 
-        <div className='column'>
-            <button className='button'> 
-                Nueva Pregunta 
-            </button>
-        </div>
+        <button className='button' onClick={() => submit({ question, details })}> Siguiente </button>
     </div>
-
-    {
-        questions.map((q, i) => 
-            <Question  {...q} key={i}/>
-        )
-    }
+}
 
 
+interface iForum { title:string, description:string, questions:iQuestion[] }
+export const Forum = ({ title, description, questions }: iForum) => {
+    const [isActive, setActive] = useState(false)
+    const submit = (question:iQuestion) => {console.log('question', question)}
 
-</div>
+    return <div className="content">
+        <h1> { title } </h1>
+        <div className='columns' >
+            <div className='column'>
+                <p> { description } </p>
+            </div>
+
+            <div className='column'>
+                <button className='button'> 
+                    Nueva Pregunta 
+                </button>
+            </div>
+        </div>
+
+        {
+            questions.map((q, i) => 
+                <Question  {...q} key={i}/>
+            )
+        }
+
+        <Modal 
+            isActive={isActive} 
+            deactivate={() => setActive(false)}
+            submit={submit}
+        />
+
+    </div>
+}
