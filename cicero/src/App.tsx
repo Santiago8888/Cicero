2db3ac595e45f3f2784eaacc51b0cc297a2eedff
Santiago8888 <santiago.marti67@gmail.com>
2021-04-26 15:51:39 -0500

    Trivial App Layout Implementation.

--- cicero/src/App.tsx
+++ cicero/src/App.tsx
@@ -1,7 +1,15 @@
-import logo from './logo.svg'
+
+import { NavBar } from './components/LayOut/NavBar'
+import { Menu } from './components/LayOut/Menu'
+import { Home } from './components/Home'
+
+import 'bulma/css/bulma.css'
 import './App.css'
 
 const App = () => <div className="App">
+    <NavBar />
+    <Menu modules={[]}/>
+    <Home />
 </div>
 
 
