5971aaad239ed76e1041181cb7ef2d93b5744829
Santiago8888 <santiago.marti67@gmail.com>
2021-04-26 17:01:15 -0500

    Imported forum and recordings props from parent.

--- cicero/src/components/Home.tsx
+++ cicero/src/components/Home.tsx
@@ -1,19 +1,18 @@
-import { Document, Quiz, Video } from './Views'
-import { Recordings } from './Forum/Recordings'
+import { Recordings, iRecordings } from './Forum/Recordings'
+import { Forum, iForum } from './Forum/Forum'
 
+import { Document, Quiz, Video } from './Views'
 import { Landing } from './Auth/Landing'
 import { iLesson } from './LayOut/Menu'
-import { Forum } from './Forum/Forum'
 
 
-interface iHome { isAuth:boolean, lesson: iLesson, isForum:boolean, isRecordings:boolean }
-export const Home = ({ isAuth, lesson, isForum, isRecordings }: iHome) => {
+interface iHome { isAuth:boolean, lesson: iLesson, forum?:iForum, recordings?:iRecordings }
+export const Home = ({ isAuth, lesson, forum, recordings }: iHome) => {
     return isAuth
-        ?   isForum ?   <Forum title={''} questions={[]} description={''}/>
-        :   isRecordings ? <Recordings recordings={[]} title={''} description={''}/>
+        ?   forum ?   <Forum {...forum}/>
+        :   recordings ? <Recordings {...recordings}/>
         :   lesson.type === 'Video' ? <Video {...lesson} next={() => {}}/>
-        :   lesson.type === 'Lectura' ? <Document {...lesson} next={() => {}}/>
+        :   lesson.type === 'Reading' ? <Document {...lesson} next={() => {}}/>
         :   lesson.type === 'Quiz' ? <Quiz  {...lesson} next={() => {}}/> : <Landing/>
         :   <Landing/>
-
 }
