ed81819735b8de516304965ceceea268ae502297
Santiago8888 <santiago.marti67@gmail.com>
2021-04-27 13:44:06 -0500

    Add Current into User Model.

--- cicero/src/App.tsx
+++ cicero/src/App.tsx
@@ -10,7 +10,7 @@ import { useEffect, useState } from 'react'
 import 'bulma/css/bulma.css'
 import './App.css'
 
-export interface iUser { email:string, progress:iPosition, quizFailures:number }
+export interface iUser { email:string, progress:iPosition, quizFailures:number, current:iPosition }
 const lesson: iLesson = { title:'', description:'', type:'Video' }
 const forum:iForum = { title:'', description:'', questions:[] }
 const recordings:iRecordings = { title:'', description:'', recordings:[] }
@@ -26,12 +26,12 @@ const modules:iModule[] = [
 
 interface iHomeData { forum?:iForum, recordings?:iRecordings, lesson:iLesson}
 const initialData:iHomeData = { forum:undefined, recordings:undefined, lesson }
-const defaultUser:iUser = { email:'test@branding.gq', progress:{module:0, lesson:0}, quizFailures:0 }
+const initialPosition = {module:0, lesson:0}
+const defaultUser:iUser = { email:'test@branding.gq', progress:initialPosition, current:initialPosition, quizFailures:0 }
 export const App = () => {
     const [isAuth, setAuth] = useState(false)
     const [isLogin, setLogin] = useState(false)
     const [homeData, setHomeData] = useState<iHomeData>(initialData)
-    const [current, setCurrent] = useState({module:0, lesson:0})
     const [user, setUser] = useState<iUser>(defaultUser)
 
 
@@ -47,41 +47,39 @@ export const App = () => {
         setLogin(true)
     }
 
-    useEffect(() => {
-        if(current.lesson > user.progress.lesson || current.module > user.progress.module) setUser({...user, progress:current})
-    }, [current])
 
     const nextLesson = ({module, lesson}:iPosition) => {
         if (modules[module].lessons[lesson+1]) return { module:module, lesson:lesson+1 }
-        else if (modules[current.module + 1]) return { module: module+1, lesson:0 }
+        else if (modules[user.current.module + 1]) return { module: module+1, lesson:0 }
         else return { module, lesson }
     }
 
     const next = () => {
         if(lesson.type === 'Quiz'){
-            if(user.quizFailures === 0) return setCurrent(nextLesson(current))
+            if(user.quizFailures === 0) return setUser({...user, current:nextLesson(user.current)})
             if(user.quizFailures === 1) return
-            if(user.quizFailures === 2) {
-                const newPosition = {...current, lesson:0}
-                setCurrent(newPosition)
-                setUser({...user, quizFailures:0, progress:newPosition})
-            }
+            if(user.quizFailures === 2) setUser({
+                ...user, 
+                quizFailures:0, 
+                progress:{...user.current, lesson:0}, 
+                current: {...user.current, lesson:0}
+            })
 
-        } else return setCurrent(nextLesson(current))
+        } else return setUser({...user, current:nextLesson(user.current)})
     }
 
     const navigate = ({module, lesson}:iPosition) => {
         if(module > user.progress.module) return
         if(lesson > user.progress.lesson) return
 
-        setCurrent({ module, lesson })
+        setUser({...user, current: { module, lesson } })
     }
 
     const approveQuiz = (score:number) => {
         if(!lesson.questions?.length) return false 
 
         const minScore = lesson.minScore || lesson.questions.length*.7
-        const isPassing = user.progress.lesson === current.lesson && user.progress.module === current.module
+        const isPassing = user.progress.lesson === user.current.lesson && user.progress.module === user.current.module
 
         if(isPassing && score >= minScore) setUser({...user, progress:nextLesson(user.progress), quizFailures:0})
         else if(isPassing && user.quizFailures > 1) setUser({...user, quizFailures:2 })
@@ -91,14 +89,14 @@ export const App = () => {
         else return false
     }
 
-    const approve = (score:number) => {
-        if(lesson.type === 'Quiz') return approveQuiz(score)
-        else return approveQuiz(score)
+    const approve = (score?:number) => {
+        if(lesson.type === 'Quiz' && score) return approveQuiz(score)
+        return setUser({...user, progress:nextLesson(user.progress)})
     }
 
     return <div className="App">
         <NavBar click={(item) => clickNavbar(item)}/>
-        <Menu modules={modules} current={current} navigate={navigate} user={user}/>
-        <Home {...homeData} isAuth={isAuth} isLogin={isLogin} login={login} next={next} approve={approveQuiz} user={user}/>
+        <Menu modules={modules} navigate={navigate} user={user}/>
+        <Home {...homeData} isAuth={isAuth} isLogin={isLogin} login={login} next={next} approve={approve} user={user}/>
     </div>
 }
