d372c6684c85e802305859182476ea4a21e2d5b9
Santiago8888 <santiago.marti67@gmail.com>
2021-04-26 19:04:38 -0500

    Add Login Form to Home Component.

--- cicero/src/components/Home.tsx
+++ cicero/src/components/Home.tsx
@@ -4,15 +4,17 @@ import { Forum, iForum } from './Forum/Forum'
 import { Document, Quiz, Video } from './Views'
 import { Landing } from './Auth/Landing'
 import { iLesson } from './LayOut/Menu'
+import { Login } from './Auth/Login'
 
 
-interface iHome { isAuth:boolean, lesson: iLesson, forum?:iForum, recordings?:iRecordings }
-export const Home = ({ isAuth, lesson, forum, recordings }: iHome) => {
+interface iHome { isAuth:boolean, isLogin:boolean, lesson:iLesson, forum?:iForum, recordings?:iRecordings }
+export const Home = ({ isAuth, isLogin, lesson, forum, recordings }: iHome) => {
     return isAuth
-        ?   forum ?   <Forum {...forum}/>
-        :   recordings ? <Recordings {...recordings}/>
-        :   lesson.type === 'Video' ? <Video {...lesson} next={() => {}}/>
-        :   lesson.type === 'Reading' ? <Document {...lesson} next={() => {}}/>
-        :   lesson.type === 'Quiz' ? <Quiz  {...lesson} next={() => {}}/> : <Landing/>
+        ?   isLogin ?  <Login submit={()=>{}}/>
+            :   forum ? <Forum {...forum}/>
+            :   recordings ? <Recordings {...recordings}/>
+            :   lesson.type === 'Video' ? <Video {...lesson} next={() => {}}/>
+            :   lesson.type === 'Reading' ? <Document {...lesson} next={() => {}}/>
+            :   lesson.type === 'Quiz' ? <Quiz  {...lesson} next={() => {}}/> : <Landing/>
         :   <Landing/>
 }
