7de4de6b37cf065c14b28beaa4f8317e9d46a51a
Santiago8888 <santiago.marti67@gmail.com>
2021-04-27 12:43:09 -0500

    Add Aprove and user to Home Screen.

--- cicero/src/components/Home.tsx
+++ cicero/src/components/Home.tsx
@@ -5,24 +5,27 @@ import { Document, Quiz, Video } from './Views'
 import { Login, iLogin } from './Auth/Login'
 import { Landing } from './Auth/Landing'
 import { iLesson } from './LayOut/Menu'
+import { iUser } from '../App'
 
 
 interface iHome extends iLogin { 
+    user:iUser
     isAuth:boolean, 
     isLogin:boolean, 
     lesson:iLesson, 
     forum?:iForum, 
     recordings?:iRecordings 
     next():void
+    approve(score:number):boolean
 }
 
-export const Home = ({ isAuth, isLogin, lesson, forum, recordings, login, next }: iHome) => {
+export const Home = ({ user, isAuth, isLogin, lesson, forum, recordings, login, next, approve }: iHome) => {
     return isAuth
         ?   isLogin ?  <Login login={login}/>
             :   forum ? <Forum {...forum}/>
             :   recordings ? <Recordings {...recordings}/>
             :   lesson.type === 'Video' ? <Video {...lesson} next={next}/>
             :   lesson.type === 'Reading' ? <Document {...lesson} next={next}/>
-            :   lesson.type === 'Quiz' ? <Quiz  {...lesson} next={next}/> : <Landing/>
+            :   lesson.type === 'Quiz' ? <Quiz  {...lesson} next={next} approve={approve} user={user}/> : <Landing/>
         :   <Landing/>
 }
