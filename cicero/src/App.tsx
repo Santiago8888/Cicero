8abad3428e86606e9f7befe36fb8981a5b564498
Santiago8888 <santiago.marti67@gmail.com>
2021-04-27 10:29:38 -0500

    User interface and first part of next method.

--- cicero/src/App.tsx
+++ cicero/src/App.tsx
@@ -1,20 +1,21 @@
-import { iLesson, iModule, Menu } from './components/LayOut/Menu'
+import { iLesson, iModule, Menu, iPosition } from './components/LayOut/Menu'
 import { NavBar, NavbarItem } from './components/LayOut/NavBar'
 import { iRecordings } from './components/Forum/Recordings'
 import { iLoginInput } from './components/Auth/Login'
 import { iForum } from './components/Forum/Forum'
 import { Home } from './components/Home'
+
 import { useState } from 'react'
 
 import 'bulma/css/bulma.css'
 import './App.css'
 
-
+interface iUser { email:string, progress:iPosition, quizFailures:number }
 const lesson: iLesson = { title:'', description:'', type:'Video', locked:false }
 const forum:iForum = { title:'', description:'', questions:[] }
 const recordings:iRecordings = { title:'', description:'', recordings:[] }
 
-const Modules:iModule[] = [
+const modules:iModule[] = [
     { title:'Modulo 1', locked:false, lessons:[]},
     { title:'Módulo 2', locked:true, lessons:[]},
     { title:'Módulo 3', locked:false, lessons:[]},
@@ -25,12 +26,14 @@ const Modules:iModule[] = [
 
 interface iHomeData { forum?:iForum, recordings?:iRecordings, lesson:iLesson}
 const initialData:iHomeData = { forum:undefined, recordings:undefined, lesson }
+const defaultUser:iUser = { email:'test@branding.gq', progress:{module:0, lesson:0}, quizFailures:0 }
 export const App = () => {
     const [isAuth, setAuth] = useState(false)
     const [isLogin, setLogin] = useState(false)
     const [homeData, setHomeData] = useState<iHomeData>(initialData)
-    const [modules, setModules] = useState(Modules)
     const [current, setCurrent] = useState({module:0, lesson:0})
+    const [user, setUser] = useState<iUser>(defaultUser)
+
 
     const clickNavbar = (item:NavbarItem) => {
         if(item === 'Forum') return setHomeData({...homeData, forum, recordings:undefined})
@@ -44,9 +47,19 @@ export const App = () => {
         setLogin(true)
     }
 
+    const next = () => {
+        // Update Current
+        if (modules[current.module].lessons[current.lesson+1]) setCurrent({...current, lesson:current.lesson+1})
+        else if (modules[current.module + 1]) setCurrent({module:current.module+1, lesson:0})
+        else alert('Congratulations')
+
+        // Update User
+
+    }
+
     return <div className="App">
         <NavBar click={(item) => clickNavbar(item)}/>
         <Menu modules={modules} current={current} navigate={(curr) => setCurrent(curr)}/>
-        <Home {...homeData} isAuth={isAuth} isLogin={isLogin} login={login}/>
+        <Home {...homeData} isAuth={isAuth} isLogin={isLogin} login={login} next={next}/>
     </div>
 }
