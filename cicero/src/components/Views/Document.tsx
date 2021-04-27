90e8f8c99b11806815fb157f48f1865219c612d7
Santiago8888 <santiago.marti67@gmail.com>
2021-04-27 14:01:59 -0500

    Document with timeout to advance next.

--- cicero/src/components/Views/Document.tsx
+++ cicero/src/components/Views/Document.tsx
@@ -1,13 +1,34 @@
+import { useEffect } from "react"
+import { iUser } from '../../App'
+
 const defaultDoc = ''
 
-interface iDocument { title:string, link?:string, description:string, next():void }
-export const Document = ({ title, link=defaultDoc, description, next }:iDocument) => <div className="content">
-    <h1> { title } </h1>
-    <p> { description } </p>
+interface iDocument { 
+    user:iUser
+    title:string
+    link?:string
+    description:string
+    min?:number
+    next():void
+    approve():boolean | void 
+}
 
-    <a target='_blank' rel='noreferrer' href={link} style={{padding:0}} className='button'>
-        Leer
-    </a>
 
-    <a className='button' onClick={next}> Siguiente </a>
-</div>
+export const Document = ({ user, title, link=defaultDoc, description, min, next, approve }:iDocument) => {
+    useEffect(() => { setTimeout(() => approve(), 1000*(min||10)) }, [])
+
+    return <div className="content">
+        <h1> { title } </h1>
+        <p> { description } </p>
+
+        <a target='_blank' rel='noreferrer' href={link} style={{padding:0}} className='button'>
+            Leer
+        </a>
+
+        <a 
+            className='button' 
+            onClick={next}
+            hidden={user.current.module > user.progress.module && user.progress.lesson > user.current.lesson}
+        > Siguiente </a>
+    </div>
+}
