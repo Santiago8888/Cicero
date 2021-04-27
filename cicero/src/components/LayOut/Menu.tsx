2c62b4733f2b7dd4d73b3149c46616d35c37a5aa
Santiago8888 <santiago.marti67@gmail.com>
2021-04-27 00:19:06 -0500

    Add current & active module to Menu.

--- cicero/src/components/LayOut/Menu.tsx
+++ cicero/src/components/LayOut/Menu.tsx
@@ -1,4 +1,5 @@
 import { iQuestion } from "../Views/Quiz"
+import { useState } from "react"
 
 type Lesson = 'Video' | 'Quiz' | 'Reading'
 export interface iLesson { 
@@ -11,26 +12,29 @@ export interface iLesson {
 }
 
 export interface iModule { title:string, locked:boolean, lessons:iLesson[] }
-interface iMenu { modules:iModule[] }
+interface iMenu { modules:iModule[], current:number }
+export const Menu = ({ modules, current }: iMenu) => {
+    const [active, setActive] = useState(current)
 
-export const Menu = ({ modules }: iMenu) => <aside className="menu">
-    {
-        modules.map(({ title, lessons }) => 
-            <>
-                <p className="menu-label"> { title } </p>
-                <ul className="menu-list">
-                    {
-                        lessons.map(({ title, type }) =>
+    return <aside className="menu">
+        {
+            modules.map(({ title, lessons, locked }, idx) => 
+                <>
+                    <a className="menu-label" onClick={() => setActive(!locked ? idx : active)}> { title } </a>
+                    <ul className="menu-list" style={{display: active ? 'initial' : 'none' }}>
+                        {
+                            lessons.map(({ title, type }) =>
                                 <li>
                                     <a>
                                         <strong> {type}: </strong> 
                                         { title } 
                                     </a>
                                 </li>
-                        )
-                    }
-                </ul>
-            </>
-        )
-    }
-</aside>
+                            )
+                        }
+                    </ul>
+                </>
+            )
+        }
+    </aside>
+}
