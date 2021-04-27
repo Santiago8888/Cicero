22bdc2062d16f41375a9ca92d1e2245e7f60764f
Santiago8888 <santiago.marti67@gmail.com>
2021-04-27 12:04:01 -0500

    Approve Quiz Method.

--- cicero/src/App.tsx
+++ cicero/src/App.tsx
@@ -5,7 +5,7 @@ import { iLoginInput } from './components/Auth/Login'
 import { iForum } from './components/Forum/Forum'
 import { Home } from './components/Home'
 
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import 'bulma/css/bulma.css'
 import './App.css'
@@ -47,18 +47,27 @@ export const App = () => {
         setLogin(true)
     }
 
+    useEffect(() => {
+        if(current.lesson > user.progress.lesson || current.module > user.progress.module) setUser({...user, progress:current})
+    }, [current])
+
     const next = () => {
         if (modules[current.module].lessons[current.lesson+1]) {
             const newPosition = { module:current.module, lesson:current.lesson+1 }
             setCurrent(newPosition)
-            setUser({...user, progress:newPosition})
+            
 
         } else if (modules[current.module + 1]) {
             const newPosition = { module:current.module+1, lesson:0 }
             setCurrent(newPosition)
-            setUser({...user, progress:newPosition})
 
         } else alert('Congratulations')
+    }
+
+    const nextLesson = ({module, lesson}:iPosition) => {
+        if (modules[module].lessons[lesson+1]) return { module:module, lesson:lesson+1 }
+        else if (modules[current.module + 1]) return { module: module+1, lesson:0 }
+        else return { module, lesson }
     }
 
     const navigate = ({module, lesson}:iPosition) => {
@@ -68,9 +77,31 @@ export const App = () => {
         setCurrent({ module, lesson })
     }
 
+    const approveQuiz = (score:number) => {
+        if(!lesson.questions?.length) return false 
+
+        const minScore = lesson.minScore || lesson.questions.length*.7
+        const isPassing = user.progress.lesson === current.lesson && user.progress.module === current.module
+
+        if(isPassing && score >= minScore) {
+            setUser({...user, progress:nextLesson(user.progress), quizFailures:0})
+            return { success:true, message: '' }
+
+        } else if(isPassing && user.quizFailures > 1) {
+            setUser({...user, progress:{module:user.progress.module, lesson:0}})
+            return { success:false, message: '' }
+
+        } else if(isPassing)  { 
+            setUser({...user, quizFailures:1})
+            return { success:false, message: '' }
+
+        } else if(score >= minScore) return { sucess:true , message:'' }
+        else return { sucess:true , message:'' }
+    }
+
     return <div className="App">
         <NavBar click={(item) => clickNavbar(item)}/>
         <Menu modules={modules} current={current} navigate={navigate} user={user}/>
-        <Home {...homeData} isAuth={isAuth} isLogin={isLogin} login={login} next={next}/>
+        <Home {...homeData} isAuth={isAuth} isLogin={isLogin} login={login} next={next} />
     </div>
 }
