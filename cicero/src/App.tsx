4485b3ad2cbcca9a09b073b570358db53598b800
Santiago8888 <santiago.marti67@gmail.com>
2021-04-27 00:17:13 -0500

    Started Module Data Feeding.

--- cicero/src/App.tsx
+++ cicero/src/App.tsx
@@ -1,6 +1,6 @@
+import { iLesson, iModule, Menu } from './components/LayOut/Menu'
 import { NavBar, NavbarItem } from './components/LayOut/NavBar'
 import { iRecordings } from './components/Forum/Recordings'
-import { iLesson, Menu } from './components/LayOut/Menu'
 import { iLoginInput } from './components/Auth/Login'
 import { iForum } from './components/Forum/Forum'
 import { Home } from './components/Home'
@@ -29,12 +29,22 @@ const recordings:iRecordings = {
     recordings:[]
 }
 
+const Modules:iModule[] = [
+    { title:'Modulo 1', locked:false, lessons:[]},
+    { title:'Módulo 2', locked:true, lessons:[]},
+    { title:'Módulo 3', locked:false, lessons:[]},
+    { title:'Módulo 4', locked:false, lessons:[]},
+    { title:'Módulo 5', locked:false, lessons:[]},
+    { title:'Módulo 6', locked:false, lessons:[]}
+]
+
 interface iHomeData { forum?:iForum, recordings?:iRecordings, lesson:iLesson}
 const initialData:iHomeData = { forum:undefined, recordings:undefined, lesson }
 export const App = () => {
     const [isAuth, setAuth] = useState(false)
     const [isLogin, setLogin] = useState(false)
     const [homeData, setHomeData] = useState<iHomeData>(initialData)
+    const [modules, setModules] = useState(Modules)
 
     const clickNavbar = (item:NavbarItem) => {
         if(item === 'Forum') return setHomeData({...homeData, forum, recordings:undefined})
@@ -51,7 +61,7 @@ export const App = () => {
 
     return <div className="App">
         <NavBar click={(item) => clickNavbar(item)}/>
-        <Menu modules={[]}/>
+        <Menu modules={modules} current={0}/>
         <Home {...homeData} isAuth={isAuth} isLogin={isLogin} login={login}/>
     </div>
 }
