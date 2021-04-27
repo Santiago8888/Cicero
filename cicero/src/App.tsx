e3cb6f4aa5e82f7b5df6410e6f2923ec16559761
Santiago8888 <santiago.marti67@gmail.com>
2021-04-26 19:04:52 -0500

    Add Login State to App.

--- cicero/src/App.tsx
+++ cicero/src/App.tsx
@@ -1,11 +1,11 @@
 
-import { NavBar } from './components/LayOut/NavBar'
 import { iLesson, Menu } from './components/LayOut/Menu'
+import { NavBar } from './components/LayOut/NavBar'
 import { Home } from './components/Home'
+import { useState } from 'react'
 
 import 'bulma/css/bulma.css'
 import './App.css'
-import { useState } from 'react'
 
 
 const lesson: iLesson = {
@@ -19,15 +19,16 @@ const data = {
     forum:undefined,
     recordings:undefined,
     lesson
-
 }
 
 export const App = () => {
     const [isAuth, setAuth] = useState(false)
+    const [isLogin, setLogin] = useState(false)
+
 
     return <div className="App">
         <NavBar />
         <Menu modules={[]}/>
-        <Home {...data} isAuth={isAuth}/>
+        <Home {...data} isAuth={isAuth} isLogin={isLogin}/>
     </div>
 }
