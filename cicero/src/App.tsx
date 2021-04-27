7f943ebda8ff458ef22a0da61dd1d295144fe701
Santiago8888 <santiago.marti67@gmail.com>
2021-04-27 00:09:18 -0500

    Add listeners for forum & recordings on navbar.

--- cicero/src/App.tsx
+++ cicero/src/App.tsx
@@ -1,6 +1,8 @@
 import { NavBar, NavbarItem } from './components/LayOut/NavBar'
+import { iRecordings } from './components/Forum/Recordings'
 import { iLesson, Menu } from './components/LayOut/Menu'
 import { iLoginInput } from './components/Auth/Login'
+import { iForum } from './components/Forum/Forum'
 import { Home } from './components/Home'
 import { useState } from 'react'
 
@@ -15,20 +17,29 @@ const lesson: iLesson = {
     locked:false    
 }
 
-const data = {
-    forum:undefined,
-    recordings:undefined,
-    lesson
+const forum:iForum = {
+    title:'',
+    description:'',
+    questions:[]    
 }
 
+const recordings:iRecordings = {
+    title:'',
+    description:'',
+    recordings:[]
+}
+
+interface iHomeData { forum?:iForum, recordings?:iRecordings, lesson:iLesson}
+const initialData:iHomeData = { forum:undefined, recordings:undefined, lesson }
 export const App = () => {
     const [isAuth, setAuth] = useState(false)
     const [isLogin, setLogin] = useState(false)
+    const [homeData, setHomeData] = useState<iHomeData>(initialData)
 
     const clickNavbar = (item:NavbarItem) => {
-        if(item === 'Forum') return
-        if(item === 'Login') setLogin(true)
-        if(item === 'Recordings') return
+        if(item === 'Forum') return setHomeData({...homeData, forum, recordings:undefined})
+        if(item === 'Login') return setLogin(true)
+        if(item === 'Recordings') return setHomeData({...homeData, forum:undefined, recordings})
     }
     
     const login = ({ email, password }:iLoginInput) => {
@@ -41,6 +52,6 @@ export const App = () => {
     return <div className="App">
         <NavBar click={(item) => clickNavbar(item)}/>
         <Menu modules={[]}/>
-        <Home {...data} isAuth={isAuth} isLogin={isLogin} login={login}/>
+        <Home {...homeData} isAuth={isAuth} isLogin={isLogin} login={login}/>
     </div>
 }
