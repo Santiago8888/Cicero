e9a8a91269301dd5943899a7bda4c3101099a63c
Santiago8888 <santiago.marti67@gmail.com>
2021-04-28 17:36:38 -0500

    Import Style from Forum.

--- cicero/src/components/Forum/Recordings.tsx
+++ cicero/src/components/Forum/Recordings.tsx
@@ -1,10 +1,6 @@
-import { cardStyle, headerStyle } from "./Forum"
-
 interface iRecording { title:string, link:string, description:string }
-const Recording = ({ title, link, description }: iRecording) => <div className='card' style={cardStyle}>
-    <header className='card-header' style={headerStyle}>
-        <p className='card-header-title' style={{color:'white', fontSize:'1.25rem'}}> { title } </p>
-    </header>
+const Recording = ({ title, link, description }: iRecording) => <div style={{maxWidth:800, textAlign:'left', margin:'auto', marginBottom:'1.5em'}}>
+    <p style={{color:'darkblue', fontSize:'1.25rem', fontWeight:600, marginBottom:0}}> { title } </p>
 
     <article className='media' style={{marginBottom:0}}>
         <figure className='media-left' style={{width:'40%', height:256}}>
@@ -13,7 +9,7 @@ const Recording = ({ title, link, description }: iRecording) => <div className='
 
         <div className='media-content' style={{paddingBottom:'0.5rem', paddingRight:'1rem'}}>
             <div className='content'>
-                <div className='content' style={{color:'whitesmoke', marginTop:'1rem'}}> 
+                <div className='content' style={{color:'gray', marginTop:'1rem'}}> 
                     { description } 
                 </div>
             </div>
@@ -24,8 +20,20 @@ const Recording = ({ title, link, description }: iRecording) => <div className='
 
 export interface iRecordings { title:string, description:string, recordings:iRecording[] }
 export const Recordings = ({ title, description, recordings}: iRecordings) => <div className="content">
-    <h1> { title } </h1>
-    <p> { description } </p> 
+    <h1 style={{fontSize:'3rem', marginBottom:'2rem', color:'darkblue'}}> { title } </h1>
+    <h3 
+        style={{
+            margin:'0rem auto',
+            color: '#333',
+            fontSize: '1.25em',
+            textAlign: 'left',
+            fontWeight: 500,
+            width: 800        
+        }}
+    > { description } </h3>
+
+    <hr style={{ backgroundColor:'darkblue', margin:'1.5rem auto 3rem', width:600 }}/>
+
 
     { recordings.map((r, i)=> <Recording {...r} key={i}/>) }
 </div>
