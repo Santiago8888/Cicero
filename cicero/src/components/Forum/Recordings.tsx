9d9a3886861ee5bf9e500ac6a89c12b9c8fd3a3e
Santiago8888 <santiago.marti67@gmail.com>
2021-04-28 17:44:34 -0500

    Style on Recordings List and Videos iframe.

--- cicero/src/components/Forum/Recordings.tsx
+++ cicero/src/components/Forum/Recordings.tsx
@@ -1,13 +1,13 @@
+import Vimeo from '@u-wave/react-vimeo'
+
 interface iRecording { title:string, link:string, description:string }
 const Recording = ({ title, link, description }: iRecording) => <div style={{maxWidth:800, textAlign:'left', margin:'auto', marginBottom:'1.5em'}}>
     <p style={{color:'darkblue', fontSize:'1.25rem', fontWeight:600, marginBottom:0}}> { title } </p>
 
     <article className='media' style={{marginBottom:0}}>
-        <figure className='media-left' style={{width:'40%', height:256}}>
-            <iframe src={ link } width="600" height="338" frameBorder="0" allowFullScreen/>
-        </figure>
+        <Vimeo video={link} width={400} height={200}/>
 
-        <div className='media-content' style={{paddingBottom:'0.5rem', paddingRight:'1rem'}}>
+        <div className='media-content' style={{paddingBottom:'0.5rem', paddingRight:'1rem', marginLeft:'1rem'}}>
             <div className='content'>
                 <div className='content' style={{color:'gray', marginTop:'1rem'}}> 
                     { description } 
