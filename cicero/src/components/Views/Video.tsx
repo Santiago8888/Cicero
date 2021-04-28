343f2c95b80d7537beb3085c28c420767a9678e0
Santiago8888 <santiago.marti67@gmail.com>
2021-04-28 15:47:46 -0500

    Video view Styled.

--- cicero/src/components/Views/Video.tsx
+++ cicero/src/components/Views/Video.tsx
@@ -4,13 +4,24 @@ import { iUser } from '../../App'
 const defaultVideo = ''
 interface iVideo { user:iUser, title:string, link?:string, description:string, next():void, approve():boolean | void}
 export const Video = ({ user, title, link=defaultVideo, description, next, approve }: iVideo) => <div className="content">
-    <h1> { title } </h1>
-    <p> { description } </p>
+    <h1 style={{fontSize:'3rem', marginBottom:'2rem', color:'darkblue'}}> { title } </h1>
+    <h3 
+        style={{
+            margin:'0rem auto',
+            color: '#333',
+            fontSize: '1.25em',
+            textAlign: 'left',
+            fontWeight: 500,
+            width: 800        
+        }}> { description } </h3>
 
-    <Vimeo video={link} onEnd={approve}/>
-    <a 
-        className='button' 
-        onClick={next} 
-        hidden={user.current.module > user.progress.module && user.progress.lesson > user.current.lesson}
-    > Siguiente </a>
+    <Vimeo video={link} onEnd={approve} width={800} height={400}/>
+    <div style={{ marginTop:'3rem', width:800, margin:'auto'}}>
+        <a 
+            onClick={next} 
+            className='button is-link' 
+            style={{float:'right', borderRadius:12, width:180, fontSize:'1.25rem', fontWeight:600, backgroundColor:'darkblue'}}
+            hidden={user.current.module > user.progress.module && user.progress.lesson > user.current.lesson}
+        > CONTINUAR </a>
+    </div>
 </div>
