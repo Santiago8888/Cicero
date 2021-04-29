61a0b1fec27a282007e2eb0a576b76384e18ffe3
Santiago8888 <santiago.marti67@gmail.com>
2021-04-29 17:58:09 -0500

    Fixed Styling on Document.

--- cicero/src/components/Views/Document.tsx
+++ cicero/src/components/Views/Document.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { iUser } from '../../App'
 
 const defaultDoc = ''
@@ -14,16 +15,19 @@ interface iDocument {
 
 
 export const Document = ({ user, title, link=defaultDoc, description, min, next, approve }:iDocument) => {
-    const initCountdown = () => setTimeout(() => approve(), 1000*60*(min || 10))
+    const [ isCounting, setCounting ] = useState(false)
+    const initCountdown = () => {
+        setCounting(true)
+        setTimeout(() => approve(), 1000*60*(min || 10))
+    }
 
     return <div className="content">
         <h1 style={{fontSize:'3rem', marginBottom:'2rem', color:'darkblue'}}> { title } </h1>
         <h3 
             style={{
                 margin:'0rem auto',
-                color: '#333',
+                color: '#444',
                 fontSize: '1.25em',
-                textAlign: 'left',
                 fontWeight: 500,
                 width: 800        
             }}
@@ -31,30 +35,39 @@ export const Document = ({ user, title, link=defaultDoc, description, min, next,
 
 
         <div style={{ width:800, margin:'3rem auto 1rem'}}>
-            <a 
-                onClick={initCountdown}
-                target='_blank' 
-                rel='noreferrer' 
-                href={link} 
-                className='button is-link'
-                style={{
-                    borderRadius:12, 
-                    width:180, 
-                    fontSize:'1.25rem', 
-                    fontWeight:600, 
-                    marginLeft:90,
-                    backgroundColor:'darkblue'
-                }}
-            >
-                Leer
-            </a>
-
-            <button 
-                onClick={next} 
-                className='button is-link' 
-                style={{float:'right', borderRadius:12, width:180, fontSize:'1.25rem', fontWeight:600, backgroundColor:'darkblue'}}
-                disabled={user.current.module === user.progress.module && user.progress.lesson === user.current.lesson}
-            > CONTINUAR </button>
+            {
+                user.current.module < user.progress.module 
+                || (user.progress.lesson < user.current.lesson && user.current.module === user.progress.module)
+                || !isCounting
+                ?
+                    <a 
+                        onClick={initCountdown}
+                        target='_blank'
+                        rel='noreferrer'
+                        href={link}
+                        className='button is-link'
+                        style={{
+                            borderRadius:12,
+                            width:180,
+                            fontSize:'1.25rem',
+                            fontWeight:600,
+                            backgroundColor:'darkblue'
+                        }}
+                    > Leer </a>
+                :
+                    <button 
+                        onClick={next}
+                        className='button is-link'
+                        style={{
+                            borderRadius:12, 
+                            width:180, 
+                            fontSize:'1.25rem', 
+                            fontWeight:600, 
+                            backgroundColor:'darkblue'
+                        }}
+                        disabled={user.current.module === user.progress.module && user.progress.lesson === user.current.lesson}
+                    > CONTINUAR </button>
+            }
         </div>
     </div>
 }
