040fcdfe461b66cfae21173d4fd95f82e8d17e35
Santiago8888 <santiago.marti67@gmail.com>
2021-04-30 14:39:16 -0500

    Responsiveness on landing page.

--- cicero/src/components/Auth/Landing.tsx
+++ cicero/src/components/Auth/Landing.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
+import { useMediaQuery } from 'react-responsive'
 import { iLoginInput, Login } from './Login'
 import Vimeo from '@u-wave/react-vimeo'
 import { Billing } from './Billing'
@@ -7,22 +8,25 @@ import { useState } from "react"
 import { User } from 'realm-web'
 
 interface iWelcome { subscribe():void }
-const Welcome = ({ subscribe }:iWelcome) => <div className="content" style={{textAlign:'center'}}>
-    <h1 style={{fontSize:'3rem', marginBottom:'2rem', color:'darkblue'}}> ASTROCONSCIENCIA </h1>
+const Welcome = ({ subscribe }:iWelcome) => {
+    const midScreen = useMediaQuery({ query: '(min-width: 900px)' })
+    return <div className="content" style={{textAlign:'center'}}>
+        <h1 style={{fontSize:'3rem', marginBottom:'2rem', color:'darkblue'}}> ASTROCONSCIENCIA </h1>
 
-    <Vimeo video={'539430817'} width={800} height={400}/>
+        <Vimeo video={'539430817'} width={midScreen ? 800 : 400} height={midScreen ? 400 : 300}/>
 
-    <div style={{marginTop:'3rem'}}>
-        <h2 style={{marginBottom:'2rem', marginRight:'auto', marginLeft:'auto', width:600, color:'navy'}}> 
-            Escucha el llamado de tu alma en las estrellas, los planetas y tu interior. 
-        </h2>
-        <a 
-            onClick={subscribe} 
-            className='button is-link' 
-            style={{ width:460, fontSize: '2rem', fontWeight:900, backgroundColor:'mediumblue', borderRadius:20 }}
-        > INICIA TU CAMNINO </a>
+        <div style={{marginTop:'3rem'}}>
+            <h2 style={{marginBottom:'2rem', marginRight:'auto', marginLeft:'auto', width:600, color:'navy'}}>
+                Escucha el llamado de tu alma en las estrellas, los planetas y tu interior.
+            </h2>
+            <a
+                onClick={subscribe}
+                className='button is-link'
+                style={{ width:460, fontSize: '2rem', fontWeight:900, backgroundColor:'mediumblue', borderRadius:20 }}
+            > INICIA TU CAMNINO </a>
+        </div>
     </div>
-</div>
+}
 
 
 export interface iLanding { mongoUser?: User, createUser(loginInput:iLoginInput):void }
