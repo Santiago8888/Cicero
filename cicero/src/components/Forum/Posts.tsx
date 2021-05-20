07d8a842b67c4578cfb1a3521ead57affe6da90a
Santiago8888 <santiago.marti67@gmail.com>
2021-05-20 11:02:27 -0500

    Admit props on Post.

--- cicero/src/components/Forum/Posts.tsx
+++ cicero/src/components/Forum/Posts.tsx
@@ -1,74 +1,61 @@
-export const Post = () => <nav className="panel">
-    <p className="panel-heading"> Repositories </p>
+import { useEffect, useState } from "react"
 
-    <div className="panel-block">
+interface iPost { title:string, detail:string, likes:number, comments:string[], submit(reply:string):void }
+export const Post = ({ title, detail, comments, submit }: iPost) => {
+    const [value, setValue] = useState('')
+    const [replies, setReplies] = useState<string[]>([])
 
-        <p className="control has-icons-left">
-            <input className="input" type="text" placeholder="Search" />
-            <span className="icon is-left">
-                <i className="fas fa-search" aria-hidden="true"></i>
-            </span>
-        </p>
-    </div>
+    useEffect(() => {
+        const recentComments = comments.filter((_, i) => i < 5)
+        setReplies(recentComments)
+    }, [comments])
 
-    <p className="panel-tabs">
-        <a className="is-active">All</a>
-        <a>Public</a>
-        <a>Private</a>
-        <a>Sources</a>
-        <a>Forks</a>
-    </p>
+    return <nav className="panel">
+        <p className="panel-heading"> { title } </p>
+        <div className="panel-block">
+            { detail }
+        </div>
 
-    <a className="panel-block is-active">
-        <span className="panel-icon">
-            <i className="fas fa-book" aria-hidden="true"></i>
-        </span>
-        bulma
-    </a>
+        <div className="panel-block">
+            <div className="field has-addons">
+                <div className="control">
+                    <input 
+                        type="text" 
+                        className="input" 
+                        placeholder="Comentar"
+                        onChange={({target:{value}}) => setValue(value)}                        
+                        onKeyPress={({ key }) => key === 'Enter' ? submit(value) : null}
+                    />
+                </div>
 
-    <a className="panel-block">
-        <span className="panel-icon">
-            <i className="fas fa-book" aria-hidden="true"></i>
-        </span>
-        marksheet
-    </a>
+                <div className="control">
+                    <a className="button is-info" onClick={() => submit('text')}> 
+                        <svg 
+                            xmlns="http://www.w3.org/2000/svg" 
+                            viewBox="0 0 24 24" 
+                            data-supported-dps="24x24" 
+                            fill="white" 
+                            className="mercado-match" 
+                            width="24" 
+                            height="24" 
+                            focusable="false"
+                        ><path d="M21 3L0 10l7.66 4.26L16 8l-6.26 8.34L14 24l7-21z"></path></svg>
+                    </a>
+                </div>
+            </div>
+        </div>
 
-    <a className="panel-block">
-        <span className="panel-icon">
-            <i className="fas fa-book" aria-hidden="true"></i>
-        </span>
-        minireset.css
-    </a>
+        { replies.map(comment => <a className="panel-block is-active"> { comment } </a> ) }
 
-    <a className="panel-block">
-        <span className="panel-icon">
-            <i className="fas fa-book" aria-hidden="true"></i>
-        </span>
-        jgthms.github.io
-    </a>
 
-    <a className="panel-block">
-        <span className="panel-icon">
-            <i className="fas fa-code-branch" aria-hidden="true"></i>
-        </span>
-        daniellowtw/infboard
-    </a>
-
-    <a className="panel-block">
-        <span className="panel-icon">
-            <i className="fas fa-code-branch" aria-hidden="true"></i>
-        </span>
-        mojs
-    </a>
-
-    <label className="panel-block">
-        <input type="checkbox" />
-        remember me
-    </label>
-
-    <div className="panel-block">
-        <button className="button is-link is-outlined is-fullwidth">
-            Reset all filters
-        </button>
-    </div>
-</nav>
+        <div 
+            className="panel-block" 
+            onClick={() => setReplies(comments)} 
+            style={{display:comments.length > replies.length ? 'initial' : 'none' }}
+        >
+            <button className="button is-link is-outlined is-fullwidth">
+                Mostrar Todos Los Comentarios
+            </button>
+        </div>
+    </nav>
+}
