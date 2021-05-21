004a7b7fecd91273897271df3cfbbea1ca9c1580
Santiago8888 <santiago.marti67@gmail.com>
2021-05-21 18:17:44 -0500

    Filter planets on Astral Chart.

--- cicero/src/components/Views/Chart.tsx
+++ cicero/src/components/Views/Chart.tsx
@@ -1,11 +1,11 @@
-import { AstralChart } from '../Astral/AstralChart'
-import { houses, planets } from '../../data/chart'
+import { houses, planets as planetsData } from '../../data/chart'
+import { AstralChart, Planet } from '../Astral/AstralChart'
 import { useMediaQuery } from 'react-responsive'
 import { iUser } from '../../App'
 
 
-interface iChart { user:iUser, title:string, description:string, next():void }
-export const Chart = ({ user, title, description, next }: iChart) => {
+interface iChart { user:iUser, title:string, description:string, planets?:Planet[], next():void }
+export const Chart = ({ user, title, description, planets=[], next }: iChart) => {
     const midScreen = useMediaQuery({ query: '(min-width: 900px)' })
     const smallScreen = useMediaQuery({ query: '(max-width: 600px)' })
 
@@ -22,14 +22,16 @@ export const Chart = ({ user, title, description, next }: iChart) => {
             }}
         > { description } </h3>
 
-        <AstralChart planets={planets} houses={houses}/>
+        <AstralChart 
+            planets={planets?.length ? planetsData.filter(({ name }) => planets?.includes(name)) : planetsData} 
+            houses={houses}
+        />
 
         <div style={{ marginTop:'3rem', width: midScreen ? 800 : 320, margin:'auto'}}>
             <button
                 onClick={next} 
                 className='button is-link' 
                 style={{
-                    float: !midScreen ? 'inherit' : 'right', 
                     borderRadius:12, 
                     width:180, 
                     fontSize:'1.25rem', 
