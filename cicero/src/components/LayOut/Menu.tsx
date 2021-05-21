dd304250d44f2fb6b814285ec42f5285413ccb8e
Santiago8888 <santiago.marti67@gmail.com>
2021-05-21 16:09:34 -0500

    Menu logic with units.

--- cicero/src/components/LayOut/Menu.tsx
+++ cicero/src/components/LayOut/Menu.tsx
@@ -1,11 +1,11 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
 import { iRecordings } from "../Forum/Recordings"
-import { useEffect, useState } from "react"
 import { iQuestion } from "../Views/Quiz"
 import { iForum } from "../Forum/Forum"
 import { iPost } from "../Forum/Posts"
 import { iUser } from '../../App'
+import { useState } from "react"
 
 
 type Lesson = 'Video' | 'Quiz' | 'Reading'
@@ -39,51 +39,80 @@ interface iMenu {
 
 
 export const Menu = ({ units, navigate, user, forum, posts, recordings }: iMenu) => {
-    const [active, setActive] = useState<number>(user?.current.module || 0)
+    const [active, setActive] = useState({ unit:user?.current.unit, module: user?.current.module})
+    const [selectedUnit, setSelected ] = useState<number | undefined>()
 
-    const expand = (id:number) => {
+    const expand = (unit:number, module:number) => {
         if(!user) return 
-        if(user.progress.module < id) return
-        setActive( id === active ? user.current.module : id)
-    }
+        if(user.progress.unit < unit) return
+        if(user.progress.unit === unit && user.progress.module < module) return
 
-    useEffect(() => { setActive(user?.current.module as number) },[user])
+        const isActiveClicked = active.unit === unit && active.module === module
+        setActive(!isActiveClicked ? { module, unit } : { unit: undefined, module: undefined }) 
+    }
 
     return <aside 
         className="menu column is-2 is-narrow-mobile is-fullheight section is-hidden-mobile"
         style={{ minHeight:'calc(100vh - 85px)', width:250, boxShadow: '3px 0 3px 0 #ccc', fontSize:'1.15em' }}
     >
         {
-            units.map(({ title, modules }, k) => <>
-                <p className="menu-label"> { title } </p>
+            units.map(({ title, modules }, u) => <>
+                { 
+                    user && u <= user.progress.unit 
+                    ? <a className="menu-label" onClick={() => setSelected(u !== selectedUnit ? u : undefined)}> { title } </a> 
+                    : <p className="menu-label"> { title } </p> 
+                }
+
                 <ul className="menu-list">
-                    { modules.map(({ title, lessons }, idx) => 
-                        <li style={{lineHeight:2}} key={idx}>
-                            <a onClick={() => expand(idx)} style={!user ? {cursor:'initial'} : {}}>
-                                { (!user || user.progress.module < idx) && <Lock/> }
+                    { (user?.current.unit  === u || selectedUnit === u) && modules.map(({ title, lessons }, m) => 
+                        <li style={{lineHeight:2}} key={m}>
+                            <a 
+                                onClick={() => expand(u, m)} 
+                                style={!user || (user.progress.unit === u && user.progress.module < m) ? {cursor:'initial'} : {}}
+                            >
+                                { (!user || (user.progress.unit === u && user.progress.module < m)) && <Lock/> }
                                 { title } 
                             </a>
                             {
-                                (active === idx || user?.current.module === idx || (!user && idx === 0)) && lessons.length
+                                (
+                                    (u === active.unit && active.module === m) 
+                                    || (user?.current.unit === u && user?.current.module === m) 
+                                    || (!user && m === 0)
+                                ) && lessons.length
                                 ?   <ul>
-                                        { lessons.map(({ title }, i) => 
-                                            <li style={{lineHeight:1.25}} key={i}>
+                                        { lessons.map(({ title }, l) => 
+                                            <li style={{lineHeight:1.25}} key={l}>
                                                 <a
                                                     style={
-                                                        user?.current.lesson === i && user?.current.module === idx 
+                                                        user?.current.unit === u  
+                                                        && user?.current.lesson === l
+                                                        && user?.current.module === m 
                                                             ?   {
                                                                     backgroundColor: !forum && !recordings && !posts ? 'darkblue' : 'lightblue', 
                                                                     borderRadius:8
                                                                 }
-                                                            : !user || (idx === user?.progress.module && i > user?.progress.lesson) 
+                                                            : !user || (
+                                                                user?.progress.unit === u 
+                                                                && m === user?.progress.module 
+                                                                && l > user?.progress.lesson
+                                                            ) 
                                                                 ? {cursor:'initial'} 
                                                                 : {}
                                                     }
-                                                    className={`${user?.current.lesson === i  && user?.current.module === idx ? 'is-active': ''}`}
-                                                    onClick={() => navigate({unit:k, module:active, lesson:i})}
+                                                    className={
+                                                        user?.current.unit === u  
+                                                        && user?.current.lesson === l
+                                                        && user?.current.module === m 
+                                                        ? 'is-active': ''
+                                                    }
+                                                    onClick={() => navigate({unit:u, module:m, lesson:l})}
                                                 > 
-                                                    { (idx === user?.progress.module && i > user?.progress.lesson) && <Lock/> }
-                                                    { title } 
+                                                    {   (
+                                                            u === user?.progress.unit
+                                                            && m === user?.progress.module 
+                                                            && l > user?.progress.lesson
+                                                        ) && <Lock/> 
+                                                    }   { title }
                                                 </a>
                                             </li>
                                         )}
