4da7645d769214fe2f2c4ddc4765be267c49bcac
Santiago8888 <santiago.marti67@gmail.com>
2021-05-20 19:49:58 -0500

    Sample Data for Posts.

--- cicero/src/components/Interaction.tsx
+++ cicero/src/components/Interaction.tsx
@@ -1,7 +1,7 @@
 import { Recordings, iRecordings } from './Forum/Recordings'
 import { Forum, iDoubt, iForum } from './Forum/Forum'
+import { Posts, iPost } from './Forum/Posts'
 
-import { iPost } from '../App'
 import { Error } from './Error'
 
 
@@ -12,10 +12,16 @@ interface iIteraction {
     submit(doubt:iDoubt):void
 }
 
+const posts:iPost[] = [{
+    title:'Title', 
+    detail:'Detail', 
+    comments:['Test', 'Tost', 'Tust', 'Tast', 'Cast', 'Age']
+}]
+
 export const Iteraction = ({ forum, recordings, questions, submit }: iIteraction) => {
 
     return  forum ? <Forum {...forum} submit={submit}/>
         :   recordings ? <Recordings {...recordings}/>
-        :   questions ?  <></>
+        :   questions ?  <Posts posts={posts} submit={() => {}} reply={() => {}} like={() => {}}/>
     : <Error/>
 }
