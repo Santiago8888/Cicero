02356ace8d37ef3a79f2345090de99335b38eca9
Santiago8888 <santiago.marti67@gmail.com>
2021-05-21 16:01:25 -0500

    Add lessons & deleted modules.

--- cicero/src/data/data.ts
+++ cicero/src/data/data.ts
@@ -1,4 +1,4 @@
-import { iLesson, iModule, iUnit } from "../components/LayOut/Menu"
+import { iLesson, iUnit } from "../components/LayOut/Menu"
 import { iRecordings } from "../components/Forum/Recordings"
 import { iForum } from "../components/Forum/Forum"
 import { iPost } from "../components/Forum/Posts"
@@ -6,7 +6,7 @@ import { iUser } from "../App"
 
 
 export const lesson: iLesson = { 
-    title:'El Sol y los Signos', 
+    title:'Quiz', 
     description:'El signo solar indica el caracter, la escencia y misión de la persona.', 
     type:'Quiz', 
     questions:[{
@@ -63,19 +63,15 @@ quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequa
 Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
 
-const lessons:iLesson[] = [{...lesson, type:'Video'}, {...lesson, type:'Reading', description:longDescription, link:'sample.pdf'}, lesson]
-export const modules:iModule[] = [
-    { title:'Modulo 1', lessons},
-    { title:'Módulo 2', lessons},
-    { title:'Módulo 3', lessons},
-    { title:'Módulo 4', lessons},
-    { title:'Módulo 5', lessons},
-    { title:'Módulo 6', lessons}
+const lessons:iLesson[] = [
+    {...lesson, type:'Video', title:'Video'}, 
+    {...lesson, type:'Reading', title:'Lectura', description:longDescription, link:'sample.pdf'}, 
+    lesson
 ]
 
 export const defaultUser:iUser = { 
     email:'test@branding.gq', 
-    progress:{unit:3, module:3, lesson:1}, 
+    progress:{unit:3, module:1, lesson:0}, 
     current:{unit:0, module:0, lesson:2}, 
     quizFailures:0 
 }
@@ -93,47 +89,47 @@ export const Posts:iPost[] = [{
 export const Units:iUnit[] = [{ 
     title: '1. ASTROCONCIENCIA', 
     modules: [
-        { title: '1.1 Introducción a la astroconsciencia', lessons:[] },
-        { title: '1.2 Historia de la astrología', lessons:[] },
-        { title: '1.3 Proposito de vida desde la Astronciencia', lessons:[] }
+        { title: '1.1 Introducción a la astroconsciencia', lessons },
+        { title: '1.2 Historia de la astrología', lessons },
+        { title: '1.3 Proposito de vida desde la Astronciencia', lessons }
     ]
 }, { 
     title: '2. LOS 12 SIGNOS DEL ZODIACO', 
     modules: [
-        { title: '2.1 Características, elementos y cualidades de los signos', lessons:[] },
-        { title: '2.2 Los 12 signos', lessons:[] }
+        { title: '2.1 Características, elementos y cualidades de los signos', lessons },
+        { title: '2.2 Los 12 signos', lessons }
     ] 
 }, { 
     title: '3. LAS 12 CASAS DE LA CARTA ASTRAL', 
     modules: [
-        { title: '3.1 Estructura de la carta astral', lessons:[] },
-        { title: '3.2 Casas en la carta astral', lessons:[] },
-        { title: '3.3 Ascendentes', lessons:[] },
-        { title: '3.4 Signos en las casas', lessons:[] }
+        { title: '3.1 Estructura de la carta astral', lessons },
+        { title: '3.2 Casas en la carta astral', lessons },
+        { title: '3.3 Ascendentes', lessons },
+        { title: '3.4 Signos en las casas', lessons }
     ] 
 }, { 
     title: '4. ASTROCONCIENCIA PLANETARIA', 
     modules: [
-        { title: '4.1 Planetas', lessons:[] },
-        { title: '4.2 Regencia/Dignidad', lessons:[] },
-        { title: '4.3 Planetas en signos y casas', lessons:[] }
+        { title: '4.1 Planetas', lessons },
+        { title: '4.2 Regencia/Dignidad', lessons },
+        { title: '4.3 Planetas en signos y casas', lessons }
     ] 
 }, { 
     title: '5. ASTROCONCIENCIA KARMICA', 
     modules: [
-        { title: '5.1 Consciencia karmica', lessons:[] },
-        { title: '5.2 Luna', lessons:[] },
-        { title: '5.3 Saturno y Kirón', lessons:[] },
-        { title: '5.4 Casa 12', lessons:[] },
-        { title: '5.5 Nodos lunares', lessons:[] }
+        { title: '5.1 Consciencia karmica', lessons },
+        { title: '5.2 Luna', lessons },
+        { title: '5.3 Saturno y Kirón', lessons },
+        { title: '5.4 Casa 12', lessons },
+        { title: '5.5 Nodos lunares', lessons }
     ] 
 }, { 
     title: '6. MISIONES', 
     modules: [
-        { title: '6.1 Nuestras misiones', lessons:[] },
-        { title: '6.2 Misión personal', lessons:[] },
-        { title: '6.3 Misión conciente', lessons:[] },
-        { title: '6.4 Misión transpersonal', lessons:[] },
-        { title: '6.5 Dones divinos', lessons:[] },
+        { title: '6.1 Nuestras misiones', lessons },
+        { title: '6.2 Misión personal', lessons },
+        { title: '6.3 Misión conciente', lessons },
+        { title: '6.4 Misión transpersonal', lessons },
+        { title: '6.5 Dones divinos', lessons },
     ] 
 }]
