ed9151b6afb9796d5332ab57536dcf34c88088c8
Santiago8888 <santiago.marti67@gmail.com>
2021-05-20 19:50:23 -0500

    Card for Panel, datetime object & Avatar.

--- cicero/src/components/Forum/Posts.tsx
+++ cicero/src/components/Forum/Posts.tsx
@@ -1,7 +1,30 @@
 import { Header, Modal, Likes } from "./Atoms"
 import { useEffect, useState } from "react"
 
-interface iPost { id?:string, title:string, detail:string, likes?:number, comments?:string[] }
+
+const monthDict = (month:number) => ({
+    0: 'Enero',
+    1: 'Febrero',
+    2: 'Marzo',
+    3: 'Abril',
+    4: 'Mayo',
+    5: 'Junio',
+    6: 'Julio',
+    7: 'Agosto',
+    8: 'Septiembre',
+    9: 'Octubre',
+    10: 'Noviembre',
+    11: 'Diciembre'
+}[month])
+
+
+export const DateTime = ({ date }:{ date:Date }) => <i style={{marginBottom:12}}>
+    { date.getHours() % 12 }:{ date.getMinutes() } { date.getHours() > 12 ? 'PM - ' : 'AM - ' }  
+    { date.getDate() } { monthDict(date.getMonth()) } { date.getFullYear() } <br/>
+</i>
+
+
+export interface iPost { id?:string, title:string, detail:string, likes?:number, comments?:string[] }
 interface IPost extends iPost { id:string, reply(text:string, postId:string):void, like(postId:string):void }
 const Post = ({ id, title, detail, likes=0, comments=[], reply, like }: IPost) => {
     const [ value, setValue ] = useState('')
@@ -12,64 +35,63 @@ const Post = ({ id, title, detail, likes=0, comments=[], reply, like }: IPost) =
         setReplies(recentComments)
     }, [comments])
 
-    return <div>
+    return <div style={{display:'flex'}}>
         <Likes likes={likes} like={() => like(id)}/>
-        <nav className="panel">
-            <p className="panel-heading"> { title } </p>
-            <div className="panel-block"> { detail } </div>
 
-            <div className="panel-block">
-                <div className="field has-addons">
-                    <div className="control">
-                        <input 
-                            type="text" 
-                            className="input" 
-                            placeholder="Comentar"
-                            onChange={({target:{value}}) => setValue(value)}                        
-                            onKeyPress={({ key }) => key === 'Enter' ? reply(value, id) : null}
-                        />
+        <div className="card" style={{textAlign:'left', width:'100%'}}>
+            <header className="card-header" style={{backgroundColor:'goldenrod'}}>
+                <p className="card-header-title" style={{color:'white'}}>
+                    { title }
+                </p>
+            </header>
+
+            <div className="card-content">
+                <nav className="level">
+                    <div className='level-item' style={{width:160}}>
+                        <div className="media">
+                            <div className="media-left">
+                                <figure className="image is-48x48">
+                                    <img src="signs/Leo.png" alt="Solar sign" />
+                                </figure>
+                                <p className="title is-4">John Smith</p>
+                            </div>
+                        </div>
                     </div>
 
-                    <div className="control">
-                        <a className="button is-info" onClick={() => reply(value, id)}> 
-                            <svg 
-                                xmlns="http://www.w3.org/2000/svg" 
-                                viewBox="0 0 24 24" 
-                                data-supported-dps="24x24" 
-                                fill="white" 
-                                className="mercado-match" 
-                                width="24" 
-                                height="24" 
-                                focusable="false"
-                            ><path d="M21 3L0 10l7.66 4.26L16 8l-6.26 8.34L14 24l7-21z"></path></svg>
-                        </a>
+                    <div className="level-item" style={{width:'calc(100% - 160px)', minHeight:100}}>
+                        <div className="content" style={{minHeight: 100, textAlign:'left', width:'100%'}}>
+                            <p> 
+                                <DateTime date={new Date()} />
+                                { detail } 
+                            </p>
+                        </div>
                     </div>
-                </div>
+                </nav>
             </div>
 
-
-            { replies.map(comment => <a className="panel-block is-active"> { comment } </a> ) }
-
-            <div 
-                className="panel-block" 
-                onClick={() => setReplies(comments)} 
-                style={{display:comments.length > replies.length ? 'initial' : 'none' }}
-            >
-                <button className="button is-link is-outlined is-fullwidth">
-                    Mostrar Todos Los Comentarios
-                </button>
-            </div>
-        </nav>
-    </div>
+            <footer className="card-footer">
+                <a href="#" className="card-footer-item"> Comentar </a>
+                <a href="#" className="card-footer-item"> Mostrar Comentarios </a>
+            </footer>
+        </div>
+        
+        <div style={{width:80}}/>
+    </div>    
 }
 
 
-interface iPosts { posts:iPost[], submit(post:iPost):void, reply(text:string, postId:string):void, like(postId:string):void }
+interface iPosts { 
+    posts:iPost[]
+    submit(post:iPost):void
+    like(postId:string):void
+    reply(text:string, postId:string):void
+}
+
 export const Posts = ({posts, submit, reply, like}: iPosts) => {
     const [ isActive, setActive] = useState(false)
     const [ newPost, setNewPost ] = useState<iPost>({ title:'', detail:''})
 
-    return <div className='content'>
+    return <div className='content' style={{maxWidth:720, margin:'auto'}}>
         <Header 
             title={"AstroChat"} 
             description={"Interactua con el grupo y comparte lo que haz aprendido."} 
