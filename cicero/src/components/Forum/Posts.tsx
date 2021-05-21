971392ec8ab620aa572b9577b9119896edf07f7f
Santiago8888 <santiago.marti67@gmail.com>
2021-05-20 20:18:12 -0500

    Can Comment State & Reply Box.

--- cicero/src/components/Forum/Posts.tsx
+++ cicero/src/components/Forum/Posts.tsx
@@ -27,6 +27,7 @@ export const DateTime = ({ date }:{ date:Date }) => <i style={{marginBottom:12}}
 export interface iPost { id?:string, title:string, detail:string, likes?:number, comments?:string[] }
 interface IPost extends iPost { id:string, reply(text:string, postId:string):void, like(postId:string):void }
 const Post = ({ id, title, detail, likes=0, comments=[], reply, like }: IPost) => {
+    const [ canComment, setCanComment ] = useState(false) 
     const [ value, setValue ] = useState('')
     const [ replies, setReplies ] = useState<string[]>([])
 
@@ -70,13 +71,42 @@ const Post = ({ id, title, detail, likes=0, comments=[], reply, like }: IPost) =
             </div>
 
             <footer className="card-footer">
-                <a href="#" className="card-footer-item"> Comentar </a>
+                { !canComment && <a href="#" className="card-footer-item" onClick={() => setCanComment(true)}> Comentar </a> }
                 <a href="#" className="card-footer-item"> Mostrar Comentarios </a>
             </footer>
-        </div>
-        
+
+            {
+                canComment && 
+                <div className="field has-addons" style={{marginBottom:10, borderTop: '2px #ededed solid', paddingTop:10}}>
+                    <div className="control" style={{width:'100%', marginLeft:20}}>
+                        <input 
+                            type="text" 
+                            className="input" 
+                            placeholder="Comentar"
+                            onChange={({target:{value}}) => setValue(value)}                        
+                            onKeyPress={({ key }) => key === 'Enter' ? reply(value, id) : null}
+                        />
+                    </div>
+
+                    <div className="control" style={{marginRight:20}}>
+                        <a className="button is-info" onClick={() => reply(value, id)}> 
+                            <svg 
+                                xmlns="http://www.w3.org/2000/svg" 
+                                viewBox="0 0 24 24" 
+                                data-supported-dps="24x24" 
+                                fill="white" 
+                                className="mercado-match" 
+                                width="24" 
+                                height="24" 
+                                focusable="false"
+                            ><path d="M21 3L0 10l7.66 4.26L16 8l-6.26 8.34L14 24l7-21z"></path></svg>
+                        </a>
+                    </div>
+                </div>
+            }
+        </div>        
         <div style={{width:80}}/>
-    </div>    
+    </div>
 }
 
 
