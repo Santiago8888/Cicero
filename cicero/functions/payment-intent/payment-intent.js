537cc904410b8aebe4a0505fa8551e5ccab5fa24
Santiago8888 <santiago.marti67@gmail.com>
2021-05-24 17:09:31 -0500

    Fixed typo on Stripe response.

--- cicero/functions/payment-intent/payment-intent.js
+++ cicero/functions/payment-intent/payment-intent.js
@@ -2,6 +2,6 @@ const { stripe_key } = process.env;
 
 exports.handler = async () => {
     const stripe = require("stripe")(stripe_key);
-    const { clientSecret } = await stripe.paymentIntents.create({ amount: 400*100, currency: "usd" });
-	return { statusCode:200, body:JSON.stringify(clientSecret) };    
+    const { client_secret } = await stripe.paymentIntents.create({ amount: 400*100, currency: "usd" });
+	return { statusCode:200, body:JSON.stringify(client_secret) }
 }
