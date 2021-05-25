80beae9a7d189d2273d3d0d300aa60281e07fe87
Santiago8888 <santiago.marti67@gmail.com>
2021-05-25 02:44:13 -0500

    User prop on forum and posts.

--- cicero/src/components/Interaction.tsx
+++ cicero/src/components/Interaction.tsx
@@ -1,11 +1,12 @@
 import { Recordings, iRecordings } from './Forum/Recordings'
 import { Forum, iDoubt, iForum } from './Forum/Forum'
 import { Posts, iPost } from './Forum/Posts'
-
 import { Error } from './Error'
+import { iUser } from '../App'
 
 
 interface iInteraction { 
+    user:iUser
     forum?:iForum
     recordings?:iRecordings 
     posts?:iPost[]
@@ -17,10 +18,10 @@ interface iInteraction {
     reply(text:string, id:string):void
 }
 
-export const Interaction = ({ forum, recordings, posts, submit, post, likePost, reply, like }: iInteraction) => {
+export const Interaction = ({ user, forum, recordings, posts, submit, post, likePost, reply, like }: iInteraction) => {
 
-    return  forum ? <Forum {...forum} submit={submit} like={like}/>
+    return  forum ? <Forum {...forum} user={user} submit={submit} like={like}/>
         :   recordings ? <Recordings {...recordings}/>
-        :   posts ?  <Posts posts={posts} post={post} reply={reply} like={likePost}/>
+        :   posts ?  <Posts user={user} posts={posts} post={post} reply={reply} like={likePost}/>
     : <Error/>
 }
