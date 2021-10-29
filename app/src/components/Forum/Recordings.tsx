82acb21fe0196def7e98312a287d18fb4a46e819
Santiago8888 <santiago.marti67@gmail.com>
2021-10-29 14:58:06 -0500

    Colors on recordings.

--- app/src/components/Forum/Recordings.tsx
+++ app/src/components/Forum/Recordings.tsx
@@ -6,7 +6,7 @@ const Recording = ({ title, link, description }: iRecording) => {
     const midScreen = useMediaQuery({ query: '(min-width: 900px)' })
 
     return <div style={{width:midScreen ? 800 : 450, textAlign:'left', margin:'auto', marginBottom:'1.5em'}}>
-        <p style={{color:'darkblue', fontSize:'1.25rem', fontWeight:600, marginBottom:0, textAlign: midScreen ? 'initial' : 'center'}}> 
+        <p style={{color:'saddlebrown', fontSize:'1.25rem', fontWeight:600, marginBottom:0, textAlign: midScreen ? 'initial' : 'center'}}> 
             { title } 
         </p>
 
@@ -43,7 +43,7 @@ export const Recordings = ({ title, description, recordings}: iRecordings) => {
     const smallScreen = useMediaQuery({ query: '(max-width: 600px)' })
 
     return <div className="content">
-        <h1 style={{fontSize:'3rem', marginBottom:'2rem', color:'darkblue'}}> { title } </h1>
+        <h1 style={{fontSize:'3rem', marginBottom:'2rem', color:'saddlebrown'}}> { title } </h1>
         <h3 
             style={{
                 margin:'0rem auto',
@@ -55,7 +55,7 @@ export const Recordings = ({ title, description, recordings}: iRecordings) => {
             }}
         > { description } </h3>
 
-        <hr style={{ backgroundColor:'darkblue', margin:'1.5rem auto 3rem', width:midScreen ? 600 : 320 }}/>
+        <hr style={{ backgroundColor:'darkolivegreen', margin:'1.5rem auto 3rem', width:midScreen ? 600 : 320 }}/>
 
 
         { recordings.map((r, i)=> <Recording {...r} key={i}/>) }
