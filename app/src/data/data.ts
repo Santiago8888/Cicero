22df7c40499fe7429f04b9ff7b707e104ca1d5e3
Santiago8888 <santiago.marti67@gmail.com>
2021-11-13 03:08:23 -0600

    Unsort quiz 13.

--- app/src/data/data.ts
+++ app/src/data/data.ts
@@ -302,34 +302,34 @@ const quiz13:iQuestion[] = [
     { 
         question: '1. ¿Cómo lidiar con las lecciones de Saturno?', 
         answers:[
-            { answer:'Identificar, soltar y entender.', value:true },
             { answer:'Escuchar, rezar y tomar acción.', value:false },
             { answer:'Resignarse, aceptar y agradecer.', value:false },
+            { answer:'Identificar, soltar y entender.', value:true },
             { answer:'Observar, buscar y enseñar.', value:false },
         ]
     },   { 
         question: '2. ¿Cómo identificamos si estamos pasando por una lección de Saturno?', 
         answers:[
-            { answer:'Las cosas dejan dejan de fluir, y obtenemos señales de las personas en nuestra vida diaria.', value:true },
             { answer:'Nos sentimos deprimidos, cansados y sin motivación.', value:false },
             { answer:'Nos corren del trabajo o tenemos problemas profesionales.', value:false },
+            { answer:'Las cosas dejan dejan de fluir, y obtenemos señales de las personas en nuestra vida diaria.', value:true },
             { answer:'Nuestra salud se resiente y constantemente discutimos con las personas a nuestro alrededor.', value:false },
         ]
     },   { 
         question: '3. ¿Cuales son las causas de que nos alejemos de nuestro centro?', 
         answers:[
-            { answer:'Estar muy dispersos o sin falta de ritmo.', value:true },
             { answer:'No escuchamos a los demás y hablamos mucho.', value:false },
+            { answer:'Estar muy dispersos o sin falta de ritmo.', value:true },
             { answer:'Olvidamos el cuidado de nuestro y alma.', value:false },
             { answer:'Actuamos en contra de nuestros valores.', value:false },
         ]
     },   { 
         question: '4. ¿Cuál de estos es un mal consejo cuando pasamos por una lección de Saturno?', 
         answers:[
-            { answer:'Entre más pronto aprendamos la lección mejor.', value:true },
             { answer:'Adquirir consciencia nos sirve para mejorar las circunstancias.', value:false },
             { answer:'Procura tener buenas intenciones, emociones, pensamientos y acciones.', value:false },
             { answer:'Realiza un cambio interno y examen de conciencia.', value:false },
+            { answer:'Entre más pronto aprendamos la lección mejor.', value:true },
         ]
     },   { 
         question: '5. ¿Para lidiar con una lección, que necesito saber?', 
@@ -342,25 +342,25 @@ const quiz13:iQuestion[] = [
     },   { 
         question: '6. ¿Cómo se transforma una situación desde la raíz?', 
         answers:[
+            { answer:'A través de la meditación y Kabbalah.', value:false },
             { answer:'Cambiar los deseos, pensamientos y sustituirlos por otros más positivos.', value:true },
             { answer:'Conectarse con tu yo interior y seguir las indicaciones de tu alma.', value:false },
-            { answer:'A través de la meditación y Kabbalah.', value:false },
             { answer:'Esperando y siendo paciente dejando que las consecuencias tomen su curso.', value:false },
         ]
     },   { 
         question: '7. ¿Qué es inercia?', 
         answers:[
-            { answer:'Eventos que siguen ocurriendo pero ya están transcendidos y no nos afectan.', value:true },
             { answer:'La resistencia que tenemos a cambiar hábitos negativos.', value:false },
             { answer:'El karma familiar que heredamos de nuestros padres y antepasados.', value:false },
             { answer:'La fuerza que ejerce Saturno para afectar o premiar nuestra vida.', value:false },
+            { answer:'Eventos que siguen ocurriendo pero ya están transcendidos y no nos afectan.', value:true },
         ]
     },   { 
         question: '8. ¿Qué significa soltar?', 
         answers:[
+            { answer:'Permitirnos ser despojados de todo lo que nos está haciendo daño.', value:false },
             { answer:'Liberarnos de la carga emocional que engendra las circunstancias negativas.', value:true },
             { answer:'Resignarse y aceptar nuestro propósito y los designios de la conciencia universal.', value:false },
-            { answer:'Permitirnos ser despojados de todo lo que nos está haciendo daño.', value:false },
             { answer:'Escuchar atentamente pero no involucrarse emocionalmente con vibraciones negativas.', value:false },
         ]
     },   { 
@@ -374,10 +374,10 @@ const quiz13:iQuestion[] = [
     },   { 
         question: '10. ¿Cuando comienza el proceso de recuperación o regeneración?', 
         answers:[
-            { answer:'Cuando soltamos', value:true },
             { answer:'Cuando nos conectamos con nuestro ser interior', value:false },
             { answer:'Cuando nos liberamos del ego', value:false },
             { answer:'Cuando vencemos a Saturno', value:false },
+            { answer:'Cuando soltamos', value:true },
         ]
     },   { 
         question: '11. ¿Cual es el siguiente paso del proceso de recuperación o regeneración?', 
@@ -390,26 +390,26 @@ const quiz13:iQuestion[] = [
     },   { 
         question: '12. ¿Cuando termina el proceso de recuperación y regeneración?', 
         answers:[
-            { answer:'Cuando entendemos que no hemos cumplido los compromisos que adquirimos.', value:true },
             { answer:'Dos años después del comienzo.', value:false },
             { answer:'Cuando regresamos a nuestro centro.', value:false },
             { answer:'Cuando terminamos de pagar el karma.', value:false },
+            { answer:'Cuando entendemos que no hemos cumplido los compromisos que adquirimos.', value:true },
         ]
     },   { 
         question: '13. ¿Que nos indica Saturno en nuestra carta astral?', 
         answers:[
-            { answer:'El qué y el donde debemos tener un comportamiento impecable.', value:true },
             { answer:'El cuando y el cómo alcanzar el éxito.', value:false },
             { answer:'Quiénes se convertirán en nuestros maestros.', value:false },
+            { answer:'El qué y el donde debemos tener un comportamiento impecable.', value:true },
             { answer:'Cuál es el camino a nuestra felicidad.', value:false },
         ]
     },   { 
         question: '14. ¿Porqué el entendimiento es la antesala de la sabiduría?', 
         answers:[
-            { answer:'Porque Saturno nos enseña el funcionamiento de nuestra realidad: cosechamos lo que sembramos.', value:true },
+            { answer:'Porque Saturno enseña, pero Neptuno ilumina.', value:false },
             { answer:'Porque Binah es la tercera esfera del árbol de la vida.', value:false },
             { answer:'Porque Saturno impone límites y esto causa ignorancia.', value:false },
-            { answer:'Porque Saturno enseña, pero Neptuno ilumina.', value:false },
+            { answer:'Porque Saturno nos enseña el funcionamiento de nuestra realidad: cosechamos lo que sembramos.', value:true },
         ]
     }, 
 ]
