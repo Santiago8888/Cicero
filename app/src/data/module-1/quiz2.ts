6de4e16504fcd5cc96ca03af6c149b80e6759527
Santiago8888 <santiago.marti67@gmail.com>
2021-11-14 03:05:28 -0600

    Quiz 1.2. as a file.

---
+++ app/src/data/module-1/quiz2.ts
@@ -0,0 +1,102 @@
+import { iQuestion } from '../../components/Views/Quiz'
+
+
+export const quiz12:iQuestion[] = [
+    { 
+        question:'1. ¿Qué significa la palabra karma?', 
+        answers:[
+            { answer:'Deuda', value:false },
+            { answer:'Acción', value:true },
+            { answer:'Pago', value:false },
+            { answer:'Salida', value:false }
+        ]
+    }, { 
+        question:'2. ¿Por qué existe el karma?', 
+        answers:[
+            { answer:'Para que nos portemos bien.', value:false },
+            { answer:'Enseñar que nuestros actos tienen consecuencias.', value:true },
+            { answer:'Vivimos en un sistema, no hay acciones aisladas.', value:true },
+            { answer:'Es una ley universal, no hay que preguntar el porqué sino aceptarla.', value:false }
+        ]
+    }, { 
+        question:'3. ¿Cuándo empezamos a ver la manifestación del karma?', 
+        answers:[
+            { answer:'Depende de la posición de Saturno y nuestra carta astral.', value:false },
+            { answer:'Cuando perjudicamos a alguien conscientemente.', value:false },
+            { answer:'Cuando vamos en contra de nuestros valores.', value:false },
+            { answer:'Cuando la acción se ha repetido muchas veces.', value:true }
+        ]
+    }, { 
+        question:'4. ¿En dónde quedan registrados todos los efectos de nuestras acciones?', 
+        answers:[
+            { answer:'En nuestra consciencia', value:false },
+            { answer:'En la memoria cósmica', value:false },
+            { answer:'En el mundo astral', value:true },
+            { answer:'No todas nuestros efectos quedan registrados.', value:false }
+        ]
+    }, { 
+        question:'5. ¿Cómo se llama el periodo entre la causa y el efecto?', 
+        answers:[
+            { answer:'Periodo de Manifestación', value:false },
+            { answer:'Misericordia', value:true },
+            { answer:'Expectativa', value:false },
+            { answer:'Gestación', value:false }
+        ]
+    }, { 
+        question:'6. ¿Por qué no vemos inmediatamente las consecuencias de nuestros actos?', 
+        answers:[
+            { answer:'Para poder practicar y aprender', value:true },
+            { answer:'Para liberar nuestro poder creador y de manifestación.', value:false },
+            { answer:'Para permitirnos enmendar nuestros errores.', value:false },
+            { answer:'Es una consecuencia de vivir en un mundo físico.', value:false }
+        ]
+    }, { 
+        question:'7. ¿Qué acciones se manifiestan más rápido?', 
+        answers:[
+            { answer:'Las positivas', value:true },
+            { answer:'Las negativas', value:false },
+            { answer:'Las intencionales', value:false },
+            { answer:'Las que hacemos en público', value:false }
+        ]
+    }, { 
+        question:'8. ¿Qué se requiere para tener un karma positivo?', 
+        answers:[
+            { answer:'Hacer meditación y oración.', value:false },
+            { answer:'Saludar a los demás amablemente.', value:false },
+            { answer:'Enmendar el daño de nuestras acciones.', value:false },
+            { answer:'Hacer acciones positivas y sostener esa frecuencia de amor, alegría o compartir.', value:true }
+        ]
+    }, { 
+        question:'9. ¿Por qué hay personas que no enfrentan consecuencias por acciones negativas?', 
+        answers:[
+            { answer:'Tienen suerte', value:false },
+            { answer:'Para enseñar a los demás', value:false },
+            { answer:'Se está acumulando su karma', value:true },
+            { answer:'No existen las acciones negativas', value:false }
+        ]
+    }, { 
+        question:'10. ¿Cuándo empezamos a tener el karma negativo en nuestra vida?', 
+        answers:[
+            { answer:'Cuando nos pasamos 50 altos', value:false },
+            { answer:'Cuando se nos acaba el karma positivo', value:false },
+            { answer:'Cuando necesitamos parar, y hacer las cosas de acuerdo a las reglas.', value:true },
+            { answer:'Cuando Saturno se encuentra en el 6 o 13 de cada signo.', value:false }
+        ]
+    }, { 
+        question:'11. ¿Cuál es la relación entre el karma y Saturno?', 
+        answers:[
+            { answer:'No tienen relación actualmente, pero fueron creados al mismo tiempo.', value:false },
+            { answer:'Saturno manifiesta la ley del karma.', value:true },
+            { answer:'Saturno puede llenarnos de gracia y evitar las consecuencias negativas.', value:false },
+            { answer:'Saturno es el Dios del Karma.', value:false }
+        ]
+    }, { 
+        question:'12. ¿Cuál es otra causa de que enfrentamos consecuencias negativas?', 
+        answers:[
+            { answer:'No buscar la espiritualidad.', value:false },
+            { answer:'Las responsabilidades que hemos adquirido no las estamos llevando a cabo.', value:true },
+            { answer:'Los pensamientos de las acciones que no llevamos a cabo.', value:false },
+            { answer:'Empeñarnos en seguir exclusivamente nuestra voluntad.', value:false }
+        ]
+    },
+]
