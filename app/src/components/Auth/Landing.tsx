3d257c66d15dc6592ed17ccaf854b8d8d234df7f
Santiago8888 <santiago.marti67@gmail.com>
2021-11-14 10:46:42 -0600

    Mobile styling on landing view.

--- app/src/components/Auth/Landing.tsx
+++ app/src/components/Auth/Landing.tsx
@@ -12,18 +12,26 @@ const Welcome = ({ click, }:iWelcome) => {
     const smallScreen = useMediaQuery({ query: '(max-width: 600px)' })
 
     return <div className='content' style={{textAlign:'center'}}>
-        <h1 style={{fontSize:!smallScreen ? '3rem' : '2rem', marginBottom:0, color:'saddlebrown'}}> SATURNO 🪐  </h1>
+        <h1 
+            style={{
+                fontSize:!smallScreen ? '3rem' : '2rem', 
+                marginBottom:0, 
+                color:'saddlebrown'
+            }}
+        > SATURNO 🪐 </h1>
 
         <div style={{marginBottom:!smallScreen ? '1rem' : 0}}>
-            {
-                !smallScreen 
-                    ?   <h2 style={{marginBottom:'1.5rem', marginRight:'auto', marginLeft:'auto', width:760, color:'saddlebrown'}}>
-                            El maestro del Karma
-                        </h2>
-                    :   <h2 style={{marginBottom:'1.5rem', marginRight:'auto', marginLeft:'auto', color:'navy', fontSize:'1.25rem', marginTop:'1rem'}}>
-                            Escucha el llamado de tu alma.
-                        </h2>
-            }
+            <h2 
+                style={{
+                    margin:`0.5rem auto 1.5rem`,
+                    width:!smallScreen ? 760 : 300, 
+                    fontSize:!smallScreen ? '2rem' : '1.5rem',
+                    color:'navy'
+                }}
+            >
+                El señor del Karma
+            </h2>
+
             <div>
                 <ReactPlayer 
                     style={{margin:'auto'}}
@@ -39,7 +47,7 @@ const Welcome = ({ click, }:iWelcome) => {
                 onClick={click}
                 className='button is-link'
                 style={{ 
-                    width:320, 
+                    width:!smallScreen ? 320 : 260, 
                     fontSize: !smallScreen ? '1.5rem' : '1.25rem', 
                     fontWeight:900, 
                     marginTop:'1rem',
