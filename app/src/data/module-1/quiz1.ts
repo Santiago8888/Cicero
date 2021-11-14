1fddb285c7544a1c7e7da4f33f0e3b3c40804519
Santiago8888 <santiago.marti67@gmail.com>
2021-11-14 03:05:20 -0600

    Quiz 1.1 as a file.

---
+++ app/src/data/module-1/quiz1.ts
@@ -0,0 +1,61 @@
+import { iQuestion } from '../../components/Views/Quiz'
+
+export const quiz11:iQuestion[] = [
+    { 
+        question:'1. ¿Qué indica Saturno en la carta astral?', 
+        answers:[
+            { answer:'Cómo encontrar abundancia', value: false },
+            { answer:'Si tienes tiempo o ya se te acabó', value: true },
+            { answer:'Nuestra espiritualidad y conexión con la fuente divina', value: false },
+            { answer:'Lo que amamos y nos hace felices', value: false }
+        ] 
+    }, { 
+        question:'2. ¿Cuándo nos indica Saturno que es tiempo de cambios?', 
+        answers:[
+            { answer:'Cada que llega al signo de Acuario', value: false },
+            { answer:'Durante la infancia y adolescencia', value: false },
+            { answer:'Aproximadamente cada siete años', value: true },
+            { answer:'A los 42 cuando se junta con Urano', value: false }
+        ] 
+    }, { 
+        question:'3. ¿Cuál es uno de los principales mensajes de Saturno?', 
+        answers:[
+            { answer:'Disfruta la vida, se feliz', value: false },
+            { answer:'Ama incondicionalmente', value: false },
+            { answer:'Perdona al prójimo', value: false },
+            { answer:'Hazte responsable, madura', value: true }
+        ] 
+    }, { 
+        question:'4. ¿Qué nos enseña Saturno?', 
+        answers:[
+            { answer:'Nuestros defectos y áreas de oportunidad', value: false },
+            { answer:'La consecuencia de nuestras acciones', value: true },
+            { answer:'El futuro y el pasado', value: false },
+            { answer:'Cómo tomar el control de nuestra vida', value: false }
+        ] 
+    }, { 
+        question:'5. ¿Cuál es la responsabilidad de Saturno?', 
+        answers:[
+            { answer:'Ponernos límites', value: true },
+            { answer:'Elevar la consciencia cósmica', value: false },
+            { answer:'Distinguir entre la verdad y la mentira', value: false },
+            { answer:'Sostener la creación', value: false }
+        ] 
+    }, { 
+        question:'6. ¿Todos tenemos el mismo Saturno?', 
+        answers:[            
+            { answer:'Solo al principio después cambia conforme a nuestras acciones.', value: false },
+            { answer:'Si, Saturno forma parte de una conciencia universal.', value: false },
+            { answer:'No, para cada uno fluye de forma diferente.', value: true },
+            { answer:'Va cambiando, pero lo que está bien y mal es igual para todos.', value: false }
+        ] 
+    }, { 
+        question:'7. ¿Cuál es el objetivo final de tu Saturno?', 
+        answers:[
+            { answer:'Enseñarnos a dar un buen ejemplo en un área específica de la vida.', value: true },
+            { answer:'Limitarnos para que no nos salgamos de control.', value: false },
+            { answer:'Premiar a quienes se conectan con su ser divino interior.', value: false },
+            { answer:'Llevar la contabilidad de tus acciones y karma.', value: false }
+        ] 
+    }
+]
