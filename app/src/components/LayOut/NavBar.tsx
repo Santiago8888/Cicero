ac8feb8c40d9b39aea32b5d1797cbe36cf20f631
Santiago8888 <santiago.marti67@gmail.com>
2021-11-14 18:02:17 -0600

    Add home data to navbar.

--- app/src/components/LayOut/NavBar.tsx
+++ app/src/components/LayOut/NavBar.tsx
@@ -1,15 +1,15 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 
-import { CSSProperties, useState } from 'react'
 import { useMediaQuery } from 'react-responsive'
-import { iUser } from '../../App'
+import { CSSProperties, useState } from 'react'
+import { iHomeData, iUser } from '../../App'
 
 
 const tabStyle:CSSProperties = { fontSize: '1.2em', backgroundColor:'darkolivegreen' }
 const navTextStyle:CSSProperties = {textAlign:'center', color:'white', backgroundColor:'darkolivegreen'}
 
 export type NavbarItem =  'Login' | 'Recordings' | 'Forum' | 'Home' | 'Posts' | 'Back' | 'Next'
-interface iNavBar { user?:iUser, click(item:NavbarItem):void }
+interface iNavBar { user?:iUser, homeData:iHomeData, click(item:NavbarItem):void }
 
 const Back = () => <svg xmlns="http://www.w3.org/2000/svg" width="56" height="32" fill="currentColor" className="bi bi-arrow-left" viewBox="0 0 16 16">
     <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z"></path>
@@ -31,7 +31,7 @@ const isAdvanced = (user:iUser | undefined) => {
     return false
 }
 
-export const NavBar = ({ user, click }: iNavBar) => {
+export const NavBar = ({ user, homeData, click }: iNavBar) => {
     const midScreen = useMediaQuery({ query: '(min-width: 1024px)' })
     const [ isActive, setActive ] = useState(false)
 
