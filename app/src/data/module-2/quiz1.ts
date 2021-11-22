5af31ea9c25fdecd524d5ea96d1f0cd23eb29a5f
Santiago8888 <santiago.marti67@gmail.com>
2021-11-22 15:12:35 -0600

    Quiz 1, first question: immediate consequence.

--- app/src/data/module-2/quiz1.ts
+++ app/src/data/module-2/quiz1.ts
@@ -1,1 +1,13 @@
-export const quiz1 = []
+import { iQuestion } from "../../components/Views/Quiz"
+
+export const quiz1:iQuestion[] = [
+    { 
+        question: '1. ¿Cuál es la primer consecuencia de tener a Saturno en un signo?', 
+        answers:[
+            { answer:'En ese signo Saturno se va a enfocar y nos va a auditar.', value:true },
+            { answer:'Nos llevaremos muy bien con las personas de ese signo.', value:false },
+            { answer:'Tendremos todas las virtudes de ese signo.', value:false },
+            { answer:'Sabremos que fue el signo de nuestro vida pasada más reciente.', value:false },
+        ]
+    },    
+]
