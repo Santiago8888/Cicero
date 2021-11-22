6258056b2ff67f9e7a160490ff176eac81ff4edf
Santiago8888 <santiago.marti67@gmail.com>
2021-11-22 11:28:07 -0600

    Fourth Video: Saturn's mision.

--- app/src/data/module-2/index.ts
+++ app/src/data/module-2/index.ts
@@ -19,12 +19,15 @@ const lessons2:iLesson[] = [
         type:'Video',
         title:'La influencia de tu signo',
         link:'https://youtu.be/k5oxhnHwYWQ'
+    }, {
+        type:'Video',
+        title:'La misión de Saturno',
+        link:'iLpqbBzRIvk'
     }
-
 ]
 
 export const module2 = [
     { title: '2.1 Saturno y tus vidas pasadas', lessons:lessons1 },
-    { title: '2.2 La influencia de tu Signo', lessons:lessons2 },
-    { title: '2.3 Tu misión de Saturno', lessons:[] }
+    { title: '2.2 La misión de Saturno', lessons:lessons2 },
+    { title: '2.3 El camino de retorno', lessons:[] }
 ]
