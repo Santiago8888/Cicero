a24c961b34063f1a682c7b3e3cfb8b946fd01a4f
Santiago8888 <santiago.marti67@gmail.com>
2021-11-22 10:53:28 -0600

    Export module 2.

---
+++ app/src/data/module-2/index.ts
@@ -0,0 +1,6 @@
+
+export const module2 = [
+    { title: '2.1 El significado del signo de tu Saturno', lessons:[] },
+    { title: '2.2 La misión de tu Saturno', lessons:[] },
+    { title: '2.3 Tus vidas pasadas', lessons:[] }
+]
