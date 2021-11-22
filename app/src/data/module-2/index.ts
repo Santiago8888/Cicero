91707610c6801ea6c121b4d7ead10d947f5a585f
Santiago8888 <santiago.marti67@gmail.com>
2021-11-22 11:30:53 -0600

    Fifth video: setting an example.

--- app/src/data/module-2/index.ts
+++ app/src/data/module-2/index.ts
@@ -2,32 +2,23 @@ import { iLesson } from "../../components/LayOut/Menu"
 
 
 const lessons1:iLesson[] = [
-    { 
-        type:'Video', 
-        title:'El significado del signo', 
-        link:'https://youtu.be/uJP6kRJq9Qs', 
-    }, {
-        type:'Video',
-        title:'Tus vidas pasadas',
-        link:'https://youtu.be/qR1KmIG5Fl0'
-    }
+    { type:'Video', title:'El significado del signo', link:'https://youtu.be/uJP6kRJq9Qs' }, 
+    { type:'Video', title:'Tus vidas pasadas', link:'https://youtu.be/qR1KmIG5Fl0' }
 ]
 
 
 const lessons2:iLesson[] = [
-    {
-        type:'Video',
-        title:'La influencia de tu signo',
-        link:'https://youtu.be/k5oxhnHwYWQ'
-    }, {
-        type:'Video',
-        title:'La misión de Saturno',
-        link:'iLpqbBzRIvk'
-    }
+    { type:'Video', title:'La influencia de tu signo', link:'https://youtu.be/k5oxhnHwYWQ' }, 
+    { type:'Video', title:'La misión de Saturno', link:'iLpqbBzRIvk' }
+]
+
+
+const lessons3:iLesson[] = [
+    { type:'Video', title:'Ser un buen ejemplo', link:'MjU_S2C_cqc' },
 ]
 
 export const module2 = [
     { title: '2.1 Saturno y tus vidas pasadas', lessons:lessons1 },
     { title: '2.2 La misión de Saturno', lessons:lessons2 },
-    { title: '2.3 El camino de retorno', lessons:[] }
+    { title: '2.3 El camino de retorno', lessons:lessons3 }
 ]
