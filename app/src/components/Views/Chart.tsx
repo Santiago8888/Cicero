5cb34c0da133adfd03f3c7a0c997231025f7dc0f
Santiago8888 <santiago.marti67@gmail.com>
2021-11-22 13:14:43 -0600

    Date of birth on chart description.

--- app/src/components/Views/Chart.tsx
+++ app/src/components/Views/Chart.tsx
@@ -15,9 +15,17 @@ interface iChart {
     next():void 
 }
 
-export const Chart = ({ user:{ natalChart:{planets, houses}}, title, description, planet, drawHouses, next, approve }: iChart) => {
+const NatalData = ({ date, location }:iUser) => <span>
+    Fecha de Nacimiento: {`${date.getDate()}-${date.getMonth() + 1}-${date.getFullYear()}`} <br/>
+    Lugar de Nacimiento: { location }
+</span>
+
+export const Chart = ({ user, title, description, planet, drawHouses, next, approve }: iChart) => {
+    const { natalChart: { planets, houses } } = user 
+
     const midScreen = useMediaQuery({ query: '(min-width: 900px)' })
     const smallScreen = useMediaQuery({ query: '(max-width: 680px)' })
+
 
     useEffect(() => { approve({}) }, [approve])
 
@@ -32,7 +40,7 @@ export const Chart = ({ user:{ natalChart:{planets, houses}}, title, description
                 fontWeight: 500,
                 width: midScreen ? 800 : 320        
             }}
-        > { description ? description[0] : '' } </h3>
+        > { description ? description[0] : <NatalData {...user} /> } </h3>
 
         {
             !smallScreen
