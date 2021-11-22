989c20be53d9cfe74e8b2dc1e3c85363ddcea91c
Santiago8888 <santiago.marti67@gmail.com>
2021-11-22 11:25:36 -0600

    Third video: influence of Saturn.

--- app/src/data/module-2/index.ts
+++ app/src/data/module-2/index.ts
@@ -13,8 +13,18 @@ const lessons1:iLesson[] = [
     }
 ]
 
+
+const lessons2:iLesson[] = [
+    {
+        type:'Video',
+        title:'La influencia de tu signo',
+        link:'https://youtu.be/k5oxhnHwYWQ'
+    }
+
+]
+
 export const module2 = [
     { title: '2.1 Saturno y tus vidas pasadas', lessons:lessons1 },
-    { title: '2.2 La influencia de tu Signo', lessons:[] },
+    { title: '2.2 La influencia de tu Signo', lessons:lessons2 },
     { title: '2.3 Tu misión de Saturno', lessons:[] }
 ]
