44b5c0c6017109bfb0debf74825e26007a1a0476
Santiago8888 <santiago.marti67@gmail.com>
2021-11-22 11:08:57 -0600

    First lesson of module 2.

--- app/src/data/module-2/index.ts
+++ app/src/data/module-2/index.ts
@@ -1,6 +1,16 @@
+import { iLesson } from "../../components/LayOut/Menu"
+
+
+const lessons1:iLesson[] = [
+    { 
+        type:'Video', 
+        title:'El significado del signo', 
+        link:'https://youtu.be/uJP6kRJq9Qs', 
+    }
+]
 
 export const module2 = [
-    { title: '2.1 El significado del signo de tu Saturno', lessons:[] },
-    { title: '2.2 La misión de tu Saturno', lessons:[] },
-    { title: '2.3 Tus vidas pasadas', lessons:[] }
+    { title: '2.1 Saturno y tus vidas pasadas', lessons:lessons1 },
+    { title: '2.2 La influencia de tu Signo', lessons:[] },
+    { title: '2.3 Tu misión de Saturno', lessons:[] }
 ]
