50ed9c246ac49fa50c491a92051726d3b1ee03b1
Santiago8888 <santiago.marti67@gmail.com>
2021-11-23 14:46:43 -0600

    Tauro placeholder answers.

--- app/src/data/module-2/quiz3.ts
+++ app/src/data/module-2/quiz3.ts
@@ -5,69 +5,84 @@ export const quiz3:iQuestion[] = [
     { 
         question:'1. ¿De que debes ser un ejemplo hoy?', 
         answers:[
-            { answer:'De valentía, liderazgo y autenticidad', value:true, sign:'Ari' }
+            { answer:'De valentía, liderazgo y autenticidad', value:true, sign:'Ari' },
+            { answer:'De un sistema de valores inquebrantable', value:true, sign:'Tau' },
         ]
     }, { 
         question:'2. ¿Qué vas a enseñar a otros?', 
         answers:[
-            { answer:'A salir adelante por sus propios medios, a levantarse de cualquier caída y tomar decisiones asertivas.', value:true, sign:'Ari' }
+            { answer:'A salir adelante por sus propios medios, a levantarse de cualquier caída y tomar decisiones asertivas.', value:true, sign:'Ari' },
+            { answer:'', value:true, sign:'Tau' },
         ]
     }, { 
         question:'3. ¿Qué aprenderás a traves de cumplir tu misión?', 
         answers:[
-            { answer:'Respetar y ganarte el respeto sin tener que exigir, siendo autosuficiente, mostrando dignidad e independencia. ', value:true, sign:'Ari' }
+            { answer:'Respetar y ganarte el respeto sin tener que exigir, siendo autosuficiente, mostrando dignidad e independencia. ', value:true, sign:'Ari' },
+            { answer:'', value:true, sign:'Tau' },
         ]
     }, { 
         question:'4. ¿Cuáles cualidades aprenderas a través de las figuras de autoridad?', 
         answers:[
-            { answer:'Humildad, valentía, dignidad y paciencia', value:true, sign:'Ari' }
+            { answer:'Humildad, valentía, dignidad y paciencia', value:true, sign:'Ari' },
+            { answer:'', value:true, sign:'Tau' },
         ]
     }, { 
         question:'5. ¿Qué te sentirás mientras NO hayas aprendido esas cualidades?', 
         answers:[
-            { answer:'Impaciente, vulnerable, desafiado, ofendido y no reconocido', value:true, sign:'Ari' }
+            { answer:'Impaciente, vulnerable, desafiado, ofendido y no reconocido', value:true, sign:'Ari' },
+            { answer:'', value:true, sign:'Tau' },
         ]
     }, { 
         question:'6. ¿Como te sentirás cuando hayas aprendido esas cualidades?', 
         answers:[
-            { answer:'Segur@ y satisfech@ de mi nueva identidad y liderazgo.', value:true, sign:'Ari' }
+            { answer:'Segur@ y satisfech@ de mi nueva identidad y liderazgo.', value:true, sign:'Ari' },
+            { answer:'', value:true, sign:'Tau' },
         ]
     }, { 
         question:'7. ¿Generalmente, en que resiente más autoridad tu karma durante la infancia?', 
         answers:[
-            { answer:'No poder ser como quería y mi comportamiento era corregido constantemente', value:true, sign:'Ari' }
+            { answer:'No poder ser como quería y mi comportamiento era corregido constantemente', value:true, sign:'Ari' },
+            { answer:'', value:true, sign:'Tau' },
         ]
     }, { 
         question:'8. ¿Qué debes desarrollar para lograr tu misión?', 
         answers:[
-            { answer:'Mi autoestima, amor propio y nueva identidad pero con una actitud de humildad.', value:true, sign:'Ari' }
+            { answer:'', value:true, sign:'Tau' },
+            { answer:'Mi autoestima, amor propio y nueva identidad pero con una actitud de humildad.', value:true, sign:'Ari' },
         ]
     }, { 
         question:'9. ¿A qué tipo de desafíos te enfrentarás?', 
         answers:[
-            { answer:'Demostrar mi valentía ante los demás y quedarme solo cuando tenga algo que resolver', value:true, sign:'Ari' }
+            { answer:'', value:true, sign:'Tau' },
+            { answer:'Demostrar mi valentía ante los demás y quedarme solo cuando tenga algo que resolver', value:true, sign:'Ari' },
         ]
     }, { 
         question:'10. ¿Qué personas atraerás a tu vida?', 
         answers:[
-            { answer:'Que traten de exceder los límites para intentar hacerme perder la dignidad y me provocarán para que aprenda paciencia', value:true, sign:'Ari' }
+            { answer:'', value:true, sign:'Tau' },
+            { answer:'Que traten de exceder los límites para intentar hacerme perder la dignidad y me provocarán para que aprenda paciencia', value:true, sign:'Ari' },
         ]
     }, { 
         question:'11. ¿Cuales actitudes debes evitar?', 
         answers:[
-            { answer:'Agresivas', value:true, sign:'Ari' }
+            { answer:'Agresivas', value:true, sign:'Ari' },
+            { answer:'', value:true, sign:'Tau' },
         ]
     }, { 
         question:'12. ¿De qué forma tiendes a causar daño?', 
         answers:[
-            { answer:'Actuando sin pensar, o de lo contrario, por no actuar', value:true, sign:'Ari' }
+            { answer:'Actuando sin pensar, o de lo contrario, por no actuar', value:true, sign:'Ari' },
+            { answer:'', value:true, sign:'Tau' },
         ]
     }, { 
         question:'13. ¿Cuál de estas lecciones no te corresponde enseñar?', 
         answers:[
             { answer:'Cómo convertirse en un ejemplo de valentía', value:false, sign:'Ari' },
             { answer:'Tomar decisiones asertivas con entusiasmo', value:false, sign:'Ari' },
-            { answer:'Levantarse de cualquier caída aprendiendo de la experiencia', value:false, sign:'Ari' }
+            { answer:'Levantarse de cualquier caída aprendiendo de la experiencia', value:false, sign:'Ari' },
+            { answer:'', value:false, sign:'Tau' },
+            { answer:'', value:false, sign:'Tau' },
+            { answer:'', value:false, sign:'Tau' },
         ]
     }, 
 ]
