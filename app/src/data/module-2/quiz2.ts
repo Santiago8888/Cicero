268160e16fa30d99d7180d5329da05a47b1625af
Santiago8888 <santiago.marti67@gmail.com>
2021-11-23 04:58:47 -0600

    Saturn fears.

--- app/src/data/module-2/quiz2.ts
+++ app/src/data/module-2/quiz2.ts
@@ -4,33 +4,33 @@ export const quiz2:iQuestion[] = [
     { 
         question: '1. ¿Qué te restringe Saturno en esta vida?', 
         answers: [
-            { answer:'El impulso y la energía', sign:'Ari', value:true },
-            { answer:'Poder ahorrar grandes cantidades de dinero', sign:'Tau', value:true },
-            { answer:'La habilidad para comunicarme con los demás', sign:'Gem', value:true },
+            { answer:'El impulso y la energía.', sign:'Ari', value:true },
+            { answer:'Poder ahorrar grandes cantidades de dinero.', sign:'Tau', value:true },
+            { answer:'La habilidad para comunicarme con los demás.', sign:'Gem', value:true },
             { answer:'', sign:'Can', value:true },
         ]
     }, {
         question:'2. ¿Para qué te restringe eso Saturno?',
         answers: [
-            { answer:'Poder medir mi fuerza, canalizarla correctamente, dejar de insistir tantas veces para lograr algo y aprender de tus resultados', sign:'Ari', value:true },
-            { answer:'Deshacerme de los bienes a los que me apego: aquellos que solo me están haciendo la vida más difícil o pesada', sign:'Tau', value:true },
-            { answer:'Aprender a hacerlo de manera clara, sincera y abierta, verbalizar mis ideas, dejar de manipular y mantener una postura adecuada y congruente', sign:'Gem', value:true },
+            { answer:'Poder medir mi fuerza, canalizarla correctamente, dejar de insistir tantas veces para lograr algo y aprender de tus resultados.', sign:'Ari', value:true },
+            { answer:'Deshacerme de los bienes a los que me apego: aquellos que solo me están haciendo la vida más difícil o pesada.', sign:'Tau', value:true },
+            { answer:'Aprender a hacerlo de manera clara, sincera y abierta, verbalizar mis ideas, dejar de manipular y mantener una postura adecuada y congruente.', sign:'Gem', value:true },
             { answer:'', sign:'Can', value:true },
         ]
     }, {
         question:'3. ¿Para que sirven los miedos subconscientes que te genera Saturno?',
         answers: [
-            { answer:'Detener mi agresividad, para pensar antes de actuar y reconocer que mis acciones tienen consecuencias', sign:'Ari', value:true },
-            { answer:'No poder invertir en demasiados negocios, para que no endeudarme, evitar apostar o especular y para que no busquar oportunidades de acumulación', sign:'Tau', value:true },
-            { answer:'', sign:'Gem', value:true },
+            { answer:'Detener mi agresividad, para pensar antes de actuar y reconocer que mis acciones tienen consecuencias.', sign:'Ari', value:true },
+            { answer:'No poder invertir en demasiados negocios, para que no endeudarme, evitar apostar o especular y para que no busquar oportunidades de acumulación.', sign:'Tau', value:true },
+            { answer:'No convencer a los demás, pensar antes de hablar, no caer en chismes y no esforzarme demasiado en caer bien o hacer reír a los demás.', sign:'Gem', value:true },
             { answer:'', sign:'Can', value:true },
         ]
     }, {
         question:'4. ¿Qué es importante que aprendas en esta vida?',
         answers: [
-            { answer:'Tacto y cooperación con los demás', sign:'Ari', value:true },
-            { answer:'Valorar los recursos y a las personas', sign:'Tau', value:true },
-            { answer:'', sign:'Gem', value:true },
+            { answer:'Tacto y cooperación con los demás.', sign:'Ari', value:true },
+            { answer:'Valorar los recursos y a las personas.', sign:'Tau', value:true },
+            { answer:'Decir que no.', sign:'Gem', value:true },
             { answer:'', sign:'Can', value:true },
         ]
     }, {
