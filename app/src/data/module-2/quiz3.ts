9de998e625f4e96dd4f97fb3f05a48da58c69233
Santiago8888 <santiago.marti67@gmail.com>
2021-11-23 15:26:54 -0600

    Quiz: Cancer placeholder questions.

--- app/src/data/module-2/quiz3.ts
+++ app/src/data/module-2/quiz3.ts
@@ -8,6 +8,7 @@ export const quiz3:iQuestion[] = [
             { answer:'De valentía, liderazgo y autenticidad.', value:true, sign:'Ari' },
             { answer:'De un sistema de valores inquebrantable.', value:true, sign:'Tau' },
             { answer:'De una persona sincera y auténtica', value:true, sign:'Gem' },
+            { answer:'', value:true, sign:'Can' },
         ]
     }, { 
         question:'2. ¿Qué vas a enseñar a otros?', 
@@ -15,6 +16,7 @@ export const quiz3:iQuestion[] = [
             { answer:'A salir adelante por sus propios medios, a levantarse de cualquier caída y tomar decisiones asertivas.', value:true, sign:'Ari' },
             { answer:'A desarrollar autovaloración, a salir de sus problemas financieros, a tomar decisiones en base a buenos valores, y a mostrar honestidad y confiabilidad.', value:true, sign:'Tau' },
             { answer:'a comunicarse y relacionarse, a tener confianza en sí mismos, pedir lo que necesitan o hablar en público.', value:true, sign:'Gem' },
+            { answer:'', value:true, sign:'Can' },
         ]
     }, { 
         question:'3. ¿Qué aprenderás a traves de cumplir tu misión?', 
@@ -22,6 +24,7 @@ export const quiz3:iQuestion[] = [
             { answer:'Respetar y ganarte el respeto sin tener que exigir, siendo autosuficiente, mostrando dignidad e independencia.', value:true, sign:'Ari' },
             { answer:'A disfrutar de tus bienes y dinero, del resultado de tus esfuerzos, compartir tus recursos con tus seres queridos.', value:true, sign:'Tau' },
             { answer:'A hacer valer tu palabra, responsabilizarte de ella, sin necesidad de convencer sino siendo congruente, amable, honesto y alegre.', value:true, sign:'Gem' },
+            { answer:'', value:true, sign:'Can' },
         ]
     }, { 
         question:'4. ¿Cuáles cualidades aprenderas a través de las figuras de autoridad?', 
@@ -29,6 +32,7 @@ export const quiz3:iQuestion[] = [
             { answer:'Humildad, valentía, dignidad y paciencia.', value:true, sign:'Ari' },
             { answer:'Honestidad, perseverancia y confianza', value:true, sign:'Tau' },
             { answer:'', value:true, sign:'Gem' },
+            { answer:'', value:true, sign:'Can' },
         ]
     }, { 
         question:'5. ¿Cómo te sentirás mientras NO hayas aprendido esas cualidades?', 
@@ -36,6 +40,7 @@ export const quiz3:iQuestion[] = [
             { answer:'Impaciente, vulnerable, desafiado, ofendido y no reconocido.', value:true, sign:'Ari' },
             { answer:'Desconfiado, abusado, robado y temeroso de perder', value:true, sign:'Tau' },
             { answer:'', value:true, sign:'Gem' },
+            { answer:'', value:true, sign:'Can' },
         ]
     }, { 
         question:'6. ¿Como te sentirás cuando hayas aprendido esas cualidades?', 
@@ -43,6 +48,7 @@ export const quiz3:iQuestion[] = [
             { answer:'Segur@ y satisfech@ de mi nueva identidad y liderazgo.', value:true, sign:'Ari' },
             { answer:'Confianza y tranquilidad de que soy una persona valiosa', value:true, sign:'Tau' },
             { answer:'', value:true, sign:'Gem' },
+            { answer:'', value:true, sign:'Can' },
         ]
     }, { 
         question:'7. ¿Generalmente, en que resiente más autoridad tu karma durante la infancia?', 
@@ -50,6 +56,7 @@ export const quiz3:iQuestion[] = [
             { answer:'No poder ser como quería y mi comportamiento era corregido constantemente.', value:true, sign:'Ari' },
             { answer:'No tener permitido mentir, ser deshonesto o injusto, o tomar algo que no fuera tuyo.', value:true, sign:'Tau' },
             { answer:'', value:true, sign:'Gem' },
+            { answer:'', value:true, sign:'Can' },
         ]
     }, { 
         question:'8. ¿Qué debes desarrollar para lograr tu misión?', 
@@ -57,6 +64,7 @@ export const quiz3:iQuestion[] = [
             { answer:'Mi autoestima, amor propio y nueva identidad pero con una actitud de humildad.', value:true, sign:'Ari' },
             { answer:'Autovaloración y un sistema de valores basado en la confianza y justicia', value:true, sign:'Tau' },
             { answer:'', value:true, sign:'Gem' },
+            { answer:'', value:true, sign:'Can' },
         ]
     }, { 
         question:'9. ¿A qué tipo de desafíos te enfrentarás?', 
@@ -64,6 +72,7 @@ export const quiz3:iQuestion[] = [
             { answer:'Demostrar mi valentía ante los demás y quedarme solo cuando tenga algo que resolver.', value:true, sign:'Ari' },
             { answer:'Demostrar mi valor personal ante los demás y tener tentaciones de tomar más de lo que te corresponde.', value:true, sign:'Tau' },
             { answer:'', value:true, sign:'Gem' },
+            { answer:'', value:true, sign:'Can' },
         ]
     }, { 
         question:'10. ¿Qué personas atraerás a tu vida?', 
@@ -71,6 +80,7 @@ export const quiz3:iQuestion[] = [
             { answer:'Que traten de exceder los límites y provocarán para intentar hacerme perder la dignidad.', value:true, sign:'Ari' },
             { answer:'Traten de hacerme caer, de ganar mi confianza a cambio de promesas y que me prometan negocios irreales.', value:true, sign:'Tau' },
             { answer:'', value:true, sign:'Gem' },
+            { answer:'', value:true, sign:'Can' },
         ]
     }, { 
         question:'11. ¿Por qué atrae esas personas a tu vida?', 
@@ -78,6 +88,7 @@ export const quiz3:iQuestion[] = [
             { answer:'Para que aprenda paciencia.', value:true, sign:'Ari' },
             { answer:'Regresar a tomar conciencia de la honestidad y rectitud', value:true, sign:'Tau' },
             { answer:'', value:true, sign:'Gem' },
+            { answer:'', value:true, sign:'Can' },
         ]
     }, { 
         question:'12. ¿Cuales actitudes debes evitar?', 
@@ -85,6 +96,7 @@ export const quiz3:iQuestion[] = [
             { answer:'Las agresivas.', value:true, sign:'Ari' },
             { answer:'Deshonestas', value:true, sign:'Tau' },
             { answer:'', value:true, sign:'Gem' },
+            { answer:'', value:true, sign:'Can' },
         ]
     }, { 
         question:'13. ¿De qué forma tiendes a causar daño?', 
@@ -92,6 +104,7 @@ export const quiz3:iQuestion[] = [
             { answer:'Actuando sin pensar, o de lo contrario, por no actuar.', value:true, sign:'Ari' },
             { answer:'Atesorar demasiado y acumular innecesariamente', value:true, sign:'Tau' },
             { answer:'', value:true, sign:'Gem' },
+            { answer:'', value:true, sign:'Can' },
         ]
     }, { 
         question:'14. ¿Cómo debes tomar tus decisiones?', 
@@ -99,6 +112,7 @@ export const quiz3:iQuestion[] = [
             { answer:'Asertivas y con entusiasmo.', value:false, sign:'Ari' },
             { answer:'En base a mis valores', value:true, sign:'Tau' },
             { answer:'', value:true, sign:'Gem' },
+            { answer:'', value:true, sign:'Can' },
         ]
     }, 
 ]
