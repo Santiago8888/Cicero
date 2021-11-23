e2d9006e917c52d0b0ec2b1246936d0c22a910de
Santiago8888 <santiago.marti67@gmail.com>
2021-11-23 14:28:07 -0600

    Quiz 2.3 intro.

--- app/src/data/module-2/quiz3.ts
+++ app/src/data/module-2/quiz3.ts
@@ -3,17 +3,17 @@ import { iQuestion } from '../../components/Views/Quiz'
 
 export const quiz3:iQuestion[] = [
     { 
-        question:'', 
+        question:'1. ¿De que debes ser un ejemplo hoy?', 
         answers:[
             { answer:'', value:true, sign:'Ari' }
         ]
     }, { 
-        question:'', 
+        question:'2. ¿Qué vas a enseñar a otros?', 
         answers:[
             { answer:'', value:true, sign:'Ari' }
         ]
     }, { 
-        question:'', 
+        question:'3. ¿Qué aprenderás a traves de cumplir tu misión?', 
         answers:[
             { answer:'', value:true, sign:'Ari' }
         ]
