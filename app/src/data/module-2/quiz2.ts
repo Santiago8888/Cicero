a9be06e3e2a2bda33c9a44af0bbb704f8cfcb420
Santiago8888 <santiago.marti67@gmail.com>
2021-11-23 03:20:49 -0600

    Tau placeholder questions.

--- app/src/data/module-2/quiz2.ts
+++ app/src/data/module-2/quiz2.ts
@@ -5,56 +5,67 @@ export const quiz2:iQuestion[] = [
         question: '1. ¿Qué te restringe Saturno en esta vida?', 
         answers: [
             { answer:'El impulso y la energía', sign:'Ari', value:true },
+            { answer:'', sign:'Tau', value:true },
         ]
     }, {
         question:'2. ¿Para qué te restringe eso Saturno?',
         answers: [
             { answer:'Poder medir mi fuerza, canalizarla correctamente, dejar de insistir tantas veces para lograr algo y aprender de tus resultados', sign:'Ari', value:true },
+            { answer:'', sign:'Tau', value:true },
         ]
     }, {
         question:'3. ¿Para que sirven los miedos subconscientes que te genera Saturno?',
         answers: [
             { answer:'Detener mi agresividad, para pensar antes de actuar y reconocer que mis acciones tienen consecuencias', sign:'Ari', value:true },
+            { answer:'', sign:'Tau', value:true },
         ]
     }, {
         question:'4. ¿Qué es importante que aprendas en esta vida?',
         answers: [
             { answer:'Tacto y cooperación con los demás', sign:'Ari', value:true },
+            { answer:'', sign:'Tau', value:true },
         ]
     }, {
         question:'5. ¿Qué debes corregir ahora?',
         answers: [
             { answer:'Mi personalidad', sign:'Ari', value:true },
+            { answer:'', sign:'Tau', value:true },
         ]
     }, {
         question:'6. ¿En que consiste tu trabajo en esta vida?',
         answers: [
             { answer:'Hacerme de una nueva identidad en donde me sientas verdaderamente valiente y poderoso sin necesidad de demostrarlo', sign:'Ari', value:true },
+            { answer:'', sign:'Tau', value:true },
         ]
     }, {
         question:'7. ¿Qué debes saber en esta vida?',
         answers: [
             { answer:'Que puedo lograr cualquier meta y vencer cualquier reto', sign:'Ari', value:true },
+            { answer:'', sign:'Tau', value:true },
         ]
     }, {
         question:'8. ¿Qué desarrollaste en vidas pasadas?',
         answers: [
             { answer:'Una fuerte personalidad, enfrentando grandes desafíos', sign:'Ari', value:true },
+            { answer:'', sign:'Tau', value:true },
         ]
     }, {
         question:'9. ¿Qué sentimiento constante tuviste en vidas pasadas?',
         answers: [
             { answer:'Necesidad de sobrevivir', sign:'Ari', value:true },
+            { answer:'', sign:'Tau', value:true },
         ]
     }, {
         question:'10. ¿Cómo actuabas generalmente en vidas pasadas?',
         answers: [
             { answer:'Jefe militar, buscando hacerme respetar a toda costa.', sign:'Ari', value:true },
+            { answer:'', sign:'Tau', value:true },
         ]
     }, {
         question:'11. ¿Qué otras actividades tuviste en vidas pasdas?',
         answers: [
             { answer:'Haber buscado la supervivencia en ambientes inhóspitos, en situaciones de mucho riesgo o peligro y actividades que requieren de mucha energía.', sign:'Ari', value:true },
+            { answer:'', sign:'Tau', value:true },
         ]
     }
 ]
