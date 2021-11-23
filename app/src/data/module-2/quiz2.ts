38615c82f5d51860de6860150adfd4a7c8d51b8d
Santiago8888 <santiago.marti67@gmail.com>
2021-11-22 18:40:17 -0600

    Question 1: restriction.

--- app/src/data/module-2/quiz2.ts
+++ app/src/data/module-2/quiz2.ts
@@ -1,1 +1,11 @@
-export const quiz2 = []
+import { iQuestion } from '../../components/Views/Quiz'
+
+export const quiz2:iQuestion[] = [
+    { 
+        question: '1. ¿Qué te restringe Saturno en esta vida?', 
+        answers: [
+            { answer:'El impulso y la energía', sign:'Ari', value:true },
+        ]
+    }
+
+]
