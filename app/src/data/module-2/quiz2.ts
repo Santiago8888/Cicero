9687d81452aefee14e1a4bf06af71709d435db1f
Santiago8888 <santiago.marti67@gmail.com>
2021-11-24 12:08:23 -0600

    Quiz: Virgo placeholder karma answers.

--- app/src/data/module-2/quiz2.ts
+++ app/src/data/module-2/quiz2.ts
@@ -9,6 +9,7 @@ export const quiz2:iQuestion[] = [
             { answer:'La habilidad para comunicarme con los demás.', sign:'Gem', value:true },
             { answer:'La capacidad de sentir armonía y protección de tu familia.', sign:'Can', value:true },
             { answer:'La fama personal y liderazgo.', sign:'Leo', value:true },
+            { answer:'.', sign:'Vir', value:true },
         ]
     }, {
         question:'2. ¿Para qué te restringe eso Saturno?',
@@ -18,6 +19,7 @@ export const quiz2:iQuestion[] = [
             { answer:'Aprender a hacerlo de manera clara, sincera y abierta, verbalizar mis ideas, dejar de manipular y mantener una postura adecuada y congruente.', sign:'Gem', value:true },
             { answer:'Encuentre afinidad, acoplamiento y verdadero bienestar, que aprenda a manejar mis emociones, respetar la jerarquía de mis padres y regresar la responsabilidad a cada integrante de la familia.', sign:'Can', value:true },
             { answer:'Desarrollar una mejor actitud hacia el amor, evitar la rigidez y disfrutar del momento.', sign:'Leo', value:true },
+            { answer:'.', sign:'Vir', value:true },
         ]
     }, {
         question:'3. ¿Para que sirven los miedos subconscientes que te genera Saturno?',
@@ -27,6 +29,7 @@ export const quiz2:iQuestion[] = [
             { answer:'No convencer a los demás, pensar antes de hablar, no caer en chismes y no esforzarme demasiado en caer bien o hacer reír a los demás.', sign:'Gem', value:true },
             { answer:'Dejar de controlar la dinámica, desempeñar solo el papel que me corresponde sin victimizarme, chantajear o manipular.', sign:'Can', value:true },
             { answer:'Desarrollar humildad y saber que no es necesario controlar tu audiencia.', sign:'Leo', value:true },
+            { answer:'.', sign:'Vir', value:true },
         ]
     }, {
         question:'4. ¿Qué es importante que aprendas en esta vida?',
@@ -36,6 +39,7 @@ export const quiz2:iQuestion[] = [
             { answer:'Decir que no.', sign:'Gem', value:true },
             { answer:'Respeto, obediencia y aceptación de la forma de ser de cada miembro de la familia', sign:'Can', value:true },
             { answer:'La autoaceptación, el sentido del humor y la apreciación..', sign:'Leo', value:true },
+            { answer:'.', sign:'Vir', value:true },
         ]
     }, {
         question:'5. ¿Qué es lo que ahora debes corregir?',
@@ -45,6 +49,7 @@ export const quiz2:iQuestion[] = [
             { answer:'Mi comunicación', sign:'Gem', value:true },
             { answer:'Mis emociones, inhibiciones y necesidad de control', sign:'Can', value:true },
             { answer:'Mi capacidad de gozar.', sign:'Leo', value:true },
+            { answer:'.', sign:'Vir', value:true },
         ]
     }, {
         question:'6. ¿En que consiste tu trabajo en esta vida?',
@@ -54,6 +59,7 @@ export const quiz2:iQuestion[] = [
             { answer:'Ser auténtico, poder expresar mis ideas congruentemente sin necesidad de ser aceptado o querido', sign:'Gem', value:true },
             { answer:'Soltar y dejar que las cosas lleven su proceso, aceptar que los que mandan son tus padres', sign:'Can', value:true },
             { answer:'Relajarte, mostrarte como eres, dejar de tratar de controlar lo que el mundo piensa de tí.', sign:'Leo', value:true },
+            { answer:'.', sign:'Vir', value:true },
         ]
     }, {
         question:'7. ¿Qué debes saber en esta vida?',
@@ -63,6 +69,7 @@ export const quiz2:iQuestion[] = [
             { answer:'No tengo que pensar como los demás ni discutir para convencerlos', sign:'Gem', value:true },
             { answer:'Cada quien madure a su ritmo', sign:'Can', value:true },
             { answer:'Como ser verdaderamente agradecido.', sign:'Leo', value:true },
+            { answer:'.', sign:'Vir', value:true },
         ]
     }, {
         question:'8. ¿Qué desarrollaste en vidas pasadas?',
@@ -72,6 +79,7 @@ export const quiz2:iQuestion[] = [
             { answer:'Gran habilidad de comunicación', sign:'Gem', value:true },
             { answer:'Una fuerte responsabilidad familiar', sign:'Can', value:true },
             { answer:'un fuerte liderazgo.', sign:'Leo', value:true },
+            { answer:'.', sign:'Vir', value:true },
         ]
     }, {
         question:'9. ¿Qué sentimiento constante tuviste en vidas pasadas?',
@@ -81,6 +89,7 @@ export const quiz2:iQuestion[] = [
             { answer:'La necesidad de saber todo lo que sucedía en todo momento', sign:'Gem', value:true },
             { answer:'Falta de madurez de parte de tus familiares', sign:'Can', value:true },
             { answer:'Tener que hacer un buen papel.', sign:'Leo', value:true },
+            { answer:'.', sign:'Vir', value:true },
         ]
     }, {
         question:'10. ¿Cómo actuabas generalmente en vidas pasadas?',
@@ -90,6 +99,7 @@ export const quiz2:iQuestion[] = [
             { answer:'Una persona conocida conocida luchabas por caerle bien a todos, ser simpátic@ y complaciente.', sign:'Gem', value:true },
             { answer:'Creía que al hacer todo por las personas que amabas era la manera de demostrar tu amor', sign:'Can', value:true },
             { answer:'De forma rígida y deje de permitir que mi espíritu lúdico se expresara.', sign:'Leo', value:true },
+            { answer:'.', sign:'Vir', value:true },
         ]
     }, {
         question:'11. ¿Qué otras actividades tuviste en vidas pasdas?',
@@ -99,6 +109,7 @@ export const quiz2:iQuestion[] = [
             { answer:'Informante, locutor, periodista, transportista, comerciante, vendedor, presentador o cómico.', sign:'Gem', value:true },
             { answer:'Madre o padre de familia, abuel@ progenitor poderos@ o adinerad@, jefe o administrador de empresa, industria, escuela, hospital, casa hogar, asilo, restaurante o desempeñado alguna actividad ecológica o relacionada con animales', sign:'Can', value:true },
             { answer:'Actor, artista, cantante, maestr@ de ceremonias, gobernante, o cualquier tipo de líder profesional, empresarial o de sociedad.', sign:'Leo', value:true },
+            { answer:'.', sign:'Vir', value:true },
         ]
     }
 ]
