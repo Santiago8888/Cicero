1193d2ef213912e05dedb3af909d635aadc8fe37
Santiago8888 <santiago.marti67@gmail.com>
2021-11-30 11:46:49 -0600

    Reorder questions for karma.

--- app/src/data/module-2/quiz2.ts
+++ app/src/data/module-2/quiz2.ts
@@ -1,93 +1,8 @@
 import { iQuestion } from '../../components/Views/Quiz'
 
-export const quiz2:iQuestion[] = [
-    { 
+export const quiz2:iQuestion[] = [{
         sign:true,
-        question: '1. ¿Qué te restringe Saturno en esta vida?', 
-        answers: [
-            { answer:'El impulso y la energía.', sign:'Ari', value:true },
-            { answer:'Poder ahorrar grandes cantidades de dinero.', sign:'Tau', value:true },
-            { answer:'La habilidad para comunicarme con los demás.', sign:'Gem', value:true },
-            { answer:'La capacidad de sentir armonía y protección de tu familia.', sign:'Can', value:true },
-            { answer:'La fama personal y liderazgo.', sign:'Leo', value:true },
-            { answer:'La salud cuando hay desequilibrio entre mi mente y cuerpo.', sign:'Vir', value:true },
-            { answer:'La armonía y el equilibrio para establecer una sociedad o matrimonio.', sign:'Lib', value:true },
-        ]
-    }, {
-        sign:true,
-        question:'2. ¿Para qué te restringe eso Saturno?',
-        answers: [
-            { answer:'Poder medir mi fuerza, canalizarla correctamente, dejar de insistir tantas veces para lograr algo y aprender de tus resultados.', sign:'Ari', value:true },
-            { answer:'Deshacerme de los bienes a los que me apego: aquellos que solo me están haciendo la vida más difícil o pesada.', sign:'Tau', value:true },
-            { answer:'Aprender a hacerlo de manera clara, sincera y abierta, verbalizar mis ideas, dejar de manipular y mantener una postura adecuada y congruente.', sign:'Gem', value:true },
-            { answer:'Encuentre afinidad, acoplamiento y verdadero bienestar, que aprenda a manejar mis emociones, respetar la jerarquía de mis padres y regresar la responsabilidad a cada integrante de la familia.', sign:'Can', value:true },
-            { answer:'Desarrollar una mejor actitud hacia el amor, evitar la rigidez y disfrutar del momento.', sign:'Leo', value:true },
-            { answer:'Poder vivir de manera integral, trabajar sin estrés, dejar de ser tan perfeccionista, obsesionarme por los detalles, soltar y confiar al terminar las tareas.', sign:'Vir', value:true },
-            { answer:'Aprender a cooperar y compartir sin perder mi identidad e independencia.', sign:'Lib', value:true },
-        ]
-    }, {
-        sign:true,
-        question:'3. ¿Para que sirven los miedos subconscientes que te genera Saturno?',
-        answers: [
-            { answer:'Detener mi agresividad, para pensar antes de actuar y reconocer que mis acciones tienen consecuencias.', sign:'Ari', value:true },
-            { answer:'No poder invertir en demasiados negocios, para que no endeudarme, evitar apostar o especular y para que no busquar oportunidades de acumulación.', sign:'Tau', value:true },
-            { answer:'No tratar de convencer a los demás, pensar antes de hablar, evitar caer en chismes y no esforzarme demasiado en caer bien o hacer reír a los demás.', sign:'Gem', value:true },
-            { answer:'Dejar de controlar la dinámica familiar, desempeñar solo el papel que me corresponde sin victimizarme, chantajear o manipular.', sign:'Can', value:true },
-            { answer:'Desarrollar humildad y saber que no es necesario controlar mi audiencia.', sign:'Leo', value:true },
-            { answer:'Aceptar que no puedo controlarlo todo, y trabajar la fe y la espiritualidad.', sign:'Vir', value:true },
-            { answer:'Lograr tomar decisiones y saber cuándo ceder.', sign:'Lib', value:true },
-        ]
-    }, {
-        sign:true,
-        question:'4. ¿Qué es importante que aprendas en esta vida?',
-        answers: [
-            { answer:'Tacto y cooperación con los demás.', sign:'Ari', value:true },
-            { answer:'Valorar los recursos y a las personas.', sign:'Tau', value:true },
-            { answer:'Decir que no.', sign:'Gem', value:true },
-            { answer:'Respeto, obediencia y aceptación de la forma de ser de cada miembro de la familia', sign:'Can', value:true },
-            { answer:'La autoaceptación, el sentido del humor y la apreciación..', sign:'Leo', value:true },
-            { answer:'Liberarme de la tensión y concentrarme en manifestar mi visión abstracta de las cosas.', sign:'Vir', value:true },
-            { answer:'Considerar lo que mi pareja o socio aporta a la relación antes de quejarme o reclamar.', sign:'Lib', value:true },
-        ]
-    }, {
-        sign:true,
-        question:'5. ¿Qué es lo que ahora debes corregir?',
-        answers: [
-            { answer:'Mi personalidad.', sign:'Ari', value:true },
-            { answer:'Mi sistema de valores.', sign:'Tau', value:true },
-            { answer:'Mi comunicación', sign:'Gem', value:true },
-            { answer:'Mis emociones, inhibiciones y necesidad de control', sign:'Can', value:true },
-            { answer:'Mi capacidad de gozar.', sign:'Leo', value:true },
-            { answer:'Mi nerviosismo y exceso de preocupación.', sign:'Vir', value:true },
-            { answer:'Mi sentimiento de injusticia, abuso y tu dependencia.', sign:'Lib', value:true },
-        ]
-    }, {
-        sign:true,
-        question:'6. ¿En que consiste tu trabajo en esta vida?',
-        answers: [
-            { answer:'Hacerme de una nueva identidad en donde me sientas verdaderamente valiente y poderoso sin necesidad de demostrarlo.', sign:'Ari', value:true },
-            { answer:'Confiar en que mis capacidades me darán siempre todo lo que necesito, sin necesidad de acumular excesivamente para el futuro.', sign:'Tau', value:true },
-            { answer:'Ser auténtico, poder expresar mis ideas congruentemente sin necesidad de ser aceptado o querido', sign:'Gem', value:true },
-            { answer:'Soltar y dejar que las cosas lleven su proceso, aceptar que los que mandan son tus padres', sign:'Can', value:true },
-            { answer:'Relajarte, mostrarte como eres, dejar de tratar de controlar lo que el mundo piensa de tí.', sign:'Leo', value:true },
-            { answer:'Encargarme de mi salud y dejar de somatizar mis miedos.', sign:'Vir', value:true },
-            { answer:'Ser justo, equilibrado, diplomático, comprender que tu socio o pareja te da lo que te corresponde  y no esperar a que cambie.', sign:'Lib', value:true },
-        ]
-    }, {
-        sign:true,
-        question:'7. ¿Qué debes saber en esta vida?',
-        answers: [
-            { answer:'Que puedo lograr cualquier meta y vencer cualquier reto.', sign:'Ari', value:true },
-            { answer:'Al ser honesto, la vida me traerá personas honestas.', sign:'Tau', value:true },
-            { answer:'No tengo que pensar como los demás ni discutir para convencerlos', sign:'Gem', value:true },
-            { answer:'Cada quien madure a su ritmo', sign:'Can', value:true },
-            { answer:'Como ser verdaderamente agradecido.', sign:'Leo', value:true },
-            { answer:'Voy a estar san@ y me sientas tranquil@.', sign:'Vir', value:true },
-            { answer:'Las relaciones solo funcionan cuando hay equilibrio en el dar y recibir.', sign:'Lib', value:true },
-        ]
-    }, {
-        sign:true,
-        question:'8. ¿Qué desarrollaste en vidas pasadas?',
+        question:'1. ¿Qué desarrollaste en vidas pasadas?',
         answers: [
             { answer:'Una fuerte personalidad.', sign:'Ari', value:true },
             { answer:'Una fuerte necesidad de seguridad.', sign:'Tau', value:true },
@@ -99,7 +14,7 @@ export const quiz2:iQuestion[] = [
         ]
     }, {
         sign:true,
-        question:'9. ¿Qué sentimiento constante tuviste en vidas pasadas?',
+        question:'2. ¿Qué sentimiento constante tuviste en vidas pasadas?',
         answers: [
             { answer:'Necesidad de sobrevivir.', sign:'Ari', value:true },
             { answer:'Que la sociedad valoraba solo a quienes tenían bienes y dinero.', sign:'Tau', value:true },
@@ -111,7 +26,7 @@ export const quiz2:iQuestion[] = [
         ]
     }, {
         sign:true,
-        question:'10. ¿Cómo actuabas generalmente en vidas pasadas?',
+        question:'3. ¿Cómo actuabas generalmente en vidas pasadas?',
         answers: [
             { answer:'Como jefe militar, buscando hacerme respetar a toda costa.', sign:'Ari', value:true },
             { answer:'Con gran habilidad financiera y administrativa.', sign:'Tau', value:true },
@@ -123,7 +38,7 @@ export const quiz2:iQuestion[] = [
         ]
     }, {
         sign:true,
-        question:'11. ¿Qué otras actividades tuviste en vidas pasdas?',
+        question:'4. ¿Qué otras actividades tuviste en vidas pasdas?',
         answers: [
             { answer:'Haber buscado la supervivencia en ambientes inhóspitos, en situaciones de mucho riesgo o peligro y actividades que requieren de mucha energía.', sign:'Ari', value:true },
             { answer:'Trabajar en bancos o empresas financieras, ser aficionado a los juegos de azar, la especulación, comerciante, empresario y hasta inversionista', sign:'Tau', value:true },
@@ -132,6 +47,90 @@ export const quiz2:iQuestion[] = [
             { answer:'Actor, artista, cantante, maestr@ de ceremonias, gobernante, o cualquier tipo de líder profesional, empresarial o de sociedad.', sign:'Leo', value:true },
             { answer:'Enfermer@, trabajador social, investigador@, administrador y encargado de cualquier área de salud o donde el servicio era lo más importante.', sign:'Vir', value:true },
             { answer:'Juez, abogado, cónsul, consejero, diplomático, o cualquier tipo de mediador donde excluí mi identidad personal a favor de una sociedad.', sign:'Lib', value:true },
+        ]
+    }, { 
+        sign:true,
+        question: '5. ¿Qué te restringe Saturno en esta vida?', 
+        answers: [
+            { answer:'El impulso y la energía.', sign:'Ari', value:true },
+            { answer:'Poder ahorrar grandes cantidades de dinero.', sign:'Tau', value:true },
+            { answer:'La habilidad para comunicarme con los demás.', sign:'Gem', value:true },
+            { answer:'La capacidad de sentir armonía y protección de tu familia.', sign:'Can', value:true },
+            { answer:'La fama personal y liderazgo.', sign:'Leo', value:true },
+            { answer:'La salud cuando hay desequilibrio entre mi mente y cuerpo.', sign:'Vir', value:true },
+            { answer:'La armonía y el equilibrio para establecer una sociedad o matrimonio.', sign:'Lib', value:true },
+        ]
+    }, {
+        sign:true,
+        question:'6. ¿Para qué te restringe eso Saturno?',
+        answers: [
+            { answer:'Poder medir mi fuerza, canalizarla correctamente, dejar de insistir tantas veces para lograr algo y aprender de tus resultados.', sign:'Ari', value:true },
+            { answer:'Deshacerme de los bienes a los que me apego: aquellos que solo me están haciendo la vida más difícil o pesada.', sign:'Tau', value:true },
+            { answer:'Aprender a hacerlo de manera clara, sincera y abierta, verbalizar mis ideas, dejar de manipular y mantener una postura adecuada y congruente.', sign:'Gem', value:true },
+            { answer:'Encuentre afinidad, acoplamiento y verdadero bienestar, que aprenda a manejar mis emociones, respetar la jerarquía de mis padres y regresar la responsabilidad a cada integrante de la familia.', sign:'Can', value:true },
+            { answer:'Desarrollar una mejor actitud hacia el amor, evitar la rigidez y disfrutar del momento.', sign:'Leo', value:true },
+            { answer:'Poder vivir de manera integral, trabajar sin estrés, dejar de ser tan perfeccionista, obsesionarme por los detalles, soltar y confiar al terminar las tareas.', sign:'Vir', value:true },
+            { answer:'Aprender a cooperar y compartir sin perder mi identidad e independencia.', sign:'Lib', value:true },
+        ]
+    }, {
+        sign:true,
+        question:'7. ¿Para que sirven los miedos subconscientes que te genera Saturno?',
+        answers: [
+            { answer:'Detener mi agresividad, para pensar antes de actuar y reconocer que mis acciones tienen consecuencias.', sign:'Ari', value:true },
+            { answer:'No poder invertir en demasiados negocios, para que no endeudarme, evitar apostar o especular y para que no busquar oportunidades de acumulación.', sign:'Tau', value:true },
+            { answer:'No tratar de convencer a los demás, pensar antes de hablar, evitar caer en chismes y no esforzarme demasiado en caer bien o hacer reír a los demás.', sign:'Gem', value:true },
+            { answer:'Dejar de controlar la dinámica familiar, desempeñar solo el papel que me corresponde sin victimizarme, chantajear o manipular.', sign:'Can', value:true },
+            { answer:'Desarrollar humildad y saber que no es necesario controlar mi audiencia.', sign:'Leo', value:true },
+            { answer:'Aceptar que no puedo controlarlo todo, y trabajar la fe y la espiritualidad.', sign:'Vir', value:true },
+            { answer:'Lograr tomar decisiones y saber cuándo ceder.', sign:'Lib', value:true },
+        ]
+    }, {
+        sign:true,
+        question:'8. ¿Qué es importante que aprendas en esta vida?',
+        answers: [
+            { answer:'Tacto y cooperación con los demás.', sign:'Ari', value:true },
+            { answer:'Valorar los recursos y a las personas.', sign:'Tau', value:true },
+            { answer:'Decir que no.', sign:'Gem', value:true },
+            { answer:'Respeto, obediencia y aceptación de la forma de ser de cada miembro de la familia', sign:'Can', value:true },
+            { answer:'La autoaceptación, el sentido del humor y la apreciación..', sign:'Leo', value:true },
+            { answer:'Liberarme de la tensión y concentrarme en manifestar mi visión abstracta de las cosas.', sign:'Vir', value:true },
+            { answer:'Considerar lo que mi pareja o socio aporta a la relación antes de quejarme o reclamar.', sign:'Lib', value:true },
+        ]
+    }, {
+        sign:true,
+        question:'9. ¿Qué es lo que ahora debes corregir?',
+        answers: [
+            { answer:'Mi personalidad.', sign:'Ari', value:true },
+            { answer:'Mi sistema de valores.', sign:'Tau', value:true },
+            { answer:'Mi comunicación', sign:'Gem', value:true },
+            { answer:'Mis emociones, inhibiciones y necesidad de control', sign:'Can', value:true },
+            { answer:'Mi capacidad de gozar.', sign:'Leo', value:true },
+            { answer:'Mi nerviosismo y exceso de preocupación.', sign:'Vir', value:true },
+            { answer:'Mi sentimiento de injusticia, abuso y tu dependencia.', sign:'Lib', value:true },
+        ]
+    }, {
+        sign:true,
+        question:'10. ¿En que consiste tu trabajo en esta vida?',
+        answers: [
+            { answer:'Hacerme de una nueva identidad en donde me sientas verdaderamente valiente y poderoso sin necesidad de demostrarlo.', sign:'Ari', value:true },
+            { answer:'Confiar en que mis capacidades me darán siempre todo lo que necesito, sin necesidad de acumular excesivamente para el futuro.', sign:'Tau', value:true },
+            { answer:'Ser auténtico, poder expresar mis ideas congruentemente sin necesidad de ser aceptado o querido', sign:'Gem', value:true },
+            { answer:'Soltar y dejar que las cosas lleven su proceso, aceptar que los que mandan son tus padres', sign:'Can', value:true },
+            { answer:'Relajarte, mostrarte como eres, dejar de tratar de controlar lo que el mundo piensa de tí.', sign:'Leo', value:true },
+            { answer:'Encargarme de mi salud y dejar de somatizar mis miedos.', sign:'Vir', value:true },
+            { answer:'Ser justo, equilibrado, diplomático, comprender que tu socio o pareja te da lo que te corresponde  y no esperar a que cambie.', sign:'Lib', value:true },
+        ]
+    }, {
+        sign:true,
+        question:'11. ¿Qué debes saber en esta vida?',
+        answers: [
+            { answer:'Que puedo lograr cualquier meta y vencer cualquier reto.', sign:'Ari', value:true },
+            { answer:'Al ser honesto, la vida me traerá personas honestas.', sign:'Tau', value:true },
+            { answer:'No tengo que pensar como los demás ni discutir para convencerlos', sign:'Gem', value:true },
+            { answer:'Cada quien madure a su ritmo', sign:'Can', value:true },
+            { answer:'Como ser verdaderamente agradecido.', sign:'Leo', value:true },
+            { answer:'Voy a estar san@ y me sientas tranquil@.', sign:'Vir', value:true },
+            { answer:'Las relaciones solo funcionan cuando hay equilibrio en el dar y recibir.', sign:'Lib', value:true },
         ]
     }
 ]
