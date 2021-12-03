e032f9ff2360e6fa989e7f35f3552cb5cd8d9cdb
Santiago8888 <santiago.marti67@gmail.com>
2021-12-02 20:24:24 -0600

    Ortography on posts.

--- app/src/data/module-2/posts.ts
+++ app/src/data/module-2/posts.ts
@@ -1,10 +1,10 @@
 export const posts1 = [
     '¿En qué momento comenzaste a creer en las vidas pasadas?',
-    '¿En qué notaste desde niñ@ que erás especialmente buen@ comparado a los demás?',
+    '¿En qué notaste desde niñ@ que eras especialmente buen@ comparado a los demás?',
     'Durante el día de hoy escucha con atención a tu conciencia, ¿Qué te dice, que gustes compartir?',
     'El primer ciclo de Saturno es a los 7 años. Compartenos una experiencia alrededor de esa edad que te hizo madurar:',
     'De acuerdo a la lista de cualidades del signo de tu Saturno, cuenta una experiencia en la que hayas visto a alguien actuar impecablemente:',
-    '¿En qué actitudes te acuerdas que tus padres hayan sido particularmente exigentes contigo?',
+    '¿En qué actitudes te acuerdas que tus padres fueron particularmente exigentes contigo?',
     '¿Cómo te imaginas que haya sido alguna de tus vidas pasadas?',
     '¿Cuáles actitudes o comportamientos has notado que son aceptables para los demás pero no para ti?',
     '¿Cuál de las cualidades del signo de tu Saturno te gustaría mostrar más?'
