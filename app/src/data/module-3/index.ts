372d90b7a911acdb3510ab367ea8892562b20a76
Santiago8888 <santiago.marti67@gmail.com>
2021-12-25 17:16:08 -0600

    Posts 1 description.

--- app/src/data/module-3/index.ts
+++ app/src/data/module-3/index.ts
@@ -2,6 +2,8 @@ import { posts1 } from './posts'
 import { iLesson } from '../../components/LayOut/Menu'
 import { quiz1 } from './quiz1'
 
+const posts1Description = ['Después de haber escuchado algunos ejemplos y características de la influencia de Saturno en las casas. Comparte tu experiencia con alguna de las siguientes preguntas sobre como ha influido Saturno en tu vida.']
+
 const quiz31Description = [
     'Después de escuchar las explicaciones de las casas, te invitamos a que hagas una pausa y reflexiones sobre el estado actual de tu vida.',
     'No hay respuestas incorrectas, este es un ejercicio para que adquieras más consciencia sobre lo que te gusta y lo que te gustaría mejorar de tu vida presente.'
@@ -11,7 +13,7 @@ const lessons1:iLesson[] = [
     { type:'Video', title:'Las casas en la astrología', link:'https://youtu.be/bvQh_9PCos8' },
     { type:'Quiz', title:'Auto Diagnóstico', description:quiz31Description, questions:quiz1 },
     { type:'Video', title:'La influencia de Saturno', link:'https://youtu.be/JtfC2DSLxVE' },
-    { type:'Reflection', title:'Ejercicio de Compartir', posts:posts1, numbered:true }
+    { type:'Reflection', title:'Ejercicio de Compartir', posts:posts1, numbered:true, description:posts1Description }
 ]
 
 const lessons2:iLesson[] = [
