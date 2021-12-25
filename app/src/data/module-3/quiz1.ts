ba3f7021f864f1b9e19c729cf92d700993813319
Santiago8888 <santiago.marti67@gmail.com>
2021-12-25 15:29:59 -0600

    Edit on questions 3 & 4.

--- app/src/data/module-3/quiz1.ts
+++ app/src/data/module-3/quiz1.ts
@@ -4,58 +4,58 @@ export const quiz1:iQuestion[] = [
     { 
         question: '1. ¿Cuál es la casa en la que te sientes más satisfech@ actualmente?', 
         answers:[
-            { answer:'Casa 7, la pareja.', value:true },
-            { answer:'Casa 10, mi profesión.', value:true },
-            { answer:'Casa 12, la espiritualidad.', value:true },
-            { answer:'Casa 6, mi salud.', value:true },
+            { answer:'Casa 4: la familia.', value:true },
+            { answer:'Casa 6: el trabajo.', value:true },
+            { answer:'Casa 7: la pareja.', value:true },
+            { answer:'Casa 12: la espiritualidad.', value:true },
         ]
     }, {
-        question:'2. ¿Cuál es la casa a la que sientes que le necesitas dedicar más tiempo o atención??',
+        question:'2. ¿Cuál es la casa a la que sientes que le necesitas dedicar más tiempo o atención?',
         answers:[
-            { answer:'Casa 1, mi yo personal.', value:true },
-            { answer:'Casa 5, el gozo y la creatividad.', value:true },
-            { answer:'Casa 10, mi profesión.', value:true },
-            { answer:'Casa 11, la ayuda desinteresada.', value:true },
+            { answer:'Casa 1: mi yo personal.', value:true },
+            { answer:'Casa 5: el gozo y la creatividad.', value:true },
+            { answer:'Casa 10: mi profesión.', value:true },
+            { answer:'Casa 11: la ayuda desinteresada.', value:true },
         ]
     }, {
         question:'3. ¿Qué es aquello que crees que puedes dar u obsequiar más a los demas?',
         answers:[
-            { answer:'Casa 3, comunicación e información.', value:true },
-            { answer:'Casa 2, dinero y valores.', value:true },
-            { answer:'Casa 12, mi entendimiento, comprensión y tiempo.', value:true },
-            { answer:'Casa 9, conocimiento y visión.', value:true }
+            { answer:'Casa 2: dinero y valores.', value:true },
+            { answer:'Casa 3: información y comunicación.', value:true },
+            { answer:'Casa 9: mi visión y filosofía.', value:true },
+            { answer:'Casa 12: entendimiento, comprensión y tiempo.', value:true },
         ]
     }, {
         question:'4. ¿Qué area de la vida disfrutas más?',
         answers:[
-            { answer:'Casa 4, el hogar.', value:true },
-            { answer:'Casa 8, momentos de instrospección)', value:true },
-            { answer:'Casa 7, la pareja.', value:true },
-            { answer:'Casa 10, mi profesión.', value:true }
+            { answer:'Casa 4: el hogar.', value:true },
+            { answer:'Casa 7: la pareja.', value:true },
+            { answer:'Casa 8: mis momentos de instrospección.', value:true },
+            { answer:'Casa 10: mi profesión.', value:true }
         ]
     }, {
         question:'5. ¿Qué casa quieres llevar a la excelencia?',
         answers:[
-            { answer:'Casa 1, mi yo personal.', value:true },
-            { answer:'Casa 9, mi sabiduria y conocimiento.', value:true },
-            { answer:'Casa 11, mi ayuda a la comunidad.', value:true },
-            { answer:'Casa 12, mi espiritualidad.', value:true }
+            { answer:'Casa 1: mi yo personal.', value:true },
+            { answer:'Casa 9: mi sabiduria y conocimiento.', value:true },
+            { answer:'Casa 11: mi ayuda a la comunidad.', value:true },
+            { answer:'Casa 12: mi espiritualidad.', value:true }
         ]
     }, {
         question:'6. ¿En que área quieres mejorar?',
         answers:[
-            { answer:'Casa 6, la salud.', value:true },
-            { answer:'Casa 7, la pareja.', value:true },
-            { answer:'Casa 2, el dinero.', value:true },
-            { answer:'Casa 8, mi yo interno.', value:true }
+            { answer:'Casa 6: la salud.', value:true },
+            { answer:'Casa 7:, la pareja.', value:true },
+            { answer:'Casa 2: el dinero.', value:true },
+            { answer:'Casa 8: mi yo interno.', value:true }
         ]
     }, {
         question:'7. ¿En que área te conectas más, recibes más luz?',
         answers:[
             { answer:'Casas 4, 5 o 7: la familia, mis hijos o pareja.', value:true },
-            { answer:'Casas 1 u 8, estando conmigo mism@.', value:true },
-            { answer:'Casas 9 u 12, mi filosofía o espiritualidad.', value:true },
-            { answer:'Casas 6 o 10, mi trabajo o profesión.', value:true }
+            { answer:'Casas 1 u 8: estando conmigo mism@.', value:true },
+            { answer:'Casas 9 u 12: mi filosofía o espiritualidad.', value:true },
+            { answer:'Casas 6 o 10: mi trabajo o profesión.', value:true }
         ]
     }
 ]
