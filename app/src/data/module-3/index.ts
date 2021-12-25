9e0f3669046dfc25f76c8d525a5183379d6e5fcd
Santiago8888 <santiago.marti67@gmail.com>
2021-12-25 14:41:06 -0600

    Unit 2 videos.

--- app/src/data/module-3/index.ts
+++ app/src/data/module-3/index.ts
@@ -6,12 +6,15 @@ const lessons1:iLesson[] = [
     { type:'Video', title:'La influencia de Saturno', link:'https://youtu.be/JtfC2DSLxVE' }
 ]
 
-const lessons2:iLesson[] = []
+const lessons2:iLesson[] = [
+    { type:'Video', title:'La casa de Saturno', link:'https://youtu.be/bvQh_9PCos8' },
+    { type:'Video', title:'Los ciclos de Saturno', link:'https://youtu.be/JtfC2DSLxVE' }
+]
 
 const lessons3:iLesson[] = []
 
 export const module3 = [
-    { title: '3.1 Saturno en las casas', lessons:lessons1 },
+    { title: '3.1 Las áreas de tu vida', lessons:lessons1 },
     { title: '3.2 Armonizando a Saturno', lessons:lessons2 },
     { title: '3.3 Saturno y el tiempo', lessons:lessons3 }
 ]
