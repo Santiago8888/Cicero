04a5d5d6f46dfb5767d0a9745d7c2160f226c498
Santiago8888 <santiago.marti67@gmail.com>
2021-12-25 14:35:39 -0600

    Module 3 empty lessons.

--- app/src/data/module-3/index.ts
+++ app/src/data/module-3/index.ts
@@ -1,5 +1,14 @@
+import { iLesson } from '../../components/LayOut/Menu'
+
+
+const lessons1:iLesson[] = []
+
+const lessons2:iLesson[] = []
+
+const lessons3:iLesson[] = []
+
 export const module3 = [
-    { title: '3.1 Saturno en las casas', lessons:[] },
-    { title: '3.2 Armonizando a Saturno', lessons:[] },
-    { title: '3.3 Saturno y el tiempo', lessons:[] }
+    { title: '3.1 Saturno en las casas', lessons:lessons1 },
+    { title: '3.2 Armonizando a Saturno', lessons:lessons2 },
+    { title: '3.3 Saturno y el tiempo', lessons:lessons3 }
 ]
