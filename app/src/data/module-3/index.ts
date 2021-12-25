79996ab767ab2f91313b31c483c82c86cf716fc0
Santiago8888 <santiago.marti67@gmail.com>
2021-12-25 14:29:57 -0600

    Declared module 3 units.

---
+++ app/src/data/module-3/index.ts
@@ -0,0 +1,5 @@
+export const module3 = [
+    { title: '3.1 Saturno en las casas', lessons:[] },
+    { title: '3.2 Armonizando a Saturno', lessons:[] },
+    { title: '3.3 Saturno y el tiempo', lessons:[] }
+]
