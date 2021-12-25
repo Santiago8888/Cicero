1d7e559fc7bf469fb52f42f5e8774b32be06a259
Santiago8888 <santiago.marti67@gmail.com>
2021-12-25 17:15:53 -0600

    Final edits on unit 3.1 suggested posts.

--- app/src/data/module-3/posts.ts
+++ app/src/data/module-3/posts.ts
@@ -1,8 +1,8 @@
 export const posts1 = [
-    'Comparte una experiencia en la que hayas sentido mucha responsabilidad en la casa de tu Saturno.',
-    'Saturno trae mucha conciencia a la casa en la que se encuentra. ¿De que eres consciente en esa área de tu vida?',
-    '¿Cuándo tuviste que luchar mucho por conseguir algo, producto de la limitación de Saturno?',
-    '¿Qué estructura tienes en la casa en la que se encuentra Saturno?',
-    '¿Qué le estás enseñando a los demás en la casa que tienes a Saturno?',
+    'Relata una experiencia en la que hayas sentido mucha responsabilidad en la casa de tu Saturno.',
+    'Saturno trae mucha conciencia a la casa en la que se encuentra. ¿De que eres consciente hoy en esa área de tu vida?',
+    '¿Cuándo tuviste que luchar mucho por conseguir algo, producto de la limitación que trae Saturno?',
+    '¿Qué estructura estás desarrollado en la casa en que se encuentra Saturno?',
+    '¿Qué le estás enseñando a los demás en esa área de tu vida en la que tienes a Saturno?',
     '¿Alguna vez no se te "perdono nada" en la casa de tu Saturno?'
 ]
