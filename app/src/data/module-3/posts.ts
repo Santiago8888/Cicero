87059cc6b96170dfceffc9196240076114ef1aa5
Santiago8888 <santiago.marti67@gmail.com>
2021-12-25 17:06:55 -0600

    Third post, limitation.

--- app/src/data/module-3/posts.ts
+++ app/src/data/module-3/posts.ts
@@ -1,4 +1,5 @@
 export const posts1 = [
     'Comparte una experiencia en la que hayas sentido mucha responsabilidad en la casa de tu Saturno.',
     'Saturno trae mucha conciencia a la casa en la que se encuentra. ¿De que eres consciente en esa área de tu vida?',
+    '¿Cuándo tuviste que luchar mucho por conseguir algo, producto de la limitación de Saturno?',
 ]
