adc6b7819595b9eb68d8bf83c580c180bc3ae44c
Santiago8888 <santiago.marti67@gmail.com>
2021-12-25 14:51:55 -0600

    Quiz 3.1: first question, fulfillment.

---
+++ app/src/data/module-3/quiz1.ts
@@ -0,0 +1,13 @@
+import { iQuestion } from "../../components/Views/Quiz"
+
+export const quiz1:iQuestion[] = [
+    { 
+        question: '1. ¿Cuál es la casa en la que te sientes más satisfech@ actualmente?', 
+        answers:[
+            { answer:'Casa 7 (pareja)', value:true },
+            { answer:'Casa 10 (profesión)', value:true },
+            { answer:'Casa 12 (espiritualidad)', value:true },
+            { answer:'Casa 6 (salud)', value:true },
+        ]
+    }    
+]
