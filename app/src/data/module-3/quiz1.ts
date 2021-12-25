1563a1b4a0b42b6057b54fd3346e6e640c906967
Santiago8888 <santiago.marti67@gmail.com>
2021-12-25 15:17:37 -0600

    Edit on questions 1 & 2.

--- app/src/data/module-3/quiz1.ts
+++ app/src/data/module-3/quiz1.ts
@@ -4,25 +4,25 @@ export const quiz1:iQuestion[] = [
     { 
         question: '1. ¿Cuál es la casa en la que te sientes más satisfech@ actualmente?', 
         answers:[
-            { answer:'Casa 7: pareja.', value:true },
-            { answer:'Casa 10: profesión.', value:true },
-            { answer:'Casa 12: espiritualidad.', value:true },
-            { answer:'Casa 6: salud.', value:true },
+            { answer:'Casa 7, la pareja.', value:true },
+            { answer:'Casa 10, mi profesión.', value:true },
+            { answer:'Casa 12, la espiritualidad.', value:true },
+            { answer:'Casa 6, mi salud.', value:true },
         ]
     }, {
-        question:'2. ¿¿Cuál es la casa a la que sientes que le necesitas dedicar más tiempo o atención??',
+        question:'2. ¿Cuál es la casa a la que sientes que le necesitas dedicar más tiempo o atención??',
         answers:[
-            { answer:'Casa 4: la familia.', value:true },
-            { answer:'Casa 1: tu yo personal.', value:true },
-            { answer:'Casa 5: el gozo.', value:true },
-            { answer:'Casa 11: ayuda desinteresada.', value:true },
+            { answer:'Casa 4, a la familia.', value:true },
+            { answer:'Casa 1, mi yo personal.', value:true },
+            { answer:'Casa 5, el gozo y la creatividad.', value:true },
+            { answer:'Casa 11, la ayuda desinteresada.', value:true },
         ]
     }, {
         question:'3. ¿Qué es aquello que crees que puedes dar u obsequiar más a los demas?',
         answers:[
             { answer:'Casa 3: comunicación e información.', value:true },
             { answer:'Casa 2: dinero y valores.', value:true },
-            { answer:'Casa 12: servicio y tiempo.', value:true },
+            { answer:'Casa 12: mi entendimiento, comprensión y tiempo.', value:true },
             { answer:'Casa 9: conocimiento y visión.', value:true }
         ]
     }, {
