ed4a59d288fd6d13058da10ebf5adfe371a98bfb
Santiago8888 <santiago.marti67@gmail.com>
2021-12-26 17:45:10 -0600

    Questions on first circle of admiration.

--- app/src/data/module-3/quiz2.ts
+++ app/src/data/module-3/quiz2.ts
@@ -2,7 +2,7 @@ import { iQuestion } from "../../components/Views/Quiz"
 
 export const quiz1:iQuestion[] = [
     {
-        question:'¿Cuál NO debe ser un objetivo de la casa en que se encuentra Saturno?',
+        question:'1. ¿Cuál NO debe ser un objetivo de la casa en que se encuentra Saturno?',
         answers:[
             { answer:'Transformar la limitación en excelencia.', value:false },
             { answer:'Ser un impecable ejemplo en esa área de la vida.', value:false },
@@ -10,8 +10,46 @@ export const quiz1:iQuestion[] = [
             { answer:'Desarrollar una estructura conforme adquiramos experiencia.', value:false }
         ]
     }, {
-        question:'',
-        answers:[],
-        sign:true
+        sign:true,
+        question:'2. ¿En qué casa tienes a Saturno?',
+        answers:[
+            { answer:'Casa Once: Las metas a largo plazo, intereses humanitarios, contribución al mundo y los amigos.', value:true }
+        ],
+    }, {
+        sign:true,
+        question:'3. ¿En qué entorno te convierte Saturno en líder?',
+        answers:[
+            { answer:'Con los amigos.', value:true }
+        ],
+    }, {
+        sign:true,
+        question:'4. ¿Cuál es una manifestación de la limitación de Saturno?',
+        answers:[
+            { answer:'Ser muy selectivo con los amigos.', value:true }
+        ],
+    }, {
+        sign:true,
+        question:'5. ¿Cuál es un ejemplo de una buena estructura de tu Saturno?',
+        answers:[
+            { answer:'Formar amistades con personas extraordinarias.', value:true }
+        ],
+    }, {
+        sign:true,
+        question:'6. ¿En dónde debes ser muy responsable de acuerdo a tu Saturno?',
+        answers:[
+            { answer:'La convivencia y diversión con amigos.', value:true }
+        ],
+    }, {
+        sign:true,
+        question:'7. ¿Cuál es una consecuencia en tu carta de que Saturno sea el Dios del tiempo?',
+        answers:[
+            { answer:'Tener amig@s de diferentes edades a la mía, ya sea mayores o menores.', value:true }
+        ],
+    }, {
+        sign:true,
+        question:'¿8. Qué deben admirar las personas más cercanas a tí?',
+        answers:[
+            { answer:'La dedicación y aprecio que tienes por mis amig@s.', value:true }
+        ],
     }
 ]
