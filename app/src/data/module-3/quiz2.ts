df1d1c1436e92dd5235d0988425491ad09dc4539
Santiago8888 <santiago.marti67@gmail.com>
2021-12-26 17:38:31 -0600

    Quiz 3.2: intro question.

---
+++ app/src/data/module-3/quiz2.ts
@@ -0,0 +1,13 @@
+import { iQuestion } from "../../components/Views/Quiz"
+
+export const quiz1:iQuestion[] = [
+    {
+        question:'¿Cuál NO debe ser un objetivo de la casa en que se encuentra Saturno?',
+        answers:[
+            { answer:'Transformar la limitación en excelencia.', value:false },
+            { answer:'Ser un impecable ejemplo en esa área de la vida.', value:false },
+            { answer:'Liberarnos de la influencia astrológica de Saturno.', value:true },
+            { answer:'Desarrollar una estructura conforme adquiramos experiencia.', value:false }
+        ]
+    }
+]
