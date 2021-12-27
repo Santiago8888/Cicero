6c58df538a85e502a36e8e596d99ffd80afbf6e4
Santiago8888 <santiago.marti67@gmail.com>
2021-12-27 09:32:25 -0600

    Add house 1 to answers.
    Signed-off-by: Santiago8888 <santiago.marti67@gmail.com>

--- app/src/data/module-3/quiz2.ts
+++ app/src/data/module-3/quiz2.ts
@@ -13,134 +13,134 @@ export const quiz2:iQuestion[] = [
         sign:true,
         question:'2. ¿En qué casa tienes a Saturno?',
         answers:[
-            { answer:'Casa Once: Las metas a largo plazo, intereses humanitarios, contribución al mundo y los amigos.', value:true },
-            { answer:'', value:true },
+            { answer:'Casa Once: Las metas a largo plazo, intereses humanitarios, contribución al mundo y los amigos.', value:true , house:11},
+            { answer:'', value:true, house:1 },
         ],
     }, {
         sign:true,
         question:'3. ¿En qué entorno te convierte Saturno en líder?',
         answers:[
             { answer:'Con los amigos.', value:true },
-            { answer:'', value:true },
+            { answer:'', value:true, house:1 },
         ],
     }, {
         sign:true,
         question:'4. ¿Cuál es una manifestación de la limitación de Saturno?',
         answers:[
             { answer:'Ser muy selectivo con los amigos.', value:true },
-            { answer:'', value:true },
+            { answer:'', value:true, house:1 },
         ],
     }, {
         sign:true,
         question:'5. ¿Cuál es un ejemplo de una buena estructura de tu Saturno?',
         answers:[
             { answer:'Formar amistades con personas extraordinarias.', value:true },
-            { answer:'', value:true },
+            { answer:'', value:true, house:1 },
         ],
     }, {
         sign:true,
         question:'6. ¿En dónde debes ser muy responsable de acuerdo a tu Saturno?',
         answers:[
             { answer:'La convivencia y diversión con amigos.', value:true },
-            { answer:'', value:true },
+            { answer:'', value:true, house:1 },
         ],
     }, {
         sign:true,
         question:'7. ¿Cuál es una consecuencia en tu carta de que Saturno sea el Dios del tiempo?',
         answers:[
             { answer:'Tener amig@s de diferentes edades a la mía, ya sea mayores o menores.', value:true },
-            { answer:'', value:true },
+            { answer:'', value:true, house:1 },
         ],
     }, {
         sign:true,
         question:'8. ¿Qué deben admirar las personas más cercanas a tí?',
         answers:[
             { answer:'La dedicación y aprecio que tienes por mis amig@s.', value:true },
-            { answer:'', value:true },
+            { answer:'', value:true, house:1 },
         ],
     }, {
         sign:true,
         question:'9. ¿Cuál es el primer paso ser reconocido en la casa de tu Saturno?',
         answers:[
             { answer:'Encontrar una causa y un grupo de personas con las cuales colaborar.', value:true },
-            { answer:'', value:true },
+            { answer:'', value:true, house:1 },
         ]
     }, {
         sign:true,
         question:'10. ¿Qué estructura deberás aportar a través de tu Saturno?',
         answers:[
             { answer:'Orden y organización al grupo con el que colaboro.', value:true },
-            { answer:'', value:true },
+            { answer:'', value:true, house:1 },
         ]
     }, {
         sign:true,
         question:'11. ¿De donde obtendras la motivación para liderear honorablemente?',
         answers:[
             { answer:'Del beneficio común que se obtiene a partir de la cauasa por la que trabajamos en equipo.', value:true },
-            { answer:'', value:true },
+            { answer:'', value:true, house:1 },
         ]
     }, {
         sign:true,
         question:'12. ¿Cómo mantendrás la humildad en la casa de Saturno?',
         answers:[
             { answer:'Siendo consciente que la causa por la que lucho no la puedo alcanzar sólo y requiero de la coperación de los demás.', value:true },
-            { answer:'', value:true },
+            { answer:'', value:true, house:1 },
         ]
     }, {
         sign:true,
         question:'13. ¿Por que serás reconocido por las personas que te conozcan?',
         answers:[
             { answer:'Mi compromiso hacia el grupo, y la causa a la que que dedico mi servicio.', value:true },
-            { answer:'', value:true },
+            { answer:'', value:true, house:1 },
         ]
     }, {
         sign:true,
         question:'14. ¿Qué ejemplo debes proyectar?',
         answers:[
             { answer:'Una persona que devuelve generosamente a la sociedad.', value:true },
-            { answer:'', value:true },
+            { answer:'', value:true, house:1 },
         ]
     }, {
         sign:true,
         question:'15. ¿Qué sabes mejor que nadie?',
         answers:[
             { answer:'Que la comunidad en la que vivimos no es perfecta, y requiere de nuestra participación para mejorarla.', value:true },
-            { answer:'', value:true },
+            { answer:'', value:true, house:1 },
         ]
     }, {
         sign:true,
         question:'16. ¿Qué te admirarán al obedecer tu vocación de servicio?',
         answers:[
             { answer:'Mi altruismo, y ayuda desinteresada.', value:true },
-            { answer:'', value:true },
+            { answer:'', value:true, house:1 },
         ]
     }, {
         sign:true,
         question:'17. ¿Qué pasará conforme avances en tu misión de servicio?',
         answers:[
             { answer:'Lo que considero mi comunidad se expandirá y cada vez involucrará a más personas.', value:true },
-            { answer:'', value:true },
+            { answer:'', value:true, house:1 },
         ]
     }, {
         sign:true,
         question:'18. ¿Qué es recomendable de acuerdo a tu Saturno?',
         answers:[
             { answer:'Elegir una causa inclusiva, independiente de cualquier asociación o afiliación política, religiosa o social.', value:true },
-            { answer:'', value:true },
+            { answer:'', value:true, house:1 },
         ]
     }, {
         sign:true,
         question:'19. ¿Cuáles son excelentes ejemplos de tu vocación de servicio?',
         answers:[
             { answer:'Preservación del medio ambiente, la lucha contra la pobreza o el servicio a los más necesitados.', value:true },
-            { answer:'', value:true },
+            { answer:'', value:true, house:1 },
         ]
     }, {
         sign:true,
         question:'20. ¿Para qué te presiona Saturno?',
         answers:[
             { answer:'Convertirme en un lider sabio y visionario que puede dar respuestas a los demás y ayudarlos a lograr sus ideales y aspiraciones.', value:true },
-            { answer:'', value:true },
+            { answer:'', value:true, house:1 },
         ]
     }
 ]
