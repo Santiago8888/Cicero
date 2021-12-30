1ab15cf10231cfac7369d5e438872530ffd5463c
Santiago8888 <santiago.marti67@gmail.com>
2021-12-30 17:42:07 -0600

    Map houses & map text utils.

--- app/src/utils/sign.tsx
+++ app/src/utils/sign.tsx
@@ -17,4 +17,23 @@ export const mapSigns = (sign:Sign)  => ({
     Pis: 'Piscis'
 }[sign])
 
-export const mapSign = (text:string, { sign }:iUser) => sign ? text.replace('DYNAMIC_SIGN', mapSigns(sign)) : text
+const mapHouses = (house:number) => {
+    if(house === 1) return 'Casa I'
+    if(house === 2) return 'Casa II'
+    if(house === 3) return 'Casa III'
+    if(house === 4) return 'Casa IV'
+    if(house === 5) return 'Casa V'
+    if(house === 6) return 'Casa VI'
+    if(house === 7) return 'Casa VII'
+    if(house === 8) return 'Casa VIII'
+    if(house === 9) return 'Casa IX'
+    if(house === 10) return 'Casa X'
+    if(house === 11) return 'Casa XI'
+    if(house === 12) return 'Casa XII'
+    return ''
+}
+
+const mapSign = (text:string, { sign }:iUser) => sign ? text.replace('DYNAMIC_SIGN', mapSigns(sign)) : text
+export const mapHouse = (text:string, { house }:iUser) => house ? text.replace('DYNAMIC_HOUSE', mapHouses(house)) : text.replace('DYNAMIC_HOUSE', '')
+
+export const mapAstroText = (text:string, user:iUser) => mapHouse(mapSign(text, user), user)
