aee9a7d60d26d93e58a08a569a2898d12dea9315
Santiago8888 <santiago.marti67@gmail.com>
2021-12-30 19:26:17 -0600

    Quiz 3 questions.

---
+++ app/src/data/module-3/quiz3.ts
@@ -0,0 +1,107 @@
+import { iQuestion } from "../../components/Views/Quiz"
+
+export const quiz3:iQuestion[] = [
+    {
+        question: '1. ¿Cuánto tarda Saturno en volver a la misma posición de tu carta natal?',
+        answers:[
+            { answer: 'Aproximadamente 29 o 29 años y medio.', value:true },
+        ]
+    }, {
+        question: '2. ¿Cuánto dura Saturno aproximadamente en cada casa?',
+        answers:[
+            { answer: 'Dos años y medio.', value:true },
+        ]
+    }, {
+        question: '3. ¿Cómo se llama el avance de Saturno a traves de las casas de tu carta?',
+        answers:[
+            { answer: 'Tránsitos', value:true },
+        ]
+    }, {
+        question: '4. ¿Cuáles son algunas de las sensaciones negativos que experimentamos cuando Saturno entra a una nueva casa?',
+        answers:[
+            { answer: 'Que ya no puedo hacer las mismas cosas, me siento limitad@ o lo que parecía que estaba bien ya no lo está.', value:true },
+        ]
+    }, {
+        question: '5. ¿Porqué Saturno también premia, cuando haces bien las cosas?',
+        answers:[
+            { answer: 'Es como un maestro justo.', value:true },
+        ]
+    }, {
+        question: '6. ¿Qué NO evalua Saturno cuando transita por una casa?',
+        answers:[
+            { answer: 'Como nos estamos portando.', value:true },
+            { answer: 'Si estamos haciendo las cosas correctas en esa área de la vida.', value:true },
+            { answer: 'La relación con las personas involucradas con esa casa.', value:true },
+        ]
+    }, {
+        question: '7. ¿Porque debemos estar atentos cuando Saturno está transitando por una casa?',
+        answers:[
+            { answer: 'A veces nos da tiempo y señales antes de traernos las consecuencias.', value:true },
+        ]
+    }, {
+        question: '8. ¿Porqué Saturno cambia la forma de evaluar estando en diferente casa?',
+        answers:[
+            { answer: 'Generalmente cada casa tiene más de un signo el cual cambia en el transcurso de Saturno.', value:true },
+        ]
+    }, {
+        question: '9. ¿Porqué Saturno es lento en su tránsito a traves de las casas?',
+        answers:[
+            { answer: 'Para darnos tiempo de aprendar las lecciones y hacer los cambios pertinentes.', value:true },
+        ]
+    }, {
+        question: '10. ¿Porque Saturno a veces se regresa y vuelve a entrar a una casa de la que salió?',
+        answers:[
+            { answer: 'Por la órbita de la Tierra y para asegurarse que no se le haya olvidado nada.', value:true },
+        ]
+    }, {
+        question: '11. ¿Cuál es el mayor obsequio de Saturno?',
+        answers:[
+            { answer: 'Entendimiento.', value:true },
+        ]
+    }, {
+        question: '12. ¿Que evalua Saturno en cada relación?',
+        answers:[
+            { answer: 'Quien soy en esa relación y no lo que obtengo de ella.', value:true },
+        ]
+    }, {
+        question: '13. ¿Qué busca Saturno al transitar nuestras casas?',
+        answers:[
+            { answer: 'Tener de manera integrada un gran crecimiento y madurez en todas las áreas de la vida.', value:true },
+        ]
+    }, {
+        question: '14. ¿Qué ocurre cuando Saturno entra a una casa?',
+        answers:[
+            { answer: 'Hay un evento importante, como una auditoria en esa casa.', value:true },
+        ]
+    }, {
+        question: '15. ¿Qué ocurre cuando Saturno sale de una casa?',
+        answers:[
+            { answer: 'Te da el resultado final de la auditoria.', value:true },
+        ]
+    }, {
+        question: '16. ¿Qué es lo último que revisa Saturno en una casa?',
+        answers:[
+            { answer: 'Mis planetas natales que están en esa casa.', value:true },
+        ]
+    }, {
+        question: '17. ¿Qué le pasa a un planeta cuando es evaluado?',
+        answers:[
+            { answer: 'También se siente restringido y forzado a brindar lo mejor de su energia.', value:true },
+        ]
+    }, {
+        question: '18. ¿Cuándo corrige Saturno a un planeta?',
+        answers:[
+            { answer: 'Cuando expresa el ego del signo en el que se encuentra.', value:true },
+        ]
+    }, {
+        question: '19. ¿Cuánto dura la evaluación de un planeta?',
+        answers:[
+            { answer: '20 grados en total, los primeros 5 grados son de avisos, los siguientes 10 son cuando más fuerte evalua Saturno y en los útlimos 5 disminuye su influencia.', value:true },
+        ]
+    }, {
+        question: '20. ¿Qué te recuerda Saturno?',
+        answers:[
+            { answer: 'Que yo quiero ser mejor.', value:true },
+        ]
+    }
+]
