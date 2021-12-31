5b3fa595e3246e06f25928a7772377ba24d4e7da
Santiago8888 <santiago.marti67@gmail.com>
2021-12-30 19:02:44 -0600

    Ortography on posts.

--- app/src/data/module-3/posts.ts
+++ app/src/data/module-3/posts.ts
@@ -8,18 +8,18 @@ export const posts1 = [
 ]
 
 export const posts2 = [
-    'Comparte el ciclo de Saturno en el te encuentras: ¿Te sientes preparad@?',
+    'Comparte el ciclo de Saturno en el que te encuentras: ¿Te sientes preparad@?',
     '¿Cómo te gustaría llegar al siguiente "examen" de Saturno?',
     '¿Te gustaría compartir una experiencia que te haya marcado durante los ciclos de Saturno?',
     'Entre los 28 y 29 ocurre el primer retorno de Saturno, ¿Que responsabilidad adquiriste en ese momento?',
     'El despertar espiritual de muchas personas ocurrió a los 33 años, ¿Cuando consideras que fue tu despertar espiritual y cómo fue?', 
-    'A partir de los 35 años identificamos nuestra área de excelencia en la que podemos serivir a los demás. ¿Ya identificaste la tuya?, ¿Cuál es?',
+    'A partir de los 35 años identificamos nuestra área de excelencia en la que podemos servir a los demás. ¿Ya identificaste la tuya?, ¿Cuál es?',
     '¿Cómo explicarías a tus hij@s lo que son los ciclos de Saturno?',
-    'Entre los 42 y 44 se comienza a terminar la oportunidad de probar cosas nuevas, ¿Hay algo uevo que aún te gustaría intentar?',
+    'Entre los 42 y 44 se comienza a terminar la oportunidad de probar cosas nuevas, ¿Hay algo nuevo que aún te gustaría intentar?',
     'A través de la madurez que has adquirido, ¿Qué es aquello que realizas, que más valoras el día de hoy?',
     'Comparte alguna ocasión que te haya tocado retomar el camino. ¿Qué aprendizajes te dejó?',
     'Sabes algo sobre tu Quirón, ¿Qué haces para sanar esa herida de tu alma?',
-    '¿Qué has aprendido en esta vida, en que haz adquirido Sabiduría, qué legado te gustaría dejar como servicio para los demás?',
+    '¿Qué has aprendido en esta vida, en que has adquirido Sabiduría, qué legado te gustaría dejar como servicio para los demás?',
     '¿Qué sueño tienes para tu esa última etapa de vejez o entrega absoluta?',
     '¿Qué te gustaría experimentar en tu próxima vida?'    
 ]
