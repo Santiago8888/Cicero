8abc5e7e22d65d66475ca52c62dfeb2d54f2635b
Santiago8888 <santiago.marti67@gmail.com>
2022-01-16 01:45:46 -0600

    Module 4 modules.

---
+++ app/src/data/module-4/index.ts
@@ -0,0 +1,5 @@
+export const module4 = [
+    { title: '4.1 Binah y el inicio de la existencia', lessons:[] },
+    { title: '4.2 Saturno desde la Kabbalah', lessons:[] },
+    { title: '4.3 El COVID y Saturno en Acuario', lessons:[] }
+]
