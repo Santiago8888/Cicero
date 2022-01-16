323e5f0dbf099e99f327251ad793ba068bebbea0
Santiago8888 <santiago.marti67@gmail.com>
2022-01-16 01:57:25 -0600

    Unit 3 - video lessons.

--- app/src/data/module-4/index.ts
+++ app/src/data/module-4/index.ts
@@ -11,8 +11,14 @@ const lessons2:iLesson[] = [
     { type:'Video', title:'Mi Misión Espiritual', link:'https://youtu.be/uE7kmnAwLhw' },
 ]
 
+const lessons3:iLesson[] = [
+    { type:'Video', title:'Saturno en Acuario', link:'https://youtu.be/QnQr0FDW5gI' },
+    { type:'Video', title:'Meditación Final', link:'https://youtu.be/BljfRq4j2uY' },
+]
+
+
 export const module4 = [
     { title: '4.1 Saturno en la Kabbalah', lessons:lessons1 },
     { title: '4.2 Binah y el inicio de la existencia', lessons:lessons2 },
-    { title: '4.3 Saturno en Acuario y El COVID', lessons:[] }
+    { title: '4.3 Saturno en Acuario y El COVID', lessons:lessons3 }
 ]
