04c098f7c5e546295ab9bfb469b7be58e23b4237
Santiago8888 <santiago.marti67@gmail.com>
2022-01-16 01:54:13 -0600

    Unit 1 video lessons.

--- app/src/data/module-4/index.ts
+++ app/src/data/module-4/index.ts
@@ -1,5 +1,13 @@
+import { iLesson } from '../../components/LayOut/Menu'
+
+
+const lessons1:iLesson[] = [
+    { type:'Video', title:'Introducción a la Kabbalah', link:'https://youtu.be/9bjn7W2F-KM' },
+    { type:'Video', title:'Saturno en el Árbol de la Vida', link:'https://youtu.be/jPLDQQq_CfM' },
+]
+
 export const module4 = [
-    { title: '4.1 Binah y el inicio de la existencia', lessons:[] },
-    { title: '4.2 Saturno desde la Kabbalah', lessons:[] },
-    { title: '4.3 El COVID y Saturno en Acuario', lessons:[] }
+    { title: '4.1 Saturno en la Kabbalah', lessons:lessons1 },
+    { title: '4.2 Binah y el inicio de la existencia', lessons:[] },
+    { title: '4.3 Saturno en Acuario y El COVID', lessons:[] }
 ]
