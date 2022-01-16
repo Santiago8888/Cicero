50d5f070c145971290c01aa2d1f2311946490b46
Santiago8888 <santiago.marti67@gmail.com>
2022-01-16 01:56:14 -0600

    Unit 2 video lessons.

--- app/src/data/module-4/index.ts
+++ app/src/data/module-4/index.ts
@@ -6,8 +6,13 @@ const lessons1:iLesson[] = [
     { type:'Video', title:'Saturno en el Árbol de la Vida', link:'https://youtu.be/jPLDQQq_CfM' },
 ]
 
+const lessons2:iLesson[] = [
+    { type:'Video', title:'La Apreciación', link:'https://youtu.be/esiDMjWW2Fk' },
+    { type:'Video', title:'Mi Misión Espiritual', link:'https://youtu.be/uE7kmnAwLhw' },
+]
+
 export const module4 = [
     { title: '4.1 Saturno en la Kabbalah', lessons:lessons1 },
-    { title: '4.2 Binah y el inicio de la existencia', lessons:[] },
+    { title: '4.2 Binah y el inicio de la existencia', lessons:lessons2 },
     { title: '4.3 Saturno en Acuario y El COVID', lessons:[] }
 ]
