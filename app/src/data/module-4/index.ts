bb60f140b9f63bbb030bf9a95afdb155019fe030
Santiago8888 <santiago.marti67@gmail.com>
2022-01-16 02:15:15 -0600

    Full index titles.

--- app/src/data/module-4/index.ts
+++ app/src/data/module-4/index.ts
@@ -3,17 +3,28 @@ import { iLesson } from '../../components/LayOut/Menu'
 
 const lessons1:iLesson[] = [
     { type:'Video', title:'Introducción a la Kabbalah', link:'https://youtu.be/9bjn7W2F-KM' },
+    { type:'Quiz', title:'Quiz 4.1', questions:[] },
+    { type:'Reading', title:'El próposito de la Kabbalah' },
     { type:'Video', title:'Saturno en el Árbol de la Vida', link:'https://youtu.be/jPLDQQq_CfM' },
+    { type:'Reflection', title:'Ejercicio de Reflexión' }
 ]
 
 const lessons2:iLesson[] = [
     { type:'Video', title:'La Apreciación', link:'https://youtu.be/esiDMjWW2Fk' },
+    { type:'Quiz', title:'Quiz 4.2', questions:[] },
     { type:'Video', title:'Mi Misión Espiritual', link:'https://youtu.be/uE7kmnAwLhw' },
+    { type:'Reading', title:'La Llave de la Creación' },
+    { type:'Reflection', title:'Ejercicio de Compartir' }
 ]
 
 const lessons3:iLesson[] = [
+    { type:'Reading', title:'Saturno y el Shabbat' },
+    { type:'Quiz', title:'Quiz 4.3', questions:[] },
     { type:'Video', title:'Saturno en Acuario', link:'https://youtu.be/QnQr0FDW5gI' },
-    { type:'Video', title:'Meditación Final', link:'https://youtu.be/BljfRq4j2uY' },
+    { type:'Reflection', title:'Reflexión Final' },
+    { type:'Video', title:'Meditación', link:'https://youtu.be/BljfRq4j2uY' },
+    { type:'Reading', title:'Siguientes Pasos' },
+    { type:'Reflection', title:'Ejercicio Final' },
 ]
 
 
