fd1cac1200cf769eaf59757bb3da2765c6755f7f
Santiago8888 <santiago.marti67@gmail.com>
2022-01-20 06:05:54 -0500

    Quiz 4.1 questions & answers.

---
+++ app/src/data/module-4/quiz1.ts
@@ -0,0 +1,209 @@
+import { iQuestion } from "../../components/Views/Quiz"
+
+export const quiz1:iQuestion[] = [
+    {
+        question: '1. ¿Qué significa lekabel (לְקַבֵּל) en hebreo?', 
+        answers:[
+            { answer:'Recibir.', value:true },
+        ]
+    }, {
+        question: '2. ¿Qué es la Kabbalah?', 
+        answers:[
+            { answer:'Una disciplina.', value:true },
+        ]
+    }, {
+        question: '3. ¿Para qué estamos aquí?', 
+        answers:[
+            { answer:'Entender como funciona la vida.', value:true },
+        ]
+    }, {
+        question: '4. ¿Cómo nos ayuda la Kabbalah en nuestra vida?', 
+        answers:[
+            { answer:'Nos brinda respuestas.', value:true },
+        ]
+    }, {
+        question: '5. ¿De dónde proviene la sabiduría de la Kabbalah?', 
+        answers:[
+            { answer:'El pentateuco (los cinco primeros libros de la Biblia o Torah).', value:true },
+        ]
+    }, {
+        question: '6. ¿Qué aprendemos a través de la historia de Moises?', 
+        answers:[
+            { answer:'A eliminar el ego y sacar la luz.', value:true },
+        ]
+    }, {
+    question: '7. ¿Cuál es el legado de Shimon bar Yochai?',
+        answers:[
+            { answer:'El Zohar, el libro del resplandor.', value:true },
+        ]
+    }, {
+        question: '8. ¿Qué estudiamos en el Zohar?',
+        answers:[
+            { answer:'El evento cósmico de cada semana que nos invita a mejorar.', value:true },
+        ]
+    }, {
+        question: '9. ¿Qué significa ser mejor persona?',
+        answers:[
+            { answer:'Tener más espiritú, más luz.', value:true },
+        ]
+    }, {
+        question: '10. ¿Cómo obtenemos más luz?', 
+        answers:[
+            { answer:'Eliminando al ego.', value:true },
+        ]
+    }, {
+        question: '11. ¿Qué es el ego?', 
+        answers:[
+            { answer:'Una máscara o muleta provisional para desarrollarnos antes de comenzar a SER.', value:true },
+        ]
+    }, {
+        question: '12. ¿Qué pasa cuando comenzamos a estudiar Kabbalah?', 
+        answers:[
+            { answer:'Nos transforma la manera como vemos la vida.', value:true },
+        ]
+    }, {
+        question: '13. ¿Cuál es la primera enseñanza de la Kabbalah?', 
+        answers:[
+            { answer:'No hay cosas ni buenas ni malas, hay situaciones que ocurren para nuestro crecimiento.', value:true },
+        ]
+    }, {
+        question: '14. ¿Qué es el árbol de la vida?', 
+        answers:[
+            { answer:'Un esquema integrado en cada ser viviente.', value:true },
+        ]
+    }, {
+        question: '15. ¿Cómo recibimos la luz?', 
+        answers:[
+            { answer:'Desde la luz infinita, gradualmente recibimos la luz que podemos contenter y sostener. ', value:true },
+        ]
+    }, {
+        question: '16. ¿De dónde provienen las limitaciones que nos impiden recibir más luz?', 
+        answers:[
+            { answer:'De nuestra mente.', value:true },
+        ]
+    }, {
+        question: '17. ¿Qué nos ayuda a recibir más luz cada semana?', 
+        answers:[
+            { answer:'Alinearnos con el evento cósmico que nos indica que tipo de luz podemos recibir esa semana.', value:true },
+        ]
+    }, {
+        question: '18. ¿De qué depende lo que manifestamos en nuestra vida?', 
+        answers:[
+            { answer:'De nuestra luz, los pensamientos y conocimientos.', value:true },
+        ]
+    }, {
+        question: '19. ¿Cuál es la primera paso, escalón o esfera durante el descenso de la luz infinita?', 
+        answers:[
+            { answer:'Inspiración, que alcanzamos conforme aumentamos nuestra capacidad de recibir.', value:true },
+        ]
+    }, {
+    question: '20. ¿Cuál es el segundo paso durante el descenso de la luz?', 
+        answers:[
+            { answer:'La sabiduría o entendimiento, que es en donde se encuentra Saturno.', value:true },
+        ]
+    }, {
+
+        question: '21. ¿Cómo manejamos la cantidad y cualidad de la luz que recibimos?', 
+        answers:[
+            { answer:'Desde nuestro deseo.', value:true },
+        ]
+    }, {
+        question: '22. ¿Cómo empieza el camino desde deseo hasta la manifestación?', 
+        answers:[
+            { answer:'Conecta nuestro deseo de recibir con nuestros pensamiento y emociones para empezar a manifestar en el mundo astral.', value:true },
+        ]
+    }, {
+        question: '23. ¿Qué es la maduración del deseo?', 
+        answers:[
+            { answer:'Pasar de desear cosas para uno mismo para comenzar a desear para los demás.', value:true },
+        ]
+    }, {
+        question: '24. ¿Cómo se desarrolla nuestro poder creador?', 
+        answers:[
+            { answer:'Al querer algo, enfocarnos, concetranos, visualizar y accionar hasta que obtengamos nuestro deseo. ', value:true },
+        ]
+    }, {
+        question: '25. ¿Por qué aquello que creamos cambia?', 
+        answers:[
+            { answer:'Por que reflejan nuestro propio cambio.', value:true },
+        ]
+    }, {
+        question: '26. ¿Cuando crece nuestra alma?', 
+        answers:[
+            { answer:'A traves de muchas vidas en las que practicamos nuestro poder creador y compartimos cada vez más.', value:true },
+        ]
+    }, {
+        question: '27. ¿Cuál es un gran ejemplo de compartir, de recibir para otros?', 
+        answers:[
+            { answer:'Cuando tenemos hijos.', value:true },
+        ]
+    }, {
+        question: '28. ¿Qué pasa cuando comienzas a compartir?', 
+        answers:[
+            { answer:'Mi vida cambia completamente de proposito, se convierte en amor, y nueesta alma se regocija.', value:true },
+        ]
+    }, {
+        question: '29. ¿Qué es el despertar?', 
+        answers:[
+            { answer:'Cuando nuestra alma se comienza a preguntar ¿porqué?, ¿después qué? y empieza a necesitar respuestas.', value:true },
+        ]
+    }, {
+        question: '30. ¿Cual de estas NO es una pregunta parte del despertar?', 
+        answers:[
+            { answer:'¿Quién creo esto?', value:false },
+            { answer: '¿Qué o quién es Dios?', value:false },
+            { answer: '¿Quén hizo el universo?', value:false }
+        ]
+    }, {
+        question: '31. ¿Cuando comienza a funcionar la Kabbalah en nuestra vida?', 
+        answers:[
+            { answer:'¡Al despertar!', value:true },
+        ]
+    }, {
+        question: '32. ¿Qué nos explica la Kabbalah?', 
+        answers:[
+            { answer:'Cómo otros fueron encontrando este camino, como les fueron dando las respuestas.', value:true },
+        ]
+    }, {
+        question: '33. ¿Qué encontramos conforme recorremos el camino de la Kabbalah?', 
+        answers:[
+            { answer:'Qué todo esto es perfecto, nos maravillamos.', value:true },
+        ]
+    }, {
+        question: '34. ¿Porqué las cosas parecen equivocadas, incorrectas o malas?', 
+        answers:[
+            { answer:'Es nuestra naturaleza, del proceso para alcanzar la sabiduria.', value:true },
+        ]
+    }, {
+        question: '35. ¿Cuál es la conclusión de el camino espiritual?', 
+        answers:[
+            { answer:'La consciencia de que es hermoso estar en este mundo y estamos aquí para amar y compartir.', value:true },
+        ]
+    }, {
+        question: '36. ¿Cómo conectar con mundos superiores?', 
+        answers:[
+            { answer:'A partir del regocijo de mi alma, y las experiencias que sirvan en pro del propósito que adquirimos en esta vida. ', value:true },
+        ]
+    }, {
+        question: '37. ¿Para qué nos sirve el Árbol de la vida?', 
+        answers:[
+            { answer:'Para ir paso a paso manifestando cada día más y alcanzar esferas superiores como la Sabiduria.', value:true },
+        ]
+    }, {
+        question: '38. ¿Qué nos toca manifestar en esta vida?', 
+        answers:[
+            { answer:'Amor.', value:true },
+        ]
+    }, {
+        question: '39. ¿De que se alimenta nuestro mundo espiritual?', 
+        answers:[
+            { answer:'Amor.', value:true },
+        ]
+    }, {
+        question: '40. ¿Para qué sirve la Kabbalah?', 
+        answers:[
+            { answer:'Terminar el caos y sufrimiento aparente y encontrar la felicidad y vida eterna.', value:true },
+        ]
+    }
+]
+
