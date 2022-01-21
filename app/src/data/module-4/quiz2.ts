8f524ca4547af916380a5ec8298d190cd2bb85c0
Santiago8888 <santiago.marti67@gmail.com>
2022-01-21 10:24:10 -0500

    Quiz 4.2

---
+++ app/src/data/module-4/quiz2.ts
@@ -0,0 +1,250 @@
+import { iQuestion } from "../../components/Views/Quiz"
+
+export const quiz2:iQuestion[] = [
+    {
+        question: '1. ¿Cuántas esferas hay en el árbol de la vida?', 
+        answers:[
+            { answer:'10', value:true },
+        ]
+    }, {
+        question: '2. ¿Qué significa Keter?', 
+        answers:[
+            { answer:'Corona', value:true },
+        ]
+    }, {
+        question: '3. ¿De qué color es Keter?', 
+        answers:[
+            { answer:'Blanco', value:true },
+        ]
+    }, {
+        question: '4. ¿Qué contiene Keter?', 
+        answers:[
+            { answer:'Absolutamente todo lo que pudieramos haber deseado o ser.', value:true },
+        ]
+    }, {
+        question: '5. ¿Porqué no podemos absorber la luz infinita de Keter?', 
+        answers:[
+            { answer:'Si lo hcieramos seriamos todo, y también nada.', value:true },
+        ]
+    }, {
+        question: '6. ¿Qué necesitamos hacer para manifestar la luz infinita de Keter?', 
+        answers:[
+            { answer:'Diferenciar la luz, elegir una de las posibilidades.', value:true },
+        ]
+    }, {
+        question: '7. ¿Hacia donde llega la luz diferenciada de Keter?', 
+        answers:[
+            { answer:'A Chochmah', value:true },
+        ]
+    }, {
+        question: '8. ¿Qué es Chochmah?', 
+        answers:[
+            { answer:'Inspiración', value:true },
+        ]
+    }, {
+        question: '9. ¿De que color es Chochmah?', 
+        answers:[
+            { answer:'Plateado', value:true },
+        ]
+    }, {
+        question: '10. ¿Qué nos aporta Chochmah?', 
+        answers:[
+            { answer:'Ideas.', value:true },
+        ]
+    }, {
+        question: '11. ¿Hacia donde se dirige la fuerza de Chochmah?', 
+        answers:[
+            { answer:'Binah.', value:true },
+        ]
+    }, {
+        question: '12. ¿De qué color es Binah?', 
+        answers:[
+            { answer:'Negro', value:true },
+        ]
+    }, {
+        question: '13. ¿Cuál es la función de Binah?', 
+        answers:[
+            { answer:'Filtra la luz de Chochmah que si se va a manifestar y deja a un lado la que todavía no se va a manifestar.', value:true },
+        ]
+    }, {
+        question: '14. ¿Cuál es el rol de Saturno en Binah?', 
+        answers:[
+            { answer:'Confinar o limitar para que podamos manifestarlo de acuerdo a nuestra capcidad y entendimiento.', value:true },
+        ]
+    }, {
+        question: '15. ¿Qué significa Binah?', 
+        answers:[
+            { answer:'Entendimiento.', value:true },
+        ]
+    }, {
+        question: '16. ¿Qué nos brinda Binah?', 
+        answers:[
+            { answer:'El propósito de nuestra existencia.', value:true },
+        ]
+    }, {
+        question: '17. ¿Cuál debe ser el resultado de Binah?', 
+        answers:[
+            { answer:'Encontrar a través del discernimiento la mejor idea que viene de Chochmah.', value:true },
+        ]
+    }, {
+        question: '18. ¿Qué encontramos después de Binah y los mundos superiores?', 
+        answers:[
+            { answer:'A Chesed', value:true },
+        ]
+    }, {
+        question: '19. ¿Qué significa Chesed?', 
+        answers:[
+            { answer:'Misericordia', value:true },
+        ]
+    }, {
+        question: '20. ¿Qué reside en Chesed?', 
+        answers:[
+            { answer:'La abundancia infinita.', value:true },
+        ]
+    }, {
+        question: '21. ¿De qué color es Chesed?', 
+        answers:[
+            { answer:'Azul', value:true },
+        ]
+    }, {
+        question: '22. ¿Cómo se le llama también a Chesed?', 
+        answers:[
+            { answer:'La inteligencia divina.', value:true },
+        ]
+    }, {
+        question: '23. ¿Qué nos posibilita Chesed?', 
+        answers:[
+            { answer:'Convertirnos en seres amorosos, compasivos.', value:true },
+        ]
+    }, {
+        question: '24. ¿Porqué la luz de Chesed busca seguir avanzando?', 
+        answers:[
+            { answer:'Para encontrar límites y no ser indulgente o excederse.', value:true },
+        ]
+    }, {
+        question: '25. ¿Hacia donde llega la luz de Chesed?', 
+        answers:[
+            { answer:'A Geburah.', value:true },
+        ]
+    }, {
+        question: '26. ¿Cuáles son las cualidades de Geburah?', 
+        answers:[
+            { answer:'Valentía, fuerza y herramientas para manifestar la idea o deseo.', value:true },
+        ]
+    }, {
+        question: '27. ¿Cuál es la limitación de Geburah?', 
+        answers:[
+            { answer:'Necesita ser justo, no puede manifestar algo injusto.', value:true },
+        ]
+    }, {
+        question: '28. ¿De qué color es Geburah?', 
+        answers:[
+            { answer:'Rojo.', value:true },
+        ]
+    }, {
+        question: '29. ¿Cuál es la función de Geburah?', 
+        answers:[
+            { answer:'Poner un deseo en marcha, comenzar a manifestar.', value:true },
+        ]
+    }, {
+        question: '30. ¿Qué requiere la luz para proseguir su camino de manifestación?', 
+        answers:[
+            { answer:'Equilibrio.', value:true },
+        ]
+    }, {
+        question: '31. ¿A llega la fuerza de Geburah?', 
+        answers:[
+            { answer:'Tifferet.', value:true },
+        ]
+    }, {
+        question: '32. ¿Qué es Tifferet?', 
+        answers:[
+            { answer:'Armonía, equilibrio, el balance perfecto.', value:true },
+        ]
+    }, {
+        question: '33. ¿En dónde se encuentra Tifferet?', 
+        answers:[
+            { answer:'En el pecho, en donde decimos yo soy.', value:true },
+        ]
+    }, {
+        question: '34. ¿De que color es Tifferet?', 
+        answers:[
+            { answer:'Amarillo.', value:true },
+        ]
+    }, {
+        question: '35. ¿Qué equilibra Tifferet?', 
+        answers:[
+            { answer:'Nuestro potencial, para poder escalar a mundos superiores y manifestar en el mundo material. ', value:true },
+        ]
+    }, {
+        question: '36. ¿Qué pasa cuando la luz parte de Tifferet?', 
+        answers:[
+            { answer:'Ponemos en práctica, lo que queremos manifestar.', value:true },
+        ]
+    }, {
+        question: '37. ¿A dónde llega la luz de Tifferet?', 
+        answers:[
+            { answer:'Netzach', value:true },
+        ]
+    }, {
+        question: '38. ¿Qué significa Netzach?', 
+        answers:[
+            { answer:'Victoria y amor.', value:true },
+        ]
+    }, {
+        question: '39. ¿Qué ocurre en Netzach?', 
+        answers:[
+            { answer:'Encontramos las emociones que son afines a nuestro deseo de manifestación.', value:true },
+        ]
+    }, {
+        question: '40. ¿Cuál es la atributo más virtuoso de Netzach?', 
+        answers:[
+            { answer:'La pureza de juicio.', value:true },
+        ]
+    }, {
+        question: '41. ¿Qué esfera sigue después de Netzach?', 
+        answers:[
+            { answer:'Hod', value:true },
+        ]
+    }, {
+        question: '42. ¿De que color son Netzach y Hod?', 
+        answers:[
+            { answer:'verde y Naranja.', value:true },
+        ]
+    }, {
+        question: '43. ¿Cuál es la función de Hod?', 
+        answers:[
+            { answer:'Encontrar y sostener los pensamientos con los que vamos a crear.', value:true },
+        ]
+    }, {
+        question: '44. ¿A donde llegan los pensamientos de Hod?', 
+        answers:[
+            { answer:'A Yesod', value:true },
+        ]
+    }, {
+        question: '45. ¿De qué color es la esfera de Yesod?', 
+        answers:[
+            { answer:'Morado.', value:true },
+        ]
+    }, {
+        question: '46. ¿Qué rige la esfera de Yesod?', 
+        answers:[
+            { answer:'Nuestros deseos.', value:true },
+        ]
+    }, {
+        question: '47. ¿Cuál es la función de Yesod?', 
+        answers:[
+            { answer:'Transmutar nuestros deseos, encaminarlos hacia la idea creadora, el plan divino.', value:true },
+        ]
+    }, {
+        question: '48. ¿Qué rige la esfera de Malchut?', 
+        answers:[
+            { answer:'Nuestras acciones.', value:true },
+        ]
+    }, {
+        question: '49. ¿Cuál es el último paso en el camino de la manifestación?', 
+        answers:[
+            { answer:'Dar el tiempo necesario a nuestras emociones, pensamientos, deseos y acciones para que logren materializarse.', value:true }
+        ]
+    }
+]
