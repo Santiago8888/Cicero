10f6857f33a35c2a3bd728cdf001d5006f74adab
Santiago8888 <santiago.marti67@gmail.com>
2022-01-21 10:23:51 -0500

    Add first unit posts.

---
+++ app/src/data/module-4/posts.ts
@@ -0,0 +1,13 @@
+export const posts1 = [
+    'Haz una lista con las 10 cosas que más agradeces hoy en tu vida:',
+    '¿Cómo ha cambiado el curso tu forma de ver y experimentar la apreciación?',
+    'Comparte una experiencia en la que agradecieras alguna limitación:',
+    '¿Quiénes son en tu vida las personas que te limitan positivamente?', 
+    '¿Qué fue algo que sólo a partir de perderlo lo valoraste?',
+    '¿Cómo recuperaste algo que habías perdido, por no apreciarlo?',
+    '¿Pará que tí qué es el alma?',
+    'Comparte una experiencia en la que te limitó o restringió tu madre para educarte:',
+    '¿Como madre o padre, que restringes el día de hoy a tus hijos y porqué?',
+    '¿En qué te limitas a ti mism@?',
+    '¿Cuándo fue que perdiste algo pero en cambio ganaste paz?'
+]
