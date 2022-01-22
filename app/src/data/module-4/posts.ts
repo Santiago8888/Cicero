2fd836f8d4ae7ca2885129c38970a0ed041ce6e5
Santiago8888 <santiago.marti67@gmail.com>
2022-01-22 12:16:14 -0500

    Remove numbering on posts.

--- app/src/data/module-4/posts.ts
+++ app/src/data/module-4/posts.ts
@@ -13,15 +13,15 @@ export const posts1 = [
 ]
 
 export const posts2 = [
-    '1. ¿Si tuvieras que definir tu misión hoy, como la expresarías?',
-    '2. ¿Cómo explicarías a un@ amig@ o compañer@ de trabajo porque es importante tener una misión?',
-    '3. ¿Cuál es el cambio principal que el curso de Saturno ha traido a tu vida?',
-    '4. ¿Ya sabes en que es lo que quieres brillar, llevar a la excelencia?',
-    '5. ¿Qué te ha enseñado el COVID a través de tanta limitación?',
-    '6. ¿Qué fue lo más valioso que aportaste al mundo durante el último año?',
-    '7. ¿Cómo cambia misión desde la perspectiva espiritual (definirla sólo en base a ser y dar)?',
-    '8. Hasta el día de hoy, ¿Cuál es el legado, o huella que has dejado al mundo?',
-    '9. ¿Cuál es tu mayor obstáculo o desafío en camino a cumplir tu misión espiritual y como planeas sobrellevarlo?',
-    '10. ¿Qué le dirías a alguien que ya no está con nosotros y sin embargo te encamino en tu misión espiritual?',
-    '11. ¿A quién le obsequiarías este curso, crees que la persona necesita la información o más bien ya está lista para recibirla?'
+    '¿Si tuvieras que definir tu misión hoy, como la expresarías?',
+    '¿Cómo explicarías a un@ amig@ o compañer@ de trabajo porque es importante tener una misión?',
+    '¿Cuál es el cambio principal que el curso de Saturno ha traido a tu vida?',
+    '¿Ya sabes en que es lo que quieres brillar, llevar a la excelencia?',
+    '¿Qué te ha enseñado el COVID a través de tanta limitación?',
+    '¿Qué fue lo más valioso que aportaste al mundo durante el último año?',
+    '¿Cómo cambia misión desde la perspectiva espiritual (definirla sólo en base a ser y dar)?',
+    'Hasta el día de hoy, ¿Cuál es el legado, o huella que has dejado al mundo?',
+    '¿Cuál es tu mayor obstáculo o desafío en camino a cumplir tu misión espiritual y como planeas sobrellevarlo?',
+    '¿Qué le dirías a alguien que ya no está con nosotros y sin embargo te encamino en tu misión espiritual?',
+    '¿A quién le obsequiarías este curso, crees que la persona necesita la información o más bien ya está lista para recibirla?'
 ]
