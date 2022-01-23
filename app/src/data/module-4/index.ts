098be12def554838351baf9d037b2fc5c1243863
Santiago8888 <santiago.marti67@gmail.com>
2022-01-23 15:49:23 -0500

    Import quiz 2 questions.

--- app/src/data/module-4/index.ts
+++ app/src/data/module-4/index.ts
@@ -1,25 +1,26 @@
 import { iLesson } from '../../components/LayOut/Menu'
 import { posts1, posts2 } from './posts'
 import { quiz1 } from './quiz1'
+import { quiz2 } from './quiz2'
 
 
 const lessons1:iLesson[] = [
     { type:'Video', title:'Introducción a la Kabbalah', link:'https://youtu.be/9bjn7W2F-KM' },
     { type:'Quiz', title:'Quiz 4.1', questions:quiz1 },
-    { type:'Reading', title:'Shabbat' },
     { type:'Video', title:'La Apreciación', link:'https://youtu.be/esiDMjWW2Fk' },
     { type:'Reflection', title:'Ejercicio de Reflexión', posts:posts1, numbered:true }
 ]
 
 const lessons2:iLesson[] = [
     { type:'Video', title:'El Camino de la Manifestación', link:'https://youtu.be/votYUa9V12c' },
-    { type:'Quiz', title:'Examen Final', questions:[] },
+    { type:'Quiz', title:'Examen Final', questions:quiz2 },
     { type:'Reading', title:'La Llave de la Creación' },
     { type:'Video', title:'Mi Misión Espiritual', link:'https://youtu.be/uE7kmnAwLhw' },
-    { type:'Reflection', title:'Ejercicio de Compartir', posts:posts2 }
+    { type:'Reflection', title:'Ejercicio de Compartir', posts:posts2, numbered:true }
 ]
 
 const lessons3:iLesson[] = [
+    { type:'Reading', title:'Shabbat' },
     { type:'Video', title:'Saturno y el COVID', link:'https://youtu.be/QnQr0FDW5gI' },
     { type:'Reflection', title:'Reflexión de la Era de Acuario' },
     { type:'Video', title:'Meditación', link:'https://youtu.be/BljfRq4j2uY' },
