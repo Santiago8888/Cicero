3365ef77341a055e5421a78a2d11cad864d89a88
Santiago8888 <santiago.marti67@gmail.com>
2022-01-23 17:04:45 -0500

    Edit on posts.

--- app/src/data/module-4/posts.ts
+++ app/src/data/module-4/posts.ts
@@ -1,27 +1,27 @@
 export const posts1 = [
     'Haz una lista con las 10 cosas que más agradeces hoy en tu vida:',
-    '¿Cómo ha cambiado el curso tu forma de ver y experimentar la apreciación?',
-    'Comparte una experiencia en la que agradecieras alguna limitación:',
+    '¿Cómo ha cambiado el curso tu forma de ver y experimentar la limitación?',
+    'Comparte una experiencia en la que agradecieras la limitación:',
     '¿Quiénes son en tu vida las personas que te limitan positivamente?', 
-    '¿Qué fue algo que sólo a partir de perderlo lo valoraste?',
-    '¿Cómo recuperaste algo que habías perdido, por no apreciarlo?',
+    '¿Qué fue algo que sólo a partir de haberlo perdido lo valoraste?',
+    '¿Cómo recuperaste algo que habías perdido por no haberlo apreciado?',
     '¿Pará que tí qué es el alma?',
-    'Comparte una experiencia en la que te limitó o restringió tu madre para educarte:',
+    'Comparte una experiencia en la que tu madre te limitó o restringió para educarte:',
     '¿Como madre o padre, que restringes el día de hoy a tus hijos y porqué?',
     '¿En qué te limitas a ti mism@?',
     '¿Cuándo fue que perdiste algo pero en cambio ganaste paz?'
 ]
 
 export const posts2 = [
-    '¿Si tuvieras que definir tu misión hoy, como la expresarías?',
+    '¿Si tuvieras que definir tu misión sucintamente, como la expresarías?',
     '¿Cómo explicarías a un@ amig@ o compañer@ de trabajo porque es importante tener una misión?',
     '¿Cuál es el cambio principal que el curso de Saturno ha traido a tu vida?',
-    '¿Ya sabes en que es lo que quieres brillar, llevar a la excelencia?',
+    '¿Ya sabes en donde quieres brillar, que llevarás a la excelencia?',
     '¿Qué te ha enseñado el COVID a través de tanta limitación?',
     '¿Qué fue lo más valioso que aportaste al mundo durante el último año?',
-    '¿Cómo cambia misión desde la perspectiva espiritual (definirla sólo en base a ser y dar)?',
-    'Hasta el día de hoy, ¿Cuál es el legado, o huella que has dejado al mundo?',
-    '¿Cuál es tu mayor obstáculo o desafío en camino a cumplir tu misión espiritual y como planeas sobrellevarlo?',
+    '¿Cómo cambia tu misión desde la perspectiva espiritual (centrarla en el ser y dar)?',
+    '¿Cuál es el legado o huella que al día de hoy has dejado al mundo?',
+    '¿Cuál es tu mayor obstáculo o desafío en camino a cumplir tu misión espiritual y cómo lo superarás?',
     '¿Qué le dirías a alguien que ya no está con nosotros y sin embargo te encamino en tu misión espiritual?',
     '¿A quién le obsequiarías este curso, crees que la persona necesita la información o más bien ya está lista para recibirla?'
 ]
