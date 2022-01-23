1f1ad76ac7460c9f74e2b09b689937dca44b1c37
Santiago8888 <santiago.marti67@gmail.com>
2022-01-23 17:14:33 -0500

    Description on lecture 2.

--- app/src/data/module-4/index.ts
+++ app/src/data/module-4/index.ts
@@ -11,10 +11,16 @@ const lessons1:iLesson[] = [
     { type:'Reflection', title:'Ejercicio de Reflexión', posts:posts1, numbered:true }
 ]
 
+const description2 = [
+    'Esta lectura te enseña el proceso para materializar una idea, pensamiento o deseo utiliza las herramientas de la Kabbalah.', 
+    'Es una técnica que puedes utilizar a cada momento de tu vida, y entre más la practiques más fácil te será utilizarla.',
+    'Tiempo mínimo de lectura sugerido: 6 minutos.',
+]
+
 const lessons2:iLesson[] = [
     { type:'Video', title:'El Camino de la Manifestación', link:'https://youtu.be/votYUa9V12c' },
     { type:'Quiz', title:'Examen Final', questions:quiz2 },
-    { type:'Reading', title:'La Llave de la Creación', link:'docs/La-Llave-de-la-Creación.pdf', min:6 },
+    { type:'Reading', title:'La Llave de la Creación', link:'docs/La-Llave-de-la-Creación.pdf', min:6, description:description2 },
     { type:'Video', title:'Mi Misión Espiritual', link:'https://youtu.be/uE7kmnAwLhw' },
     { type:'Reflection', title:'Ejercicio de Compartir', posts:posts2, numbered:true }
 ]
@@ -25,7 +31,7 @@ const lessons3:iLesson[] = [
     { type:'Reflection', title:'Reflexión de la Era de Acuario' },
     { type:'Video', title:'Meditación', link:'https://youtu.be/BljfRq4j2uY' },
     { type:'Reading', title:'Siguientes Pasos' },
-    { type:'Reflection', title:'Ejercicio Final' },
+    { type:'Reflection', title:'Ejercicio Final', end:true, disabled:true },
 ]
 
 
